d04b2bd19b7884633881068e93135481d17cb879
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 12:28:47 -0300

    Sync LUA 113400 a GAME

---
+++ GAME/113400.lua
@@ -0,0 +1,11 @@
+--game: APB Reloaded
+
+addappid(113400)
+addappid(113402)
+addappid(113403)
+addappid(113405)
+addappid(226490)
+addappid(228984,0,"df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068")
+addappid(229002,0,"f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738")
+addappid(265060)
+addappid(265061)
