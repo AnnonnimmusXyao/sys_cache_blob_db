ef4047f6d71e8624b4d34c65cb31ab990ccc9406
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:24:00 -0300

    Sync LUA 248390 a GAME

---
+++ GAME/248390.lua
@@ -0,0 +1,17 @@
+--game: Craft The World
+
+addappid(248390)
+addappid(248391,0,"274bd06c718fcd808c0da011f82ad6dd30d033820190fb44486a5d9620b13d4b")
+addappid(248392,0,"7be3e3d259ff5cd3081f3ff4835215fca8d605a5967ae88d08892a48ccc916a0")
+addappid(492680)
+addappid(609800)
+addappid(1105370)
+addappid(1105371)
+addappid(1157170)
+addappid(1157310)
+addappid(1209150)
+addappid(1338630)
+addappid(1509120)
+addappid(2739550)
+addappid(3392970)
+addappid(4010190)
