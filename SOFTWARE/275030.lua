3acbb39236797f7e7e8331f80dd486b60f199fc4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:11:27 -0300

    Sync LUA 275030 a SOFTWARE

---
+++ SOFTWARE/275030.lua
@@ -0,0 +1,5 @@
+--game: Punch! ViaCAD 2D/3D v9 + 3D Printing PowerPack LT
+
+addappid(275030)
+addappid(504120)
+addappid(504120,0,"f956d581d7f51240388578bc37ecbb59c0929ef48c58c2f02c4baa4ffa9cd0e1")
