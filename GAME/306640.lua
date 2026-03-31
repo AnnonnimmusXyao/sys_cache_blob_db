3768e316d2f2c11310b73dbdfd6b5dfdd504dccd
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 15:00:36 -0300

    Sync LUA 306640 a GAME

---
+++ GAME/306640.lua
@@ -0,0 +1,6 @@
+--game: Battle Academy 2: Eastern Front
+
+addappid(229000)
+addappid(306640)
+addappid(306641,0,"fcf265f36fd15aaca4b9aeedfd0fa6748cdacb8981afe7384a1e691f74b74873")
+addappid(345930)
