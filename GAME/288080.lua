3ab36c748316d233e407462152feb9d7019313eb
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:29:12 -0300

    Sync LUA 288080 a GAME

---
+++ GAME/288080.lua
@@ -0,0 +1,10 @@
+--game: BoneTown: The Second Coming Edition
+
+addappid(288080)
+addappid(288082,0,"5f22d0bf8b0e5f6406e83e97193e036d10f13596cce7c1efc60b2648ac4016e7")
+addappid(1668100)
+addappid(2466570)
+addappid(2963010)
+addappid(2963030)
+addappid(2963040)
+addappid(2963050)
