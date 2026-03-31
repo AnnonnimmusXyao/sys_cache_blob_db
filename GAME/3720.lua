c37dcbbc617c176191a3f32d1a7af209e9b83b24
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 03:39:17 -0300

    Sync LUA 3720 a GAME

---
+++ GAME/3720.lua
@@ -0,0 +1,5 @@
+--game: Evil Genius
+
+addappid(3720)
+addappid(3721,0,"aff71f7f506ee2628f3345671fa9fbf5f5810ef1c93bd73edcf9f6390f6dae7e")
+addappid(3722,0,"bb58332e0f4bfb20fec49d371621684dc49f9b5773b26d8876839bb62defd388")
