5ef18dd815ec75ee0828c74bda6e536b2579425e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 15:29:56 -0300

    Sync LUA 324810 a GAME

---
+++ GAME/324810.lua
@@ -0,0 +1,8 @@
+--game: TOXIKK™
+
+addappid(324810)
+addappid(324811,0,"a51015a3975c486d102bb417bc29cde384c86f3533c13e86d46073f361589d8e")
+addappid(503270)
+addappid(533490)
+addappid(533491)
+addappid(533492)
