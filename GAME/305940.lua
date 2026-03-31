0c77180d673a439ba16bb2522626d81efb6d4796
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:59:27 -0300

    Sync LUA 305940 a GAME

---
+++ GAME/305940.lua
@@ -0,0 +1,10 @@
+--game: Project AURA
+
+addappid(228982)
+addappid(228985)
+addappid(305940)
+addappid(305942,0,"fc172ca744fc01aee38bb15b1866352848d45bf74889660672a48f93932926ef")
+addappid(788980)
+addappid(788981)
+addappid(788982)
+addappid(788983)
