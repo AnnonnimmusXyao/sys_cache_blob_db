8b3b9030f2a36057421aab36a544f45a2c036db8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:49:44 -0300

    Sync LUA 300260 a GAME

---
+++ GAME/300260.lua
@@ -0,0 +1,5 @@
+--game: Planetoid Pioneers
+
+addappid(300260,0,"da518417f5e227dc052004a6671c3fdcf1d44e4b7fe3a4de27e3a0e2c1d21ba6")
+addappid(300261,0,"9cbe1c336c16a960bcf560f3267be1e5d7b2be1d998b3576706ba7f55fc5b8f1")
+addappid(667680)
