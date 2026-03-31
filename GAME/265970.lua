1c8718681d3551f5a48cc0ecf53b05ebb38bef46
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:58:55 -0300

    Sync LUA 265970 a GAME

---
+++ GAME/265970.lua
@@ -0,0 +1,9 @@
+--game: Worlds of Magic
+
+addappid(228983)
+addappid(228985)
+addappid(229004)
+addappid(265970)
+addappid(265971,0,"7ba28768f8ad18ddb92326afe05acf80949375daed5d3fa0c0726db21aef8619")
+addappid(369130)
+addappid(369131)
