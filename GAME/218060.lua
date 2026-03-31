d4a6d21e497d6f09401b697e9e3b0be4add7ef6e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 12:57:39 -0300

    Sync LUA 218060 a GAME

---
+++ GAME/218060.lua
@@ -0,0 +1,5 @@
+--game: BIT.TRIP Presents... Runner2: Future Legend of Rhythm Alien
+
+addappid(218060)
+addappid(228983)
+addappid(218061,0,"0fd877eaee236e3bd4396b1c904ae91a2267feefc7c146e70fda1d936210672d")
