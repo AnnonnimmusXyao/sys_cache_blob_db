7670efdeb469b34266e23fdd0f8073fb70c8f3c1
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 04:26:28 -0300

    Sync LUA 67000 a GAME

---
+++ GAME/67000.lua
@@ -0,0 +1,5 @@
+--game: The Polynomial - Space of the music
+
+addappid(67000)
+addappid(67001,0,"f38df57c21074b4a4b6078582b5250e544d62d28ee8ea62a054a335d80a1d619")
+addappid(67002,0,"9613dd7df58b72870c6e67d75f9bdf1a3a682d3e00bc635ac459cc5b02ddb8b4")
