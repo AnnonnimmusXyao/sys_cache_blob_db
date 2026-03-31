35630a68129e69f83ec5983fb28662c2190e368e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:22:24 -0300

    Sync LUA 283840 a GAME

---
+++ GAME/283840.lua
@@ -0,0 +1,9 @@
+--game: ALLTYNEX Second
+
+addappid(283840)
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(229000)
+addappid(283841,0,"58bc92effd3e703643951263ddfbaa44664819aef5a656edb843cd39468d7857")
