7fffc5f75329575e823bf44ea2acad393af35f04
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 12:56:01 -0300

    Sync LUA 215690 a GAME

---
+++ GAME/215690.lua
@@ -0,0 +1,8 @@
+--game: Zeno Clash 2
+
+addappid(215690)
+addappid(228983)
+addappid(229002)
+addappid(229020)
+addappid(229030)
+addappid(215691,0,"2150095b5a1788aa3d4255cd1dc7d33ba14b5ed88ac7316f1bd1f2648f3e7437")
