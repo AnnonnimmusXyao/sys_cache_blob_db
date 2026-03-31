801caf914f46bd463831504313c9446c60987fee
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:49:11 -0300

    Sync LUA 299740 a GAME

---
+++ GAME/299740.lua
@@ -0,0 +1,8 @@
+--game: Miscreated
+
+addappid(228983)
+addappid(228985)
+addappid(299740)
+addappid(299741,0,"374e2440029e9bcf547aaa4006a9bde588a14abf387104899ad1f756098b1df1")
+addappid(995380)
+addappid(1192820)
