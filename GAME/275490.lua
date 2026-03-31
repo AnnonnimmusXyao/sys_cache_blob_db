f7e4b43697b24f5fc8567bfd326684fd6e184f0d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:12:10 -0300

    Sync LUA 275490 a GAME

---
+++ GAME/275490.lua
@@ -0,0 +1,7 @@
+--game: Canyon Capers
+
+addappid(228985)
+addappid(229020)
+addappid(275490)
+addappid(275491,0,"71f91caf9287378e0d4724220823aabf22ca4137b21aa2d76ab68abf31cbe455")
+addappid(307390)
