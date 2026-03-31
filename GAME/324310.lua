e81cddf590396cd097704bfd12cf510077ed1c8d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 15:28:56 -0300

    Sync LUA 324310 a GAME

---
+++ GAME/324310.lua
@@ -0,0 +1,9 @@
+--game: Bus Simulator 16
+
+addappid(228985)
+addappid(324310)
+addappid(324311,0,"83f90bd9262bcdf8f300eb9a121175e65dadf6f15e85de9cd38b03b4c9295ae9")
+addappid(324312,0,"d4f02234ba0f374ba686a8982196e843dcb64d2c993522dfb30923bdb3584f8d")
+addappid(443430)
+addappid(563890)
+addappid(609880)
