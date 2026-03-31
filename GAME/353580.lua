ad8894e89b75e777f175702297402cd9713433a4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 20:45:26 -0300

    Sync LUA 353580 a GAME

---
+++ GAME/353580.lua
@@ -0,0 +1,8 @@
+--game: Rad Rodgers: World One
+
+addappid(228985)
+addappid(229004)
+addappid(229005)
+addappid(229033)
+addappid(353580)
+addappid(353581,0,"e4c4b34a754ccfdbaa42ead5195c333dde260959eb733cb880ea9e59bdefc4bc")
