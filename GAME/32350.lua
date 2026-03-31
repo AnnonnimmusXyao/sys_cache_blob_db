73da46a39cad37474f952920c93a4ea578eab940
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 04:04:15 -0300

    Sync LUA 32350 a GAME

---
+++ GAME/32350.lua
@@ -0,0 +1,5 @@
+--game: STAR WARS™ Starfighter™
+
+addappid(32350)
+addappid(32351,0,"33a38e803deeb8e9ee7b67089502525661fc9beb7d9d1a8345cb977f2a97fa1d")
+addappid(32356,0,"45fe2d69380362a00cbcfa5ed3fec57bc3207d25b6d438b34394382b106646a8")
