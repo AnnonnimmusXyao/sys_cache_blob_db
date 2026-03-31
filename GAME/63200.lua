b5d36c2736ea45cf88ffd06327bc6272b56477ae
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 04:24:33 -0300

    Sync LUA 63200 a GAME

---
+++ GAME/63200.lua
@@ -0,0 +1,8 @@
+--game: Monday Night Combat
+
+addappid(63200)
+addappid(63201,0,"f3b8d529ed4d2362ca168cd7600e13c9af87f12bb797c3430b90744d7163b4dd")
+addappid(63210)
+addappid(63211)
+addappid(63212)
+addappid(63213)
