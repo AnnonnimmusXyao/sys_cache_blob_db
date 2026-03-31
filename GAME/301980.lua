3844d96f235e994f873f8c26db4837bc623ae5c4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:52:50 -0300

    Sync LUA 301980 a GAME

---
+++ GAME/301980.lua
@@ -0,0 +1,6 @@
+--game: Disney Pirates of the Caribbean: At Worlds End
+
+addappid(301980)
+addappid(301981,0,"863c4b65c338841c9c20fd38184ac3100e3459ecf8dbafe5eafc41dfbb9a42f0")
+addappid(301982,0,"475f3647eb71fba3cc72efb5378e6dc1f03c106d1b248fbb1e9fe97dda3e4415")
+addappid(301984,0,"c65e9d10374bfd7132e1ec4a442b60ad3414857cad49b7adb40981768eba19dc")
