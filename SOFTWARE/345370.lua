d10b3dd19884e0e1a76fe34cb3e6d8195f66613c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 19:54:58 -0300

    Sync LUA 345370 a SOFTWARE

---
+++ SOFTWARE/345370.lua
@@ -0,0 +1,12 @@
+--game: TyranoBuilder Visual Novel Studio
+
+addappid(345370)
+addappid(345371,0,"ec67d7097f3cbd7b2b2fbfcbaadd805fb26f28f043eed4203619ca5773dd824d")
+addappid(2104500)
+addappid(2120780)
+addappid(2206370)
+addappid(2408850)
+addappid(2408851)
+addappid(2410380)
+addappid(2597720)
+addappid(2597730)
