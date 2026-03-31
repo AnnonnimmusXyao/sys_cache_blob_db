e3551a37f0c6258e98034faf94ec4f2e37b4583c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:15:56 -0300

    Sync LUA 239430 a GAME

---
+++ GAME/239430.lua
@@ -0,0 +1,7 @@
+--game: Q.U.B.E: Director's Cut
+
+addappid(228983)
+addappid(229004)
+addappid(229032)
+addappid(239430)
+addappid(239431,0,"2ad88d51cb19c74ab18375bdf87894e100bf95b24f1e8374a4c3fcedaacbfd55")
