bfacaf5cbaa081d055b2c4c181b47166a5b82b6f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 15:25:32 -0300

    Sync LUA 322130 a GAME

---
+++ GAME/322130.lua
@@ -0,0 +1,5 @@
+--game: Disney Princess: Enchanted Journey
+
+addappid(322130)
+addappid(322131,0,"3df2c7fe14f46271c6ea6dc8dacd4a35db979268a19dbdc11479109e2274168e")
+addappid(322132,0,"59b5c1117e879a79bf69f54c7de79c72ee8a18e910eb5d993b859a01040695d4")
