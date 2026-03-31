9c7444ebbbd96570c44f58b07e3e8e85618d359b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:58:29 -0300

    Sync LUA 265750 a GAME

---
+++ GAME/265750.lua
@@ -0,0 +1,7 @@
+--game: Secret Ponchos
+
+addappid(265750)
+addappid(265751,0,"9165cde6a5ee74a0fb34696daef05182d47526714b2df88c3d677a5b2feafb35")
+addappid(307470)
+addappid(307660)
+addappid(389750)
