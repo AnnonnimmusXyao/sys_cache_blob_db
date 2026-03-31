6101d8621252ae219676852f146fb3f791e8d7df
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:31:02 -0300

    Sync LUA 252970 a GAME

---
+++ GAME/252970.lua
@@ -0,0 +1,9 @@
+--game: McDROID
+
+addappid(228983)
+addappid(252970)
+addappid(252972,0,"8a6bd3c38a696b017b9f7122c9efdd1a5006c41919003ebea37b7a6089a3d8a3")
+addappid(318010)
+addappid(368330)
+addappid(400950)
+addappid(401110)
