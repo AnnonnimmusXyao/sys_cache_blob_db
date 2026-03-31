b266679a26cb5667a3baa919e834b897ad430cd3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:46:17 -0300

    Sync LUA 297920 a GAME

---
+++ GAME/297920.lua
@@ -0,0 +1,8 @@
+--game: Space Colony: Steam Edition
+
+addappid(297920)
+addappid(228981)
+addappid(228983)
+addappid(229002)
+addappid(297921,0,"97de666f7c5fba4db8f6fb547932b55f2f7870eed43def1426ad6a60e1f98b95")
+addappid(297928,0,"d81e005ee8f4b7dc906d274550f9807ce4edb3a91e423f1280934147b29a42ed")
