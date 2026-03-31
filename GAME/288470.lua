e79c06274ed94ccff4aa99308685057a6f6f5ca3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:29:41 -0300

    Sync LUA 288470 a GAME

---
+++ GAME/288470.lua
@@ -0,0 +1,8 @@
+--game: Fable Anniversary
+
+addappid(228983)
+addappid(288470)
+addappid(288471,0,"5795bc052b2a0d53947c274a5da3926e6ba122feed6400c5f833e323177dfea9")
+addappid(315150)
+addappid(315151)
+addappid(319120,0,"0584d850aa452f1020491d01f0981832d8525f47eadf9561a3d3ae45932f9eba")
