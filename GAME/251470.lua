1eb206f06d5990e68265f81bd80f67c5aba77eb1
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:28:03 -0300

    Sync LUA 251470 a GAME

---
+++ GAME/251470.lua
@@ -0,0 +1,7 @@
+--game: TowerFall Ascension
+
+addappid(229002)
+addappid(229012)
+addappid(251470)
+addappid(251471,0,"d0f40bbe2a311a5daf79c576307113a685b556e563ee89409ccdb0be66feac55")
+addappid(337840,0,"19c292db638ea335b03ec9fca1e7e69140e28b76c4e960976840e744fc6d3953")
