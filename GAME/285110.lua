93c0749ce92361577adf326c688632a5bb22aaa3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:24:41 -0300

    Sync LUA 285110 a GAME

---
+++ GAME/285110.lua
@@ -0,0 +1,9 @@
+--game: GearCity
+
+addappid(285110)
+addappid(228981)
+addappid(228982)
+addappid(228985)
+addappid(228987)
+addappid(229020)
+addappid(285116,0,"7ed3ec854999242f3bc6b69713a026c30a0ad3bd7f8c0805898bc43a462cdf4f")
