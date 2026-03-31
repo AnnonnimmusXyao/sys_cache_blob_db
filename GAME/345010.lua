3dbc5d248496ef7637dcb9d9646a9a00201d68c0
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 19:49:54 -0300

    Sync LUA 345010 a GAME

---
+++ GAME/345010.lua
@@ -0,0 +1,9 @@
+--game: Ascent - The Space Game
+
+addappid(345010)
+addappid(1591510)
+addappid(345050)
+addappid(391830)
+addappid(391840)
+addappid(345012,0,"8e8e247a4b3d7e625e2902b6ba5c3bbbfaf906f5e7a4355ac0bab23c2ebd0e2c")
+addappid(345015,0,"4e2461403804a40b9ae51b9c6e840252fcddd2db172f37fc1a8abd3c81ec774a")
