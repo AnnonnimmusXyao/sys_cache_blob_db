33206d5e64f0dbdf431f1e40fbdbb3948a86e88b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:12:48 -0300

    Sync LUA 236150 a GAME

---
+++ GAME/236150.lua
@@ -0,0 +1,10 @@
+--game: Starpoint Gemini 2
+
+addappid(236150)
+addappid(229020)
+addappid(229031)
+addappid(236151,0,"09d9d963baec05a786f0062d26b1d8be8cfd8326fa7c8db7f88b72cdb8912e08")
+addappid(236169,0,"4c519a6fabfb76e3800f8812de12787e375ad95aef586e492303da9cfc283d47")
+addappid(345870,0,"7a9f852da2abedf09cc6d539855c91fb3ea93b4f1eb38f0deacdfe29c5a8dc08")
+addappid(371060,0,"ec1d6aa48bfe14b951633b865a2818ee425a0a3a686c4ee0d067d144743289ab")
+addappid(411420,0,"daadfec9258ca7a29edc76a0f5473ea81b9cb62f1e53dafe11df1ac493faa640")
