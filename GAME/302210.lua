b43ebe7894c4f73ad7ea3844db5eee15c8a2b560
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:53:25 -0300

    Sync LUA 302210 a GAME

---
+++ GAME/302210.lua
@@ -0,0 +1,5 @@
+--game: Curse: The Eye of Isis
+
+addappid(302210)
+addappid(302211,0,"2ea0e030943adef9eb801a5d18d2bdc10a77cfef7b0e09a70b2438ac2118b310")
+addappid(302212,0,"cbb06f6fe44d2d72130fef389c0bfa6d55c7261836f87c3d348a290afa14f42e")
