6579a40d50d50817d719cefb38aaa5a0a261576a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 19:37:10 -0300

    Sync LUA 343090 a GAME

---
+++ GAME/343090.lua
@@ -0,0 +1,7 @@
+--game: SimpleRockets
+
+addappid(228985)
+addappid(229003)
+addappid(229020)
+addappid(343090)
+addappid(343091,0,"e593b75c3adb8763394ff863aa778ec01f43ef296b8acdecd911ec812fde7698")
