4688af0d275a89d67cc69b237954c852d1b84391
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:35:36 -0300

    Sync LUA 257350 a GAME

---
+++ GAME/257350.lua
@@ -0,0 +1,9 @@
+--game: Baldur's Gate II: Enhanced Edition
+
+addappid(257350)
+addappid(228982)
+addappid(229020)
+addappid(257351,0,"88e74b749a3e454dcff08e818290a58bcb741800f002aa0e40cfdca9d364f5c8")
+addappid(257352,0,"e3455666047d6559b851052b68f9f73041bbffb76ee74ba8cb93b1f0ccc0e5fe")
+addappid(257353,0,"ef916ff1549de04b067c8cc39c4ce5ae4ebc1a8fb410c27b35ca856ba34b883d")
+addappid(257354,0,"b3c8a168d08424cbdab3eb502d4a177b32515e334c705d3257dfa8630073a818")
