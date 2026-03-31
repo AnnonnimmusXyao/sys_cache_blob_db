8972f05f358b98e2e66f36c5385eae36d7c9e623
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 14:35:51 -0300

    Sync LUA 292000 a GAME

---
+++ GAME/292000.lua
@@ -0,0 +1,7 @@
+--game: No More Room in Hell 2
+
+addappid(228989)
+addappid(228990)
+addappid(292000)
+addappid(292001,0,"2bb3dfcc7245e9a85e6adcb82c91257dfd6e3ca66bdc188476820020395cc812")
+addappid(292002)
