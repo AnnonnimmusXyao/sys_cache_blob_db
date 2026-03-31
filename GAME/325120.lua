571ae49d22c3aa825d7d92e2adcab4822fe203da
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 15:30:09 -0300

    Sync LUA 325120 a GAME

---
+++ GAME/325120.lua
@@ -0,0 +1,5 @@
+--game: Notch - The Innocent LunA: Eclipsed SinnerS
+
+addappid(325120)
+addappid(325121,0,"ff5850b3044ba7f4a42d627c542089bc7b0b5a7cdd621f6c4e0725c239baecc1")
+addappid(325122,0,"b316136e5cbcf628632282149973a128ef209a75da9d7991d3d583063b87d65f")
