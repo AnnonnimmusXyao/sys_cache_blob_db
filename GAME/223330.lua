95b39fea40db3a04fc1787c296dd38084d05c76a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 13:01:47 -0300

    Sync LUA 223330 a GAME

---
+++ GAME/223330.lua
@@ -0,0 +1,9 @@
+--game: Sea Dogs: To Each His Own
+
+addappid(223330)
+addappid(223332,0,"2da3ab8a20d6c98aafb7eca66059a33c0301df0d258409518ba2372ab21bfca5")
+addappid(240480)
+addappid(452520)
+addappid(452521)
+addappid(452522)
+addappid(452523)
