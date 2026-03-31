a181ffda0a40ffe3f8f920543c21b2c302e606f3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 04:02:36 -0300

    Sync LUA 29720 a GAME

---
+++ GAME/29720.lua
@@ -0,0 +1,14 @@
+--game: Guild Wars® Reforged
+
+addappid(29540)
+addappid(29541,0,"7f93d15c7db86244f86cecadc8137f20f72eddd640ea65297e2f067d0471dde6")
+addappid(29560)
+addappid(29570)
+addappid(29580)
+addappid(29600)
+addappid(29610)
+addappid(29620)
+addappid(29630)
+addappid(29700)
+addappid(29710)
+addappid(29720)
