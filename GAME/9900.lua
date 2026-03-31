21689dbad96e20146385e1288c0b4dab7a70c750
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 03:46:36 -0300

    Sync LUA 9900 a GAME

---
+++ GAME/9900.lua
@@ -0,0 +1,12 @@
+--game: Star Trek Online
+
+addappid(9900)
+addappid(9905)
+addappid(9906)
+addappid(9907)
+addappid(9908)
+addappid(9910)
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+addappid(237680)
+addappid(237700)
+addappid(325170)
