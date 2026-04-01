195edbc2fd57ff30bfe25b8b3d6ecbc8a2465774
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 23:42:36 -0300

    Sync LUA 385130 a GAME

---
+++ GAME/385130.lua
@@ -0,0 +1,14 @@
+--game: DvDrum, Ultimate Drum Simulator!
+
+addappid(229007)
+addappid(385130)
+addappid(385131,0,"ab96518f47952d574a3a62e4013b23981d03bb9f10c761b41d6d81207ef9da74")
+addappid(676250)
+addappid(676251)
+addappid(676252)
+addappid(676253)
+addappid(676254)
+addappid(676255)
+addappid(676256)
+addappid(676257)
+addappid(676258)
