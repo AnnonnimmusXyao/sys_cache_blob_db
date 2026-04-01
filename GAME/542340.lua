06f40aa157c59a56bdc6d19c64495f76fd06ece4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 14:42:45 -0300

    Sync LUA 542340 a GAME

---
+++ GAME/542340.lua
@@ -0,0 +1,9 @@
+--game: Slingshot people
+
+addappid(228987)
+addappid(229005)
+addappid(229012)
+addappid(229020)
+addappid(229030)
+addappid(542340)
+addappid(542341,0,"39e549d11a55939b9115950aa08e16929903035cd98a66bae4444268c5a4eb5d")
