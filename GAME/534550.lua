b0d95098ff05ef8499f0e27d1a75f9a86aec7e08
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 13:29:47 -0300

    Sync LUA 534550 a GAME

---
+++ GAME/534550.lua
@@ -0,0 +1,7 @@
+--game: Guacamelee! 2
+
+addappid(228985)
+addappid(534550)
+addappid(534551,0,"532dc45b5876ab2ab47aee412f56eb4a7bc4cfe3bfcb95bc2ca43ab352ee8462")
+addappid(942540)
+addappid(944350)
