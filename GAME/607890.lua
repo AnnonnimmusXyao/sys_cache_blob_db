ef21d1f8541d5febdd1f12998321fa9a1e38e81b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 17:34:15 -0300

    Sync LUA 607890 a GAME

---
+++ GAME/607890.lua
@@ -0,0 +1,5 @@
+--game: Sword Art Online: Hollow Realization Deluxe Edition
+
+addappid(228985)
+addappid(607890)
+addappid(607891,0,"2819352a1d4053633e8f2329f90f4a57b99468de507727499988a276babeb3c7")
