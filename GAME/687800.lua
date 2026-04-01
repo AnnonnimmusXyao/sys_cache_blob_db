436448633f593ac2f07fb64605900a3d0398b2a8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 20:46:49 -0300

    Sync LUA 687800 a GAME

---
+++ GAME/687800.lua
@@ -0,0 +1,10 @@
+--game: Heads Will Roll: Reforged
+
+addappid(687800)
+addappid(687801,0,"c2ca504542832cb9bfbb6785fafb8bd15bf790b11ca42c4c627bebcdbdf13e9c")
+addappid(2517510)
+addappid(2618340)
+addappid(2747080)
+addappid(2850080)
+addappid(3172940)
+addappid(3857000)
