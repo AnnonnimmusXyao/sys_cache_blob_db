2595f9570e70f3f84b7f49c12bd7f86ba30c0f6f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 08:26:57 -0300

    Sync LUA 435040 a GAME

---
+++ GAME/435040.lua
@@ -0,0 +1,6 @@
+--game: Incredible Dracula: Chasing Love Collector's Edition
+
+addappid(435040)
+addappid(435041,0,"fc01e1d49b0a3a62935e621ba894fb0f0fa3050a39c1a903da1af1f2c7b363b5")
+addappid(435042,0,"aaafd31eb5311b19a9bf64f4453e359949039f5d500d44dce9a5ecd20b20e53a")
+addappid(435043,0,"1f2520b7197419d53f88da7c3076d6616e7a06450395a04dd129a767dfd6e413")
