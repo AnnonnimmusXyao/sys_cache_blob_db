46a9db86690b4500428d59d71fbe1e3f9872bef5
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 16:42:13 -0300

    Sync LUA 589360 a GAME

---
+++ GAME/589360.lua
@@ -0,0 +1,12 @@
+--game: Ni no Kuni™ II: Revenant Kingdom
+
+addappid(589360)
+addappid(589361,0,"9402d58bea96df572890e2739abfdddef9706b87e79a208795d15596a74e4e3f")
+addappid(689800)
+addappid(689810)
+addappid(689860)
+addappid(804740)
+addappid(804741)
+addappid(804742)
+addappid(804750)
+addappid(898570)
