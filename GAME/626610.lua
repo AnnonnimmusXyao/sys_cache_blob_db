ae917d6dec0817d4f9236f77987c4558dba5b4af
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 18:08:18 -0300

    Sync LUA 626610 a GAME

---
+++ GAME/626610.lua
@@ -0,0 +1,8 @@
+--game: TT Isle of Man: Ride on the Edge
+
+addappid(228983)
+addappid(228987)
+addappid(626610)
+addappid(626611,0,"5b20b9f8d584b1d834bd87b6cba07624173ab6764bff60dd387601bc4cc7e9a5")
+addappid(756750)
+addappid(814060)
