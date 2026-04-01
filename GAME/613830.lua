0cf406476d3d6d1b96f917350e1fd4a5beca2a09
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 17:45:38 -0300

    Sync LUA 613830 a GAME

---
+++ GAME/613830.lua
@@ -0,0 +1,8 @@
+--game: CHRONO TRIGGER®
+
+addappid(613830)
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(613831,0,"1e8869a6b81930b7548235c629efb0dc53a8a8f5a6eb5f7b8524bbca953c8141")
