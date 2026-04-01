d61b37bd1fd6ace9a024969b0ffc8acb41cde6b6
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 10:20:29 -0300

    Sync LUA 498720 a GAME

---
+++ GAME/498720.lua
@@ -0,0 +1,5 @@
+--game: Inklings
+
+addappid(498720)
+addappid(498721,0,"15e9da657dd35da5f6b9fb7a49ffcc8ededd4a1e0dd135128ebf8a0c8c106fe1")
+addappid(498723,0,"9ee258e46b5ef983b252de2d44c22bec820442ed4aa33c2246c36acb6813fb8b")
