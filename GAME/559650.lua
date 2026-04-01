753c5409ee9271068702188a169969986f679749
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 15:36:37 -0300

    Sync LUA 559650 a GAME

---
+++ GAME/559650.lua
@@ -0,0 +1,12 @@
+--game: Witch It
+
+addappid(559650)
+addappid(559651,0,"a7187005733a13b0010375c98d113e6ad44bb945f73e3f9d582dc81f88a520ec")
+addappid(639750)
+addappid(645870)
+addappid(3212440)
+addappid(3212450)
+addappid(3212460)
+addappid(3212470)
+addappid(3311600)
+addappid(3365350)
