9e9d879dea08d1f22e00469de1bb9e832e624157
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 16:59:37 -0300

    Sync LUA 597180 a GAME

---
+++ GAME/597180.lua
@@ -0,0 +1,10 @@
+--game: Old World
+
+addappid(597180)
+addappid(597181,0,"44832d82ba73423f7ad8811dab24c4890d02a444b7c94d2d4c73e7e66360a704")
+addappid(1952840)
+addappid(2261870)
+addappid(2587410)
+addappid(2587420)
+addappid(2587430)
+addappid(3406150)
