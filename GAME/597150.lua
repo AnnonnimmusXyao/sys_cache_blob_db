44f155dc530206e96cca93a7e3326ffe1d28db62
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 16:59:29 -0300

    Sync LUA 597150 a GAME

---
+++ GAME/597150.lua
@@ -0,0 +1,8 @@
+--game: Hyper Knights
+
+addappid(597150)
+addappid(597151,0,"cf0d6c456b6d6d42eb0d576381c53c864676928b4ef48420f29ba5dde109472d")
+addappid(694200)
+addappid(698470)
+addappid(703671)
+addappid(703770)
