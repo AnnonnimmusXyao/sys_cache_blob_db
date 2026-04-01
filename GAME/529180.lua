39e4cd6eeb5db764948b5cdd7dbc8165d778b645
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 13:16:21 -0300

    Sync LUA 529180 a GAME

---
+++ GAME/529180.lua
@@ -0,0 +1,9 @@
+--game: Dark and Light
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(529180)
+addappid(529181,0,"decc2a0b636b44fe0b92160c5a67c834a3a1f59feb51c76a7db36c9ffefde258")
+addappid(793070)
