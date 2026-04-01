4fa5e91be5b12f5b1323795e7cadfe0b477e2563
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 19:04:22 -0300

    Sync LUA 658160 a GAME

---
+++ GAME/658160.lua
@@ -0,0 +1,8 @@
+--game: ClickRaid
+
+addappid(658160)
+addappid(658161,0,"30e85dc01b8857acb9faf437bd7d02cacc960f964dcb745cf8df67efbeb26357")
+addappid(658910)
+addappid(738870)
+addappid(757280)
+addappid(804310)
