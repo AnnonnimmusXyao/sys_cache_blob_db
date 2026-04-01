f7b55ece19dcd8fdedb1e80cfb2c44d0bd59bc16
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 12:55:35 -0300

    Sync LUA 520910 a GAME

---
+++ GAME/520910.lua
@@ -0,0 +1,7 @@
+--game: Kokurase Episode 1
+
+addappid(520910)
+addappid(520911,0,"12ae8a1a38413ef877ebed3350dace701ca0777b6138412d3ea88140b0733143")
+addappid(520914,0,"a0cff92919b05d098ed27fe02f0b63ba714bd1e4fd348db0a0f8aa2dc4278796")
+addappid(558640)
+addappid(558641)
