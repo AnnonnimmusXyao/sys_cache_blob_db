23f6c354996dd6fe56e3f93da51af2e1b2e0ff46
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 01:06:28 -0300

    Sync LUA 423490 a GAME

---
+++ GAME/423490.lua
@@ -0,0 +1,8 @@
+--game: Air Missions: HIND
+
+addappid(228982)
+addappid(423490)
+addappid(423491,0,"84ee169cd02cd52105d3777ec59eda5fb7bf448b9a7229d09405d4dd5d196cad")
+addappid(549970)
+addappid(549971)
+addappid(562820)
