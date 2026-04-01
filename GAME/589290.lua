1da05a86c37865871d755c18881c439f6883705d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 16:42:03 -0300

    Sync LUA 589290 a GAME

---
+++ GAME/589290.lua
@@ -0,0 +1,15 @@
+--game: Holdfast: Nations At War
+
+addappid(589290)
+addappid(589291,0,"a7c1417721600265bba5c83ede099119af0a65f1e2f3b341cc26c5cc7987b7b3")
+addappid(1243770)
+addappid(1324000)
+addappid(1415510)
+addappid(1489800)
+addappid(1906770)
+addappid(1906771)
+addappid(1960210)
+addappid(1960220)
+addappid(2107250)
+addappid(2692480)
+addappid(3320400)
