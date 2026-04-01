7816bf0b50e3193c140c61d50b421f5857d20127
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 22:07:57 -0300

    Sync LUA 375530 a GAME

---
+++ GAME/375530.lua
@@ -0,0 +1,6 @@
+--game: Football, Tactics & Glory
+
+addappid(375530)
+addappid(375531,0,"531ccfe7bb71efdd67df507ba75737ccd4b93307333854b86f6d4274b354b24a")
+addappid(1293080,0,"76855d18960ffdf8f90229b55dcd4bfd845ee9b3d53dba8cf6b1034d45550e60")
+addappid(1650120)
