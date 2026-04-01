fd79e867312b2dccac0f06f5e74e8b77ad7acff4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 21:01:11 -0300

    Sync LUA 355860 a GAME

---
+++ GAME/355860.lua
@@ -0,0 +1,7 @@
+--game: Albino Lullaby: Episode 1
+
+addappid(355860)
+addappid(355861,0,"802dd364d97acee882c7767b4285fe1787cb7ec35c1a1f15efca4c1bd74abfe1")
+addappid(403260)
+addappid(403320)
+addappid(403330)
