9874e394ad4f9c9a87ec0ea801f5bb4039d20875
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 15:20:28 -0300

    Sync LUA 556740 a GAME

---
+++ GAME/556740.lua
@@ -0,0 +1,6 @@
+--game: LEGRAND LEGACY: Tale of the Fatebounds
+
+addappid(229004)
+addappid(556740)
+addappid(556741,0,"766f62e355be6178df7c872df2e6c0144932e003f4e6e65224ad6f52b5bbe122")
+addappid(793910)
