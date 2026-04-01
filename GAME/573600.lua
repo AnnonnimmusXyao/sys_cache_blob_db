4c5fda974800a5dcdb5e25afe383edc33cf77d5c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 16:06:21 -0300

    Sync LUA 573600 a GAME

---
+++ GAME/573600.lua
@@ -0,0 +1,6 @@
+--game: Think of the Children
+
+addappid(573600)
+addappid(573603,0,"f5c3ed10a74949d732037403a6d6dae47e48b72977720bd4c129d4f3b030a1be")
+addappid(573604,0,"1bc4ea787d582dfe1e183df09dd56218f97ef43f04c4eefb47d35d0f29951921")
+addappid(735300)
