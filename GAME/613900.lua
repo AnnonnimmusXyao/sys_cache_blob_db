7783ee69d715a2da36924ab96b97a8125436b7bc
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 17:45:52 -0300

    Sync LUA 613900 a GAME

---
+++ GAME/613900.lua
@@ -0,0 +1,9 @@
+--game: Museum of Other Realities
+
+addappid(613900)
+addappid(1348090)
+addappid(1351570)
+addappid(1351571)
+addappid(1351572)
+addappid(1568390,0,"e4e098d6a7cd49716a951c4b838a18166cea7baf02572f0b6d11a8088ee2c436")
+addappid(1759370,0,"96a85621fdd9c38835b57e5e22b71e34c53672ee0d876ceaec36015a2ae5144f")
