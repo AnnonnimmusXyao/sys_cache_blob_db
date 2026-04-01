9dfbb60e6d7beae05b3ab86fec052511dd5efede
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 18:47:29 -0300

    Sync LUA 651380 a GAME

---
+++ GAME/651380.lua
@@ -0,0 +1,6 @@
+--game: BANG! BANG! Totally Accurate Redneck Simulator
+
+addappid(651380)
+addappid(228987)
+addappid(229006)
+addappid(651381,0,"2878985f7ecdbad76b3e4e95b05d8df46330a27eb81cce9da5465d395f3f3507")
