b8e0ec0929480023f82adcbc97f8737197778da9
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 20:13:49 -0300

    Sync LUA 684450 a GAME

---
+++ GAME/684450.lua
@@ -0,0 +1,11 @@
+--game: Surviving the Aftermath
+
+addappid(684450)
+addappid(684451,0,"ecae554f887c65bddfa54bffde64e0fe5c4315fb7bd3421186694d4b3555b45a")
+addappid(1436890)
+addappid(1600410)
+addappid(1766280)
+addappid(1803780)
+addappid(1803781)
+addappid(1803782)
+addappid(2146190)
