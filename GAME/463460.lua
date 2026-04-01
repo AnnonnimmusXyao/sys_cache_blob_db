ad6d2e39fa7b945693da4217b48bce222db9f27d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 09:19:21 -0300

    Sync LUA 463460 a GAME

---
+++ GAME/463460.lua
@@ -0,0 +1,5 @@
+--game: Resette's Prescription ~Book of memory, Swaying scale~
+
+addappid(463460)
+addappid(475420,0,"7650849a1e146156b7935d08feac48eaa1713cdda223eb6f22403413f2e655ab")
+addappid(463463,0,"26514db4a2832262d77934c9b4973355053be28cb44736974abaa01daa424df3")
