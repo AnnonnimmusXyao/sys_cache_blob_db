9c131962640864ebb3e63b13bdd29fe4e4800fb6
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 18:04:05 -0300

    Sync LUA 623700 a GAME

---
+++ GAME/623700.lua
@@ -0,0 +1,11 @@
+--game: Rec Center Tycoon - Management Simulator
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(228987)
+addappid(229000)
+addappid(229002)
+addappid(623700)
+addappid(623701,0,"21ec65e6822a0f15e3d861374eae7d079c2cff329e1a286772667c91d307c94c")
