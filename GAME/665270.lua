1ab1a7cde6e8835d289debfeeaeeddcf918e0159
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 19:23:10 -0300

    Sync LUA 665270 a GAME

---
+++ GAME/665270.lua
@@ -0,0 +1,5 @@
+--game: 弹幕音乐绘 ～风雷幻奏曲～ / Barrage Musical  ~A Fantasy of Tempest~
+
+addappid(229002)
+addappid(665270)
+addappid(665271,0,"e86af823b6dd5b24c355b943120b03c2e6ea47c9fc93bb147b1b03bf1a1ef521")
