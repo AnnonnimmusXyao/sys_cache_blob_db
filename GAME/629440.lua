9b4120f80560ed8bbe9c98c9214c9f6ba825251a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 18:14:10 -0300

    Sync LUA 629440 a GAME

---
+++ GAME/629440.lua
@@ -0,0 +1,9 @@
+--game: Graze Counter
+
+addappid(629440)
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(229000)
+addappid(629441,0,"00917c305042b744d650fe62e205e13d6b9c4fb87b8d77b13ee3db19fc2dfa84")
