483a3d90527f37de23c361330ce71e8f24ff7947
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 19:09:57 -0300

    Sync LUA 660160 a GAME

---
+++ GAME/660160.lua
@@ -0,0 +1,10 @@
+--game: Field of Glory II
+
+addappid(660160)
+addappid(660161,0,"8e53a2dafa5479621f2fd5dcafa2fc77a57946667633dd66a64f4fe6d0c00709")
+addappid(737740)
+addappid(754790)
+addappid(801920)
+addappid(876360)
+addappid(1004260)
+addappid(2110620)
