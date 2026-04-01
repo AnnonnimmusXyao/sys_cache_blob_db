20f3e2dba1ffbf6bd102482a84bd39897e99fd06
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 09:23:29 -0300

    Sync LUA 465490 a GAME

---
+++ GAME/465490.lua
@@ -0,0 +1,7 @@
+--game: Stellar Tactics
+
+addappid(465490)
+addappid(228983)
+addappid(228985)
+addappid(229020)
+addappid(465492,0,"ac918fe0ba06171c5c4e54f51907d68cc905204dd68ff689929ba1af3f3435e4")
