9b5ed80bf8f42e2f8417884a1983d1ca58ee9bfe
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 12:11:47 -0300

    Sync LUA 502370 a GAME

---
+++ GAME/502370.lua
@@ -0,0 +1,7 @@
+--game: Warhammer 40,000: Sanctus Reach
+
+addappid(502370)
+addappid(502371,0,"19996d49b389b4203817159379b7a56684311727a0525fdd45a7d1e8ee924c3d")
+addappid(623140)
+addappid(716660)
+addappid(841150)
