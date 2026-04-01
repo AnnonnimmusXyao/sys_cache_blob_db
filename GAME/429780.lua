dcc56734922f3b4a417160eab937a8bd3c55d4b5
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 08:16:06 -0300

    Sync LUA 429780 a GAME

---
+++ GAME/429780.lua
@@ -0,0 +1,7 @@
+--game: Bunny Hop League
+
+addappid(228987)
+addappid(229005)
+addappid(429780)
+addappid(429781,0,"90f79db2fa29bd102f7e36b317b429db2320413f567e9abf290a9323612fd1a8")
+addappid(640670)
