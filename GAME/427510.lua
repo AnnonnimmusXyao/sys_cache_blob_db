8e32251eeb74fa2e36720424c2aed661ac81968e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 08:10:36 -0300

    Sync LUA 427510 a GAME

---
+++ GAME/427510.lua
@@ -0,0 +1,9 @@
+--game: Way of the Samurai 3
+
+addappid(228981)
+addappid(229033)
+addappid(427510)
+addappid(427511,0,"06a40ec453b38b8756a9927dc2ac1ef5a76d779fd9e548649a385029a56a4fab")
+addappid(428310)
+addappid(428311)
+addappid(428312)
