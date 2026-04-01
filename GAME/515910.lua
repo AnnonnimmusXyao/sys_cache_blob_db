016cd844b29096caed1fde5319bca0c81864604f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 12:46:26 -0300

    Sync LUA 515910 a GAME

---
+++ GAME/515910.lua
@@ -0,0 +1,7 @@
+--game: Hooligan Vasja
+
+addappid(228983)
+addappid(515910)
+addappid(515911,0,"51ec649d7200b53b9b9fc98d54808e635c7c27cd494f47113800982f47aabd6c")
+addappid(542800)
+addappid(560910)
