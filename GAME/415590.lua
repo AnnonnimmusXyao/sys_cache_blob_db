1fc2e7972c132c45b90ce855bbdd9a9996efb2f2
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 00:54:51 -0300

    Sync LUA 415590 a GAME

---
+++ GAME/415590.lua
@@ -0,0 +1,8 @@
+--game: Earthfall
+
+addappid(228985)
+addappid(229033)
+addappid(415590)
+addappid(415591,0,"aea7b15d78d3bf150bc12d1c108701613ef7280234d444654423ddc03b0dbe35")
+addappid(578340)
+addappid(874800)
