ad8da64cdc8ded0ce8b347551c54f14ac52b6e46
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 08:52:38 -0300

    Sync LUA 449500 a GAME

---
+++ GAME/449500.lua
@@ -0,0 +1,6 @@
+--game: Hush Hush - Unlimited Survival Horror
+
+addappid(449500)
+addappid(449501,0,"6f8af4a3644777a53152dffbd8abbe43510baa55e79f6cadd398ceb2ace7159f")
+addappid(449502,0,"281f8db4753574f8d27aa9f2e4423c5981797a041b55d3a01585982c38260a5e")
+addappid(449503,0,"2881b210adbda1c3cddb51753619f75bd17d337ab67f7ffab525b951d1a65e1e")
