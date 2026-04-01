2cf88b590334c6308f6e10c59cc973616ce4d1c1
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 01:02:58 -0300

    Sync LUA 420560 a GAME

---
+++ GAME/420560.lua
@@ -0,0 +1,7 @@
+--game: Firefighting Simulator - The Squad
+
+addappid(229005)
+addappid(420560)
+addappid(420561,0,"b9c305a15d2bf9b3391d9655749df9c99e797b311fdb5f4d137c7429732a9590")
+addappid(420562)
+addappid(420563)
