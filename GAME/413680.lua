56726b623c219e6ba170db62313e41c8b58406ab
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 00:51:57 -0300

    Sync LUA 413680 a GAME

---
+++ GAME/413680.lua
@@ -0,0 +1,9 @@
+--game: Statues
+
+addappid(228985)
+addappid(229003)
+addappid(229004)
+addappid(229005)
+addappid(229033)
+addappid(413680)
+addappid(413681,0,"e59eda648e32b3eb5ed31652c4fac34117c8916a4a19540440c78494cae7ea54")
