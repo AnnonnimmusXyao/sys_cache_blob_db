d206a98b63ecd3537525f45580ea9306911e2307
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 16:28:22 -0300

    Sync LUA 582980 a GAME

---
+++ GAME/582980.lua
@@ -0,0 +1,7 @@
+--game: Castle of Shikigami
+
+addappid(229005)
+addappid(229020)
+addappid(582980)
+addappid(582981,0,"1977cd7dc04d833f2a8c6e19defae805f7cec74101626c67f91edd3ad356d14c")
+addappid(675390)
