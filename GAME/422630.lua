7712cdd829f9af5d0b28546fd841c0e82556ec2f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 01:05:33 -0300

    Sync LUA 422630 a GAME

---
+++ GAME/422630.lua
@@ -0,0 +1,5 @@
+--game: Metaverse Construction Kit
+
+addappid(422630)
+addappid(422631,0,"44d0bf4dcc82cb2a1a8ff49cb24e3a48cfdbed26f03bc0e3538eed4d46058def")
+addappid(422633,0,"f7e6ea8ae8ccf7164d3d2f9ba93263b196e2d66a32c5e8995c50806fbc898e56")
