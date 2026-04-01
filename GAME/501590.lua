139cad719cf1e3415479421fd5055e3378e974ac
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 12:09:50 -0300

    Sync LUA 501590 a GAME

---
+++ GAME/501590.lua
@@ -0,0 +1,7 @@
+--game: Bulletstorm: Full Clip Edition
+
+addappid(228983)
+addappid(228985)
+addappid(501590)
+addappid(501591,0,"72d72f0c658a2ba05a59dd33e4b6d50667cf1fe8606fe0f0ef2c5d330acd75a5")
+addappid(561730)
