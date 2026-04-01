ea569df480ce0953aeaaa48068dbd05d23b3ca80
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 21:10:40 -0300

    Sync LUA 357910 a GAME

---
+++ GAME/357910.lua
@@ -0,0 +1,6 @@
+--game: QuestEvent
+
+addappid(357910)
+addappid(357915,0,"fede693edfc8a56128b168cd7a8949ceac28d6ca7cfedebd2a1844369aaf8630")
+addappid(368480,0,"5f3db8685c8b1be7d69ce8b90d1e4fc07b0949cb6dc6f9465b669fa976468798")
+addappid(877500)
