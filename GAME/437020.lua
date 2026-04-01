8359ea1864d53f50b4d47340e82b8da6b3a2ce4d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 08:30:13 -0300

    Sync LUA 437020 a GAME

---
+++ GAME/437020.lua
@@ -0,0 +1,9 @@
+--game: WITCH-BOT MEGLILO
+
+addappid(437020)
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(229000)
+addappid(437021,0,"fd6ab1a6e6fb780c4d07d9abd60f1933e5113e051497392f81f0c5eea25e160f")
