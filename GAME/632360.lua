42a5a9bbbb03126c52237712f016065b4f30a262
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 18:19:41 -0300

    Sync LUA 632360 a GAME

---
+++ GAME/632360.lua
@@ -0,0 +1,7 @@
+--game: Risk of Rain 2
+
+addappid(632360)
+addappid(632361,0,"074d702b52f219d0d3440d09a1ded40e4ab4468c8a77b96dd97b1467a3e1a9f0")
+addappid(1607890)
+addappid(2306620)
+addappid(2781620)
