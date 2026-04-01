c54bcb4be2342d1d3b905ea61f1d02b922cafe08
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 00:05:56 -0300

    Sync LUA 391280 a GAME

---
+++ GAME/391280.lua
@@ -0,0 +1,5 @@
+--game: Epic Manager - Create Your Own Adventuring Agency!
+
+addappid(391280)
+addappid(391281,0,"2057a5fcbd4645b685e37d47c788c8759ad05845d85d9c690c5e6c29dd9a57ea")
+addappid(502590)
