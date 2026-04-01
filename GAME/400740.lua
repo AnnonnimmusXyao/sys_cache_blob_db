908182021eb5577244b8b6799b1debb73e4d0564
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 00:28:55 -0300

    Sync LUA 400740 a GAME

---
+++ GAME/400740.lua
@@ -0,0 +1,7 @@
+--game: VERGE:Lost chapter
+
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(400740)
+addappid(400741,0,"82cf8d087ae143c16888b4d9a9c21e684dfb8699217d445e6c7c203474cca226")
