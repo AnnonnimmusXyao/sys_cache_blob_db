989b2223afccae89d3705f06b0d512c44925cd11
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 08:08:06 -0300

    Sync LUA 425220 a GAME

---
+++ GAME/425220.lua
@@ -0,0 +1,14 @@
+--game: One Piece Burning Blood
+
+addappid(228985)
+addappid(425220)
+addappid(425221,0,"67498d5ca47e8d438e47b160b8c99fff1ffe41ee1ff73fbfe13f157be02d16f3")
+addappid(458440)
+addappid(458441)
+addappid(458442)
+addappid(458443)
+addappid(458444)
+addappid(458445)
+addappid(458460)
+addappid(501570)
+addappid(501580)
