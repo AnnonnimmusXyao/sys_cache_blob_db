d31d3bb8edfb80591e5ba110a1d986c70ec17ff4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 18:27:06 -0300

    Sync LUA 638000 a GAME

---
+++ GAME/638000.lua
@@ -0,0 +1,5 @@
+--game: When Ski Lifts Go Wrong
+
+addappid(638000)
+addappid(638001,0,"99cd590a0ddd231e2af01c439d21c9facfba6a02a760f903582685c2c3270338")
+addappid(638002,0,"264e1b92d65cca303d330d4876abed30e14e8aa38eb7e1f5bb2801b138c2ea42")
