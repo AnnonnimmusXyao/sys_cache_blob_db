c7d4f30a84a8532b790f9816c6a9b09042c253be
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 14:48:32 -0300

    Sync LUA 544550 a GAME

---
+++ GAME/544550.lua
@@ -0,0 +1,11 @@
+--game: Stationeers
+
+addappid(544550)
+addappid(544551,0,"d1ac2705285c1f7d74d0a4ef441cc09605d7d0f5bb588fead0f781d2cffea960")
+addappid(1038400)
+addappid(1038500)
+addappid(2089290)
+addappid(2542990)
+addappid(3166330)
+addappid(3196210)
+addappid(3196220)
