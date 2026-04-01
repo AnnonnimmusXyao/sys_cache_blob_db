6607e90cf275cd4e3abbb1838500924ea6a69184
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 09:55:42 -0300

    Sync LUA 491530 a GAME

---
+++ GAME/491530.lua
@@ -0,0 +1,6 @@
+--game: Notruf 112 | Emergency Call 112
+
+addappid(228985)
+addappid(491530)
+addappid(491531,0,"75706ad45ded4eb130afacc177c675a508d3adac6e944c5cf03260a58db58d94")
+addappid(630850)
