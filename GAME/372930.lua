2a4322c018ad2f342f17aa84b451388a9a977276
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 21:54:47 -0300

    Sync LUA 372930 a GAME

---
+++ GAME/372930.lua
@@ -0,0 +1,5 @@
+--game: New York Mysteries: Secrets of the Mafia Collector's Edition
+
+addappid(372930)
+addappid(372931,0,"d2236afae00f55c7dae21adbbaa841a22fd46f86ff6c188ac20954b89096be97")
+addappid(372933,0,"7762e51ff0c6373c44bd6551ac65239c1bfc0083b6a6e13269504e48ef2cfbac")
