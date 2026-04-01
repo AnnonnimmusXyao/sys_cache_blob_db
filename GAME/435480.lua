16790c8eb1312acb07b1989b7fa25c5126ae014c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 08:27:57 -0300

    Sync LUA 435480 a GAME

---
+++ GAME/435480.lua
@@ -0,0 +1,8 @@
+--game: Trials of the Blood Dragon
+
+addappid(228982)
+addappid(229002)
+addappid(435480)
+addappid(488980)
+addappid(488981)
+addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
