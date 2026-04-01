a6d4b8f9ff284894b4395ffdf8ae484f3073cdc2
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 21:36:34 -0300

    Sync LUA 368260 a GAME

---
+++ GAME/368260.lua
@@ -0,0 +1,14 @@
+--game: Marvel's Midnight Suns
+
+addappid(229002)
+addappid(368260)
+addappid(368261,0,"70e0c68fc7e9ff3b4054bcfda8648cae98efa3b3905a29d91c2eaeee83dd9b21")
+addappid(1523211,0,"6111b2e3d21efa5e5b26a91f75ff32d12aa8c5bb547c4d0215eda184a7f4c49b")
+addappid(1712442)
+addappid(1922631)
+addappid(1922632)
+addappid(1922633)
+addappid(1922634)
+addappid(2074340)
+addappid(2100870)
+addappid(2100871)
