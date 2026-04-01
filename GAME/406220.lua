131b69572478890ddd7e3c94db04dca75bab78e2
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 00:38:49 -0300

    Sync LUA 406220 a GAME

---
+++ GAME/406220.lua
@@ -0,0 +1,6 @@
+--game: Gnomes Vs. Fairies: Greckel's Quest
+
+addappid(406220)
+addappid(406221,0,"0afa423bffebd9095246129d6bcab5a6bf1d37e93470ac736226e59fe0b03a7c")
+addappid(406223,0,"ca2e91a221c70fa144bd38384e45e720934d1461d50b4cb834bfb65e879461b2")
+addappid(575190)
