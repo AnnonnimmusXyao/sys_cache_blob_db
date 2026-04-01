3d792384ae57e152427f7204277855f2a5bc8a87
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 19:27:03 -0300

    Sync LUA 666820 a GAME

---
+++ GAME/666820.lua
@@ -0,0 +1,7 @@
+--game: Skirmish Line
+
+addappid(666820)
+addappid(666821,0,"c541d085bf978485d322e7bece54844c0c003ba0f9ae718804d1e1bdd6e7931e")
+addappid(1041010)
+addappid(1068400)
+addappid(1208440)
