72cd53c062bf00b27388f5bea593fd5a1f2254ff
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 18:00:22 -0300

    Sync LUA 621830 a GAME

---
+++ GAME/621830.lua
@@ -0,0 +1,6 @@
+--game: WRC 7 FIA World Rally Championship
+
+addappid(228983)
+addappid(621830)
+addappid(621831,0,"20e3152c0296460f28ce3f5a211affca305a09e6d9b9f6bbcce13bc819d0f462")
+addappid(627390)
