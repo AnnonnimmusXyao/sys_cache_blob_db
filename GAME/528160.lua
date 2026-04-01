ab498f15c0e8970d726c8daeb7ff9382a03c8a50
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 13:13:10 -0300

    Sync LUA 528160 a GAME

---
+++ GAME/528160.lua
@@ -0,0 +1,10 @@
+--game: Solstice Chronicles: MIA
+
+addappid(228983)
+addappid(228985)
+addappid(229031)
+addappid(229032)
+addappid(229033)
+addappid(528160)
+addappid(528161,0,"0dae17234537fba91da3c9ba144ebb731f3fb6fb1e4d7383f1de0d910ce15fd5")
+addappid(3243340)
