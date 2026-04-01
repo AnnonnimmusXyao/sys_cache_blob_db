3b6f4221baca789e4eb901db0c20ae239654d96b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 17:35:06 -0300

    Sync LUA 608800 a GAME

---
+++ GAME/608800.lua
@@ -0,0 +1,10 @@
+--game: Guns of Icarus Alliance
+
+addappid(608800)
+addappid(209084)
+addappid(209085)
+addappid(209086)
+addappid(209081)
+addappid(209082)
+addappid(209083)
+addappid(608801,0,"59f1efb5bbc29b7c24a5f2130f0d4355135b3ab5a87e8c17f9ae2f3e04602723")
