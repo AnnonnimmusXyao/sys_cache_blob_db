ff1fa0735ff6c92b703e6fb8f8035dba37d414db
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-03-31 22:26:57 -0300

    Sync LUA 378630 a GAME

---
+++ GAME/378630.lua
@@ -0,0 +1,6 @@
+--game: Shadows on the Vatican - Act II: Wrath
+
+addappid(378630)
+addappid(378631,0,"b97403f13946b939b18af93bc742a0eadd0e69ae0bec73301c65fbca68f6f7e7")
+addappid(378632,0,"976afe881a90ef50ced77e64144bc29dcb832296dfd34796a46941520e6e2d1b")
+addappid(378639,0,"12a5b67fb95f4f93438ac5acd17ae25da1fb1811befebbb3ee0b46838329b043")
