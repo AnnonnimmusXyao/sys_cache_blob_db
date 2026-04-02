732f2a71b5e05d8833d7f448b9532ed6bdb67c01
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:55:21 -0300

    Sync LUA 814530 a GAME

---
+++ GAME/814530.lua
@@ -0,0 +1,8 @@
+--game: Death Crown
+
+addappid(228987)
+addappid(229005)
+addappid(814530)
+addappid(814531,0,"76a970e820bd33607a47add3e56d9ebe66a937f4fec79f4184573972ee998457")
+addappid(1231850)
+addappid(1312820)
