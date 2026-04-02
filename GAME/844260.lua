6cdeeadac2af178d218c73865c4e2db91ab09a3a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 04:18:50 -0300

    Sync LUA 844260 a GAME

---
+++ GAME/844260.lua
@@ -0,0 +1,7 @@
+--game: Rustler (Grand Theft Horse)
+
+addappid(844260)
+addappid(844261,0,"6468a8908f465a6dd342aa80dc4b43dfb4e86cfd44684749b63342c1c17a2dde")
+addappid(1570170)
+addappid(1628870)
+addappid(1628880)
