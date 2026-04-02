7467c7635c30988b27365e8af07a66fb076d20f9
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 05:08:32 -0300

    Sync LUA 864700 a GAME

---
+++ GAME/864700.lua
@@ -0,0 +1,7 @@
+--game: Dinosaur Fossil Hunter
+
+addappid(229007)
+addappid(864700)
+addappid(864701,0,"faedd46777d12749271e88108954cd2db14f7920472fe445a1b62c5867cff3fc")
+addappid(2065550)
+addappid(2217690)
