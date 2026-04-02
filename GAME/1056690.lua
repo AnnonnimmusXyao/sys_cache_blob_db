4edbd4f0b82dadc327b2c95b5681c6f44b4e2545
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 17:51:47 -0300

    Sync LUA 1056690 a GAME

---
+++ GAME/1056690.lua
@@ -0,0 +1,5 @@
+--game: Drink More Glurp
+
+addappid(1056690)
+addappid(1056691,0,"1f9981aaa7daa4018070b6713c0d0dcac106705301af0f9410b72b8c8c729106")
+addappid(1056693,0,"5af022c95f537febb2f9b0add6b65b114ce03214c0eed7bf8aeb647959f1eed1")
