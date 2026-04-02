5f48a73600cba34c6255b06e79f7d0ae7e5fcd1e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:15:56 -0300

    Sync LUA 764920 a GAME

---
+++ GAME/764920.lua
@@ -0,0 +1,8 @@
+--game: Fear the Night - 恐惧之夜
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(764920)
+addappid(764921,0,"dc067df4c0ccec7c188e6cd7d75704804705e7bf7dd7778076f9e662a113224a")
