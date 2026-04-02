f74cdb30511daff37cfd7222a6e3b16dc027b66c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 18:23:17 -0300

    Sync LUA 1069210 a GAME

---
+++ GAME/1069210.lua
@@ -0,0 +1,10 @@
+--game: Pulang Insanity - Director's Cut
+
+addappid(1069210)
+addappid(228985)
+addappid(229004)
+addappid(229005)
+addappid(229006)
+addappid(229012)
+addappid(229033)
+addappid(1069211,0,"df65187fbcd4d429b1e181973de797d636e2c48fc2ae51d83a4455c5b3e8c23d")
