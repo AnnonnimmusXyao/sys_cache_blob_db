417f2045902e135ab7a86e8e05ba57732492230b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:39:45 -0300

    Sync LUA 773580 a GAME

---
+++ GAME/773580.lua
@@ -0,0 +1,7 @@
+--game: Game Dev Studio
+
+addappid(228987)
+addappid(773580)
+addappid(773581,0,"9fbd4427bb2219a98c08153980e42df041c4efcb89a0621b6465d238c3a5cb82")
+addappid(1224000)
+addappid(1224001,0,"68945e4d95012da440bba9dabab61422573d1acc9ecf2f801ceff32bb12744bd")
