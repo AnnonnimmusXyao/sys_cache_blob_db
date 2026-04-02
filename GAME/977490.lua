ba65837b30bf46088c949ebfc6255fa27901ccb4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 15:02:38 -0300

    Sync LUA 977490 a GAME

---
+++ GAME/977490.lua
@@ -0,0 +1,13 @@
+--game: Jigsaw Masterpieces
+
+addappid(977490)
+addappid(977491,0,"6d44fe7e927dc04832862d83cb87cc38e3a0f39f889acdb3d70f6ebf108f867b")
+addappid(977492,0,"37b1964011928430dfe1abd1ec689bf330c56bd30ae2af4cce795fa3da5472f1")
+addappid(1011100)
+addappid(1013780)
+addappid(1036810)
+addappid(1037360)
+addappid(1037370)
+addappid(1058970)
+addappid(1059700)
+addappid(1059720)
