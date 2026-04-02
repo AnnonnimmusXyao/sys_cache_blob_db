1f367436ad918067562e163d272d0142e2c4a91d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 13:53:25 -0300

    Sync LUA 944740 a GAME

---
+++ GAME/944740.lua
@@ -0,0 +1,8 @@
+--game: 三国虎将传VR2-Sanguo Warriors VR2
+
+addappid(228987)
+addappid(229005)
+addappid(229006)
+addappid(229031)
+addappid(944740)
+addappid(944741,0,"f073bd6b2f8526b548f6e81e7a465f173158316472b3fc46dc1e2199d4b02914")
