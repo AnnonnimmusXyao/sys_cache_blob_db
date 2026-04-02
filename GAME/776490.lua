7bcdfe3c00d39374da89f830b40bdae211808e9e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:44:50 -0300

    Sync LUA 776490 a GAME

---
+++ GAME/776490.lua
@@ -0,0 +1,10 @@
+--game: 永远消失的幻想乡 ～ The Disappearing of Gensokyo
+
+addappid(776490)
+addappid(776491,0,"69fca9154f831bbcd5c53ce8e95a2a7b986a2a9a2858071b3ba4061ae7dd7a7d")
+addappid(778530)
+addappid(779280)
+addappid(780980)
+addappid(780990)
+addappid(781010)
+addappid(781020)
