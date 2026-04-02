613f80847f31cb2d79e3039e4e43d5fd29789804
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 20:50:49 -0300

    Sync LUA 1135260 a GAME

---
+++ GAME/1135260.lua
@@ -0,0 +1,8 @@
+--game: The Falconeer: Revolution Remaster
+
+addappid(1135260)
+addappid(1135261,0,"c19bb6d1c7ec099597fcc92bc6d90c41f61d7130b01b7ca4593d149e00369da2")
+addappid(1434030)
+addappid(1474610)
+addappid(1516140)
+addappid(1604190)
