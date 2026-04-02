4a20db837b909a613789c975c22d8c6aafad5101
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 03:59:49 -0300

    Sync LUA 839630 a GAME

---
+++ GAME/839630.lua
@@ -0,0 +1,10 @@
+--game: Survisland / 实境求生
+
+addappid(839630)
+addappid(229001)
+addappid(229002)
+addappid(229003)
+addappid(229004)
+addappid(229005)
+addappid(229006)
+addappid(839632,0,"83ec6ce4f152923f755cf7d5d9a7c2c480c09d600302e5caac0932702cf23a75")
