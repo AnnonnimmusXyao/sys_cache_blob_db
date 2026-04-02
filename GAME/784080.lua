e1cdaa50aab6860a10c4f74d7aaecb9d9a86355e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:59:18 -0300

    Sync LUA 784080 a GAME

---
+++ GAME/784080.lua
@@ -0,0 +1,12 @@
+--game: MechWarrior 5: Mercenaries
+
+addappid(784080)
+addappid(784081,0,"b8b97d071286439862b9f60a8898a85f6e6c648afd1e3064b37f125523f14a46")
+addappid(1456180)
+addappid(1456210)
+addappid(1705820)
+addappid(1969380)
+addappid(2179800)
+addappid(2489020)
+addappid(2782050)
+addappid(3799610)
