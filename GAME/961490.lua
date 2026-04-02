bf7fbb86eaa2726b4e0c4a2f71a49ddc3cd95a4e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 14:30:22 -0300

    Sync LUA 961490 a GAME

---
+++ GAME/961490.lua
@@ -0,0 +1,6 @@
+--game: Travis Strikes Again: No More Heroes Complete Edition
+
+addappid(228987)
+addappid(961490)
+addappid(961491,0,"19f71102c5a771a09df9a11cbdb822e16dcd1e030d6b0acbb117a72ff3b9f9db")
+addappid(1172600)
