3337bb61b07b288945f8f8b4daabd3bc78088f7b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:30:36 -0300

    Sync LUA 768520 a GAME

---
+++ GAME/768520.lua
@@ -0,0 +1,13 @@
+--game: Red Solstice 2: Survivors
+
+addappid(229033)
+addappid(768520)
+addappid(768521,0,"6f73b9272499736de64e539fa4273b4f3068592d4e4cc2411d7c7faf71df7f8b")
+addappid(1588820)
+addappid(1588821)
+addappid(1604220)
+addappid(1736390)
+addappid(1751750)
+addappid(1832100)
+addappid(1931720)
+addappid(2012090)
