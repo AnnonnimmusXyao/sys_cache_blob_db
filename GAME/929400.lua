843154b5f59bd8f00aed39731d7706a3afdd7954
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 08:17:13 -0300

    Sync LUA 929400 a GAME

---
+++ GAME/929400.lua
@@ -0,0 +1,5 @@
+--game: Mahjong Magic Journey 3
+
+addappid(929400)
+addappid(929401,0,"50ad8cf401dcc4fc3ea8d49419a98ce3da8514fcf701751633ff7a41a379a99c")
+addappid(929404,0,"d150294f2e4a59af3d09515a24e194b96bba9eb040de51d9678f131624d53dff")
