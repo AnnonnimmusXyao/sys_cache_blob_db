c0995131baebf3ad2ef49ce2fa614ba11cb43f46
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:49:25 -0300

    Sync LUA 810500 a GAME

---
+++ GAME/810500.lua
@@ -0,0 +1,7 @@
+--game: PowerBeatsVR - VR Fitness
+
+addappid(810500)
+addappid(810501,0,"961e04b3623f017c42572c3ca7cef69c0ddc1447422359c752ee94ad925db11b")
+addappid(2070470)
+addappid(2703630)
+addappid(3598630)
