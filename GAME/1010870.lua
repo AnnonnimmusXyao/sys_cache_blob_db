406a8be6f7b9bcfa224dc1c0f3c33912c9079c72
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 16:10:57 -0300

    Sync LUA 1010870 a GAME

---
+++ GAME/1010870.lua
@@ -0,0 +1,6 @@
+--game: 最后的47小时 - The Last 47 Hours
+
+addappid(1010870)
+addappid(1010871,0,"0b22dfe28f4e2c8868299f0e4b4dbafaef286d67afd51c00cec174017798040e")
+addappid(1031520)
+addappid(1031530)
