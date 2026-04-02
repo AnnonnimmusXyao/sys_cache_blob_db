ce11b182c54ea47faa32d63fd4d548ab8d3be5ba
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:52:50 -0300

    Sync LUA 780290 a GAME

---
+++ GAME/780290.lua
@@ -0,0 +1,8 @@
+--game: Gloomhaven
+
+addappid(229006)
+addappid(780290)
+addappid(780291,0,"556d7497acead556deb456600974b551adc59260250f1973267ad37ae195d1a5")
+addappid(1809490)
+addappid(1958560)
+addappid(2584170)
