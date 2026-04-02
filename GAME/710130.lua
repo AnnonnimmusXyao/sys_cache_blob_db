b16fc75581cd376ed09574eb6498f9ff68317abe
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 21:52:28 -0300

    Sync LUA 710130 a GAME

---
+++ GAME/710130.lua
@@ -0,0 +1,6 @@
+--game: 隐龙传：影踪(Hidden Dragon Legend: Shadow Trace)
+
+addappid(710130)
+addappid(710131,0,"3439aa851a12a1de3d8829cc5e75f09669955799e9f34f051eeb3ecfefe443c9")
+addappid(723430)
+addappid(723440)
