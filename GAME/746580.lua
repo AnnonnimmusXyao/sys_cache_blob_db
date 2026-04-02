a88cbb16a1ba8e31f4098c194df353a895d8aa9b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 00:19:23 -0300

    Sync LUA 746580 a GAME

---
+++ GAME/746580.lua
@@ -0,0 +1,12 @@
+--game: HARDCORE MECHA
+
+addappid(746580)
+addappid(746582,0,"62bb5d1a8c8fdf7da74c644516197a58f4d8a54b700fb073c1cfc4fa2e8a3586")
+addappid(955590)
+addappid(1125640)
+addappid(1125641)
+addappid(1125642)
+addappid(1146680)
+addappid(1146690)
+addappid(1187230)
+addappid(1344050)
