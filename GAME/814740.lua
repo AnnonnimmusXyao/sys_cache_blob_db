bb9d7a04f1f9417153e7d604ea2f4079a4c417ed
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:56:10 -0300

    Sync LUA 814740 a GAME

---
+++ GAME/814740.lua
@@ -0,0 +1,5 @@
+--game: Armored Battle Crew [World War 1] - Tank Warfare and Crew Management Simulator
+
+addappid(814740)
+addappid(814741,0,"401bc18d61dadc140cd4e4d3fd58c98bc5f399540c6cc0b5bdd5a731b5b2c7c2")
+addappid(1091160)
