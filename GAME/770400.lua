e68e5916f4b20a7517977f2a8145054256e72fd2
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:34:01 -0300

    Sync LUA 770400 a GAME

---
+++ GAME/770400.lua
@@ -0,0 +1,8 @@
+--game: Fairy of the treasures
+
+addappid(770400)
+addappid(770401,0,"78980056981f05a60956d6716402f8d6941b3762dc4b8bb564eef13db67bf84c")
+addappid(792360)
+addappid(816050)
+addappid(973490)
+addappid(997740)
