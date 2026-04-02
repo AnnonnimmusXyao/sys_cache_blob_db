8f1eff3acc6f5d9f36665b0799e490c108ef8081
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 01:58:41 -0300

    Sync LUA 783710 a SOFTWARE

---
+++ SOFTWARE/783710.lua
@@ -0,0 +1,7 @@
+--game: Virtual Desktop Dashboard
+
+addappid(228983)
+addappid(228985)
+addappid(229006)
+addappid(783710)
+addappid(783711,0,"033f19bf2c9ba42ce48dc01e8129ccf0527987b45719b229f4c1659d62ff62b1")
