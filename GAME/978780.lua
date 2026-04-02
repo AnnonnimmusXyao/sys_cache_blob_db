f50418299488f3157131c037930774a3f110a25a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 15:05:40 -0300

    Sync LUA 978780 a GAME

---
+++ GAME/978780.lua
@@ -0,0 +1,11 @@
+--game: STORY OF SEASONS: Friends of Mineral Town
+
+addappid(228987)
+addappid(229005)
+addappid(978780)
+addappid(978781,0,"6c1f00459c6e87fccd19c861a3905933c304256f5f3e7ffea2b870ce0c521b0d")
+addappid(1292300)
+addappid(1292301)
+addappid(1292302)
+addappid(1292303)
+addappid(1292304)
