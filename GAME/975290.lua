f7ac6803c072e50748ffb3194c0afa4cd2a0d306
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 14:57:57 -0300

    Sync LUA 975290 a GAME

---
+++ GAME/975290.lua
@@ -0,0 +1,9 @@
+--game: The Pepper Prince: Episode 1 - Red Hot Chili Wedding
+
+addappid(975290)
+addappid(975291,0,"a7cc65832e90904146fefdeb301412beef199523c2f68a9f3deb9a016af6977b")
+addappid(975300)
+addappid(975301)
+addappid(975302)
+addappid(975303)
+addappid(1007550)
