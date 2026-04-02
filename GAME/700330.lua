6b739a896d9b184bb7327c843718eaac48d690c3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 21:22:28 -0300

    Sync LUA 700330 a GAME

---
+++ GAME/700330.lua
@@ -0,0 +1,8 @@
+--game: SCP: Secret Laboratory
+
+addappid(229007)
+addappid(700330)
+addappid(700331,0,"3d5c02fae29037cd244fb3cd9fc0c41a75f3a6c62bcb306fb39e79d0d5f6f2bd")
+addappid(859210)
+addappid(2702460)
+addappid(2787940)
