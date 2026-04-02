ceaa744d339bf5c1046527c3e2adb3364717d957
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 03:42:31 -0300

    Sync LUA 828440 a GAME

---
+++ GAME/828440.lua
@@ -0,0 +1,8 @@
+--game: The Other Side Of The Screen
+
+addappid(229006)
+addappid(229012)
+addappid(229020)
+addappid(229030)
+addappid(828440)
+addappid(828441,0,"fc010e5d1110eb960705aab10f411679e16a9e723c5c5588df0b57fb7860f7c3")
