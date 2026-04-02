699218acc89168ed7f1ff54f1e74e3840c5a9eed
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:34:18 -0300

    Sync LUA 803050 a GAME

---
+++ GAME/803050.lua
@@ -0,0 +1,9 @@
+--game: Per Aspera
+
+addappid(803050)
+addappid(803051,0,"9cc56df6c0ed8ed20759eae18a6a268eeb4fb20cfd38b309b45db6855f92dc5b")
+addappid(1479440)
+addappid(1688480)
+addappid(1726200)
+addappid(1966430)
+addappid(2287380)
