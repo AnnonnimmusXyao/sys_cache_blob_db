35a576cd40261f06819353f9057a6ab5971e6d8a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 14:33:28 -0300

    Sync LUA 963330 a GAME

---
+++ GAME/963330.lua
@@ -0,0 +1,7 @@
+--game: Nya Nya Nya Girls (ʻʻʻ)_(=^･ω･^=)_(ʻʻʻ)
+
+addappid(229000)
+addappid(963330)
+addappid(963331,0,"1ec456d2b2ea38bf43172e6b6d65af62aeb687266322ffc1df8482d9069984ec")
+addappid(997470)
+addappid(1082040)
