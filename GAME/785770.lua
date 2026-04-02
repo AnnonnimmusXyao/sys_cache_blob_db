8f5e7084c484f30fbbbc7ce177051b15b2ab5a03
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:00:28 -0300

    Sync LUA 785770 a GAME

---
+++ GAME/785770.lua
@@ -0,0 +1,6 @@
+--game: Emergency Call 112 – The Fire Fighting Simulation 2
+
+addappid(785770)
+addappid(785771,0,"2c7097ddbc5e503a653700238917cebd42e5cb34f1bd54118c32df3457f45516")
+addappid(1925400)
+addappid(2715800)
