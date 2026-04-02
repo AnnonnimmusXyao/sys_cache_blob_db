161bc77e916d07fe0ff13691f64aaec54b96c166
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 06:09:09 -0300

    Sync LUA 886900 a GAME

---
+++ GAME/886900.lua
@@ -0,0 +1,9 @@
+--game: Chef: A Restaurant Tycoon Game
+
+addappid(228987)
+addappid(229006)
+addappid(886900)
+addappid(886901,0,"0b6fd34defc26d8d74d5460729a95fda85ceb98c8aef21176b1c5f6ed97df757")
+addappid(1617870)
+addappid(2057810)
+addappid(2375390)
