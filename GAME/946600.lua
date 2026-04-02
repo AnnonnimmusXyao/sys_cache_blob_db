71a347bd0c28c91ae1c46aaece047ffde177b447
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 13:57:28 -0300

    Sync LUA 946600 a GAME

---
+++ GAME/946600.lua
@@ -0,0 +1,8 @@
+--game: Girl with a big SWORD
+
+addappid(946600)
+addappid(946601,0,"0eb5c9e25c9bab24a916c390aacf87b076afd62178a0f4a268c93a90e536beba")
+addappid(948780)
+addappid(948810)
+addappid(973500)
+addappid(3305780)
