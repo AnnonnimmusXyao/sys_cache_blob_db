8bfe1add99115de1bb3fed98c1d0f2393b060fb0
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 20:23:33 -0300

    Sync LUA 1123450 a GAME

---
+++ GAME/1123450.lua
@@ -0,0 +1,5 @@
+--game: Chicory: A Colorful Tale
+
+addappid(1123450)
+addappid(1123451,0,"b4cd0ce8c522800cd37e1ce4a2f47d6f570fd1ed3df8dd1bf10cef18724104fa")
+addappid(1123452,0,"b75b2188c9f776dde7029006973a4c8a8a69d2fb264e9b90a5e1d677a8b22cb0")
