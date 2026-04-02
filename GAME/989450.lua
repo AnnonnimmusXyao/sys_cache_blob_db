ff1f29fb6c4a8136078d0f32c5721d78f30fd772
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 15:28:06 -0300

    Sync LUA 989450 a GAME

---
+++ GAME/989450.lua
@@ -0,0 +1,9 @@
+--game: 时之回廊/Corridor of time
+
+addappid(228987)
+addappid(229000)
+addappid(229001)
+addappid(229002)
+addappid(229003)
+addappid(989450)
+addappid(989451,0,"05d48ee7483c5056e2a7545096ad1bf18d3a1cb5aa000c3d7c294107c0954669")
