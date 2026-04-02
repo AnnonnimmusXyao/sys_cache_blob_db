652585b9f137e1efcbd962823c82e721bcf41d64
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 04:07:38 -0300

    Sync LUA 843790 a GAME

---
+++ GAME/843790.lua
@@ -0,0 +1,4 @@
+--game: HUMAN LIVE-HOW LONG CAN HUMAN BEINGS EXIST?人类能生存多久？挑战各种灾难，地球世界末日，冒险策略模拟经营游戏
+
+addappid(843790)
+addappid(843791,0,"23d97b8b8489124133ec6d1088cf5241bca7bd007b3b6e202a25d440f5577754")
