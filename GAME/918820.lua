4442a601e676d96eadc0a83fb3fff5f52358918e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 07:41:02 -0300

    Sync LUA 918820 a GAME

---
+++ GAME/918820.lua
@@ -0,0 +1,7 @@
+--game: Headliner: NoviNews
+
+addappid(228987)
+addappid(229003)
+addappid(229004)
+addappid(918820)
+addappid(918821,0,"fef43ad025c22f4f077f6609347b0865ff8ffd50d65987490f2e3718d48aea1c")
