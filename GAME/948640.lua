41ed922aabb31675b42098a5db0c219c0fcdc7dd
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 14:01:46 -0300

    Sync LUA 948640 a GAME

---
+++ GAME/948640.lua
@@ -0,0 +1,6 @@
+--game: Wizardry: Labyrinth of Lost Souls
+
+addappid(228983)
+addappid(948640)
+addappid(948641,0,"26889659d0161f4c07950842c6437d7da11a83cbea608857b11c7c6fb48f84d1")
+addappid(1022570)
