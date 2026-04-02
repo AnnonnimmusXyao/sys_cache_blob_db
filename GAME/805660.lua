3f7177e9a0bbbdbcd70a7802b9de158856cddb8a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:38:48 -0300

    Sync LUA 805660 a GAME

---
+++ GAME/805660.lua
@@ -0,0 +1,8 @@
+--game: Rad Rodgers - Radical Edition
+
+addappid(228985)
+addappid(229004)
+addappid(229005)
+addappid(229033)
+addappid(805660)
+addappid(805661,0,"c1435e899d1aa755449c14b20c61b216738bc0135c05f8c8a2ceedb8929f9276")
