c949ddb6b3b949f7bcd2fe6165d0e6a2395b423b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 03:47:06 -0300

    Sync LUA 832080 a GAME

---
+++ GAME/832080.lua
@@ -0,0 +1,5 @@
+--game: Google Spotlight Stories: Back to the Moon
+
+addappid(832080)
+addappid(832081,0,"391e3376d8e707f1938474d8ca9e2ada4b1a007101117c5cdb708183da44903c")
+addappid(832082,0,"ff81399c40d0e79500fe932bfe9b43aacbc84779e7cd5fe40d9154966c6307be")
