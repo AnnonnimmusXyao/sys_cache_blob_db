e7b27f77af8370b4aac9a3ba56e39ccb7e45e8b5
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 04:22:42 -0300

    Sync LUA 846030 a GAME

---
+++ GAME/846030.lua
@@ -0,0 +1,9 @@
+--game: ΔV: Rings of Saturn
+
+addappid(846030)
+addappid(846031,0,"095ebd18370da2b2974902cfd88ddb05ebc4e930027d37328ebccce55ad1438b")
+addappid(1211880)
+addappid(1908440)
+addappid(1938680)
+addappid(2263460)
+addappid(3966110)
