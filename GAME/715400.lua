5f7d2e48e87708910e760a2b81d98d82978c028c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 22:07:00 -0300

    Sync LUA 715400 a GAME

---
+++ GAME/715400.lua
@@ -0,0 +1,12 @@
+--game: Frozen Flame
+
+addappid(715400)
+addappid(715401,0,"137ac76cbbb80df9e88bf9287ca2973bd41e26ac5793c5d2aaffd8290b899438")
+addappid(2221290)
+addappid(2221300)
+addappid(2221310)
+addappid(2221320)
+addappid(2221330)
+addappid(2221340)
+addappid(2221360)
+addappid(2358760)
