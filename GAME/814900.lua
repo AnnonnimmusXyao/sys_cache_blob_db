de2c728a2fb44c875fdb561630170a666f8bbec3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:56:33 -0300

    Sync LUA 814900 a GAME

---
+++ GAME/814900.lua
@@ -0,0 +1,7 @@
+--game: 三国虎将传VR-Sanguo Warriors VR
+
+addappid(229005)
+addappid(229020)
+addappid(229031)
+addappid(814900)
+addappid(814901,0,"d93132006e491b1f64d6befc0561b5ec8cb39b14f912341bbc46e0f20e7be5aa")
