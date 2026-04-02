a635d1460b1372759cf4074c3212432fb671a607
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 02:25:37 -0300

    Sync LUA 798420 a GAME

---
+++ GAME/798420.lua
@@ -0,0 +1,8 @@
+--game: Professional Fishing
+
+addappid(798420)
+addappid(798421,0,"e91ec1d1b02d69881047e5b8d12fa2e93224d2654c2cb6c020e53c4cd8217125")
+addappid(937790)
+addappid(937791)
+addappid(937792)
+addappid(988750)
