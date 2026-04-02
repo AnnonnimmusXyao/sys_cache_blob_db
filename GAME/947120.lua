de0a52a5191c078bdcdb24bc4981c9f75c62ff69
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 13:59:03 -0300

    Sync LUA 947120 a GAME

---
+++ GAME/947120.lua
@@ -0,0 +1,5 @@
+--game: Where The Water Tastes Like Wine: Fireside Chats
+
+addappid(947120)
+addappid(947123,0,"a3e95932de4fb9703ec9d26c670fee0a38816efef439c30f15a5ad428dd06239")
+addappid(947124,0,"7131224f1f108fdb11e8c8f8862048d30bec84fdb2c74071c979850bb8bc6fa5")
