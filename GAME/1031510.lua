44bb380d18a115455155b32d7cd16fd7af913d3b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 16:57:10 -0300

    Sync LUA 1031510 a GAME

---
+++ GAME/1031510.lua
@@ -0,0 +1,7 @@
+--game: SUPER ROBOT WARS X
+
+addappid(228987)
+addappid(1031510)
+addappid(1031511,0,"e7f11a666207c8b4d689d0d41096268edca6014bd3817d1d2d8ef27f67a6eb34")
+addappid(1126740)
+addappid(1128550)
