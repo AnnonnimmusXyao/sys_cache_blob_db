c6d643646591a31ace473fe021d2dc8b3c93fd74
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 22:40:03 -0300

    Sync LUA 726000 a GAME

---
+++ GAME/726000.lua
@@ -0,0 +1,8 @@
+--game: No King No Kingdom
+
+addappid(726000)
+addappid(726001,0,"3bf0157ba38dcf5822c04e5d3f68dd27dadbf3dc3810810b466a61f4303721ca")
+addappid(1679900)
+addappid(1933250)
+addappid(2078220)
+addappid(2138370)
