79949aecfe77512f06e713a38d139735f4b91a83
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 18:37:00 -0300

    Sync LUA 1075190 a GAME

---
+++ GAME/1075190.lua
@@ -0,0 +1,6 @@
+--game: A Game of Thrones: The Board Game - Digital Edition
+
+addappid(1075190)
+addappid(1075191,0,"0776c426b3cfe73cc6a8639e8ee248f204495ca77662902dc998abb19f67d0ee")
+addappid(1416390)
+addappid(1416391)
