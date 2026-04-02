5c73ec4d59ab78f612018122f3d7aa7cf0dc1205
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 00:46:27 -0300

    Sync LUA 755470 a GAME

---
+++ GAME/755470.lua
@@ -0,0 +1,5 @@
+--game: The World Next Door
+
+addappid(755470)
+addappid(755471,0,"a52fbcec8fb061636f248e886446b9bd58874287ecb1269c1bcfeab8f4faddb9")
+addappid(1053860,0,"4ee1c339b0a080af6e0f801e147b50878af9458dc215ec46dc91b1bb02ece321")
