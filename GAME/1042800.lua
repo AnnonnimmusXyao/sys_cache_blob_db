39362675b1a9b209d938e2acf6a4c2ac229cd53b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 17:21:09 -0300

    Sync LUA 1042800 a GAME

---
+++ GAME/1042800.lua
@@ -0,0 +1,6 @@
+--game: Warhammer 40,000: Inquisitor - Prophecy
+
+addappid(527431)
+addappid(527432)
+addappid(1042800)
+addappid(1042801,0,"1e5d9df64c85c7259e5764bc91970185aae825bae7e8bc0ad41b029e2a140782")
