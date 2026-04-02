aad52c92d2d4bfbd1098e6f3275d7db40c6605fb
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 20:44:07 -0300

    Sync LUA 1132190 a GAME

---
+++ GAME/1132190.lua
@@ -0,0 +1,5 @@
+--game: Coloring Game: Little City
+
+addappid(1132190)
+addappid(1132191,0,"e556583e6565e68ea52611ce5425becd63d902cee359aaa53a6f23cf9611def6")
+addappid(1132193,0,"84d30d3cd6fa14a2baea6813f7a703a3bccfee8e1ac6c5817db9ea855173efc9")
