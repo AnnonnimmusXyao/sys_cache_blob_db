e14fe8704af7a32a09c0fecd42f7d41189c57fef
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 07:57:36 -0300

    Sync LUA 923420 a GAME

---
+++ GAME/923420.lua
@@ -0,0 +1,8 @@
+--game: Farstorm
+
+addappid(228987)
+addappid(229005)
+addappid(923420)
+addappid(923422,0,"dfe246db28901e8ed3c05d7377d2a54cd62b181a90398b29759ac37568573ce1")
+addappid(1145990)
+addappid(1148640)
