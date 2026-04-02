873eb0c90e69ad568bb2e30dbd69ffff75d497d7
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 03:51:33 -0300

    Sync LUA 834800 a GAME

---
+++ GAME/834800.lua
@@ -0,0 +1,8 @@
+--game: SolSeraph
+
+addappid(228987)
+addappid(229004)
+addappid(229020)
+addappid(229033)
+addappid(834800)
+addappid(834801,0,"103b2dd1eebc0aa19417e979134d1299d9f693f0f03d2e8fb9e32e8b4e64ca9b")
