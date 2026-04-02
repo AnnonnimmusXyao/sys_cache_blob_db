a468204d8a7e5a4e80de29f7dfd5df446ed6cfa9
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 15:22:35 -0300

    Sync LUA 986680 a GAME

---
+++ GAME/986680.lua
@@ -0,0 +1,8 @@
+--game: PAGUI打鬼
+
+addappid(986680)
+addappid(986681,0,"5c358ba3611cd58fa952e06591c5397863d0d564b8c9b7a9a148a03828a4c4c4")
+addappid(3547950)
+addappid(3678680)
+addappid(3969840)
+addappid(3970000)
