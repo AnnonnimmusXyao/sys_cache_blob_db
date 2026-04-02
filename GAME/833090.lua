ae0a22404bfc49998c0cd7265460dcc794c642b8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 03:48:27 -0300

    Sync LUA 833090 a GAME

---
+++ GAME/833090.lua
@@ -0,0 +1,9 @@
+--game: Swords of Gurrah
+
+addappid(228983)
+addappid(228985)
+addappid(228987)
+addappid(229005)
+addappid(229006)
+addappid(833090)
+addappid(833091,0,"008e52ad3f48720f0c84b78b62e39da8c1f31d5f23d3ce9e6c7c6be3cbc68b4d")
