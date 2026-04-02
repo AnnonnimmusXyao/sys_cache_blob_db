f4d2e5bb5c5c5badf7b92620c4f125c4d397c476
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 13:46:37 -0300

    Sync LUA 941410 a GAME

---
+++ GAME/941410.lua
@@ -0,0 +1,6 @@
+--game: Touhou: Dreaming Butterfly | 东方蝶梦志
+
+addappid(228987)
+addappid(229003)
+addappid(941410)
+addappid(941411,0,"0d1901e1494a91e24bce5226cd62ba73557dca5022c186cfaa1149e241c1779e")
