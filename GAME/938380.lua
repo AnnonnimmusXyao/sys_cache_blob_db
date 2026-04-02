920e1e250c5b20f5ff89c3d3f8b4a4408d86ae65
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 08:32:10 -0300

    Sync LUA 938380 a GAME

---
+++ GAME/938380.lua
@@ -0,0 +1,7 @@
+--game: Townsmen - A Kingdom Rebuilt
+
+addappid(228987)
+addappid(229020)
+addappid(938380)
+addappid(938381,0,"e2343bc799cc70578f3f833d189447740e9824c230493046aec23b8698c5b545")
+addappid(1176670)
