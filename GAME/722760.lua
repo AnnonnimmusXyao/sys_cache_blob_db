9dad371262f8540c3519d37b67813ca1b7437750
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-01 22:32:00 -0300

    Sync LUA 722760 a GAME

---
+++ GAME/722760.lua
@@ -0,0 +1,6 @@
+--game: Captain Firebeard and the Bay of Crows
+
+addappid(722760)
+addappid(722761,0,"aa07e989bce0248edb93dd936bff9047bff36da64b570a061a632af1edfb2456")
+addappid(722762,0,"48b5fb1a7cced1b97f775966eabaf3b8f990f8066d4bf38cf83ed8bf743cb245")
+addappid(831920,0,"03cff94f4f2d5e377d6fac42f2c4a2164708a0e51214430679295bf7a798a4d9")
