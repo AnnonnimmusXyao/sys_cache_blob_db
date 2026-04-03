93e61b4ab6193e6f424612d84d9327681c728898
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 06:01:21 -0300

    Sync LUA 1370060 a GAME

---
+++ GAME/1370060.lua
@@ -0,0 +1,6 @@
+--game: 琪露诺的信仰之战 ~ Cirno's battle of faith
+
+addappid(229007)
+addappid(229033)
+addappid(1370060)
+addappid(1370062,0,"539b4f854b4dd94258fdd108f5aaf94510ea799bc20fff94017a127d5d8f3651")
