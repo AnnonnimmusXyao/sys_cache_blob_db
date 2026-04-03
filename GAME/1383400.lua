a3a22ec350baf20aff57826e1c1a7925a8bef5d3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 06:32:34 -0300

    Sync LUA 1383400 a GAME

---
+++ GAME/1383400.lua
@@ -0,0 +1,7 @@
+--game: Crystal Ortha
+
+addappid(1383400)
+addappid(1383401,0,"2da0d635b71bf98fcb98df22eb57ea70dbe242cfb31eef01d4db335ac69c79e2")
+addappid(1435550)
+addappid(1435551)
+addappid(1435552)
