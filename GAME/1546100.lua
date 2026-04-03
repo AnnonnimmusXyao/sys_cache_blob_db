d671c43f015647d00cbe69f6c0b969315e5083ae
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 12:21:52 -0300

    Sync LUA 1546100 a GAME

---
+++ GAME/1546100.lua
@@ -0,0 +1,10 @@
+--game: A Musical Story
+
+addappid(228983)
+addappid(228985)
+addappid(229002)
+addappid(229020)
+addappid(1546100)
+addappid(1546101,0,"b23da0eab58a14544638327cf2062bbe7afd4e2b57429b60e77b094382d12b76")
+addappid(1877540)
+addappid(1877550)
