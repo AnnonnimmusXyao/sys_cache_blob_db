fbc4314932fedd49ff3e80231dbc20f915b2d76c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 04:28:54 -0300

    Sync LUA 1325400 a SOFTWARE

---
+++ SOFTWARE/1325400.lua
@@ -0,0 +1,7 @@
+--game: VEGAS Pro 18 Edit Steam Edition
+
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(1325400)
+addappid(1325401,0,"d78647a715c6b05690ff1b183cb54d055600c85737c23a8ded663c542382a391")
