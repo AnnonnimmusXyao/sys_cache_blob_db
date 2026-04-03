59805274a1e4ab483da72df50f38ec6eb96f57b1
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 23:40:30 -0300

    Sync LUA 1195350 a GAME

---
+++ GAME/1195350.lua
@@ -0,0 +1,7 @@
+--game: Your little story: Winter
+
+addappid(228987)
+addappid(229006)
+addappid(229020)
+addappid(1195350)
+addappid(1195351,0,"d3197680028e9155ed9cf104d4fa98abf8faa5b7736f756b34fb018257342c09")
