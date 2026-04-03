4a46cb2afb7d419c3173c18e44ae713be9ccc4d3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 13:58:48 -0300

    Sync LUA 1585700 a GAME

---
+++ GAME/1585700.lua
@@ -0,0 +1,9 @@
+--game: Trap Genesis
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(229000)
+addappid(1585700)
+addappid(1585701,0,"9f199ea37f971ad3005d1348b3b27dc711bbc56ba8a464e1f819bc470e44934b")
