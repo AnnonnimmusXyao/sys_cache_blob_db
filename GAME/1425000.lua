e3f0c80cd73502fce164a1b934576470cf1fdd51
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 08:05:02 -0300

    Sync LUA 1425000 a GAME

---
+++ GAME/1425000.lua
@@ -0,0 +1,11 @@
+--game: Endless Alice
+
+addappid(1425000)
+addappid(1425001,0,"538a019189be9b823a8c7ff25c61d33657551e295cfe7a4d4abde120d7eb0eb7")
+addappid(2779970)
+addappid(3758720)
+addappid(3758730)
+addappid(3818470)
+addappid(3818480)
+addappid(3818490)
+addappid(3823330)
