ec10daba408cbfbab0a207d3379aa14b8cef1475
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 17:26:01 -0300

    Sync LUA 1684100 a GAME

---
+++ GAME/1684100.lua
@@ -0,0 +1,6 @@
+--game: 幻恋の夜宴: ハルーシサノバこいし ~ Halluci-Sabbat of Koishi
+
+addappid(1684100)
+addappid(1684101,0,"5f25649f14ebfebeb81cc617218e9ed5418475d0241f6d6cf70664969c648774")
+addappid(2653140)
+addappid(2937320)
