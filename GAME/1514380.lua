7f656645184a7ce2e4f1376a039cabdce338d74d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 11:18:11 -0300

    Sync LUA 1514380 a GAME

---
+++ GAME/1514380.lua
@@ -0,0 +1,7 @@
+--game: COGEN: Sword of Rewind / COGEN: 大鳥こはくと刻の剣
+
+addappid(1514380)
+addappid(1514381,0,"d85f9fc72eb520ce836a9079038c1550003690a1c05a6086056912574f2820fd")
+addappid(1644060)
+addappid(1644061)
+addappid(1644070)
