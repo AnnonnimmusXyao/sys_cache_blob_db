c31077685800ce5043f7405583914111cebf8264
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 18:52:34 -0300

    Sync LUA 1724140 a GAME

---
+++ GAME/1724140.lua
@@ -0,0 +1,6 @@
+--game: Craftomation 101: Programming & Craft
+
+addappid(1724140)
+addappid(1724141,0,"1e4e8afa0f3ccdd424ab57c22d719aaef9f15414e01e2b0bd5978a1d375daf7a")
+addappid(2795690)
+addappid(2832980)
