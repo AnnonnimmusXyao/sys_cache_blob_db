901b536ddd63d468d9d8f6baa2393cb438daf1c8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 22:19:57 -0300

    Sync LUA 1164060 a GAME

---
+++ GAME/1164060.lua
@@ -0,0 +1,6 @@
+--game: The Last Show of Mr. Chardish
+
+addappid(228983)
+addappid(228985)
+addappid(1164060)
+addappid(1164061,0,"8b843bb410283548db00e449c0cc3736a8f759715da2b6dccfc4e3f52f1ba3b1")
