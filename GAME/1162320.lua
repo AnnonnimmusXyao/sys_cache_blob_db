be0f3d9e53c71ec544ef45b6c2f4775dcb4dffdd
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 22:15:53 -0300

    Sync LUA 1162320 a GAME

---
+++ GAME/1162320.lua
@@ -0,0 +1,8 @@
+--game: AVA: Dark History
+
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(228987)
+addappid(1162320)
+addappid(1162321,0,"4d579ab18701a0e468bc8f81f1493b5e12501aacef0923ad197452ba7852030b")
