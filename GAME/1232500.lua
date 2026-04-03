8e607462ae985312befaec87468114c28ca8ed64
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 01:15:04 -0300

    Sync LUA 1232500 a GAME

---
+++ GAME/1232500.lua
@@ -0,0 +1,8 @@
+--game: Super Hiking  Simulator 2020
+
+addappid(228987)
+addappid(1232500)
+addappid(1232501,0,"b45d67499c7e0e067b304ea56963939eb079fa459fdd941f8735a5d8ec5c512d")
+addappid(1240151)
+addappid(1290210)
+addappid(1706690)
