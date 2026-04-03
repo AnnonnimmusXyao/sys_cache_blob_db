89b15441c164c6b867db01ae28d3eaf8fa067466
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 02:25:18 -0300

    Sync LUA 1266100 a GAME

---
+++ GAME/1266100.lua
@@ -0,0 +1,5 @@
+--game: Vampire: The Masquerade — Parliament of Knives
+
+addappid(1266100)
+addappid(1266101,0,"a0cd0015ed443f9177cd5ec45a29fb2aa768cf8e08e2cedc3d5a87ced039fa6d")
+addappid(2120360)
