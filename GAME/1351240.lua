f20d30814bc1aed878e5d04990612859e225fa4e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 05:21:12 -0300

    Sync LUA 1351240 a GAME

---
+++ GAME/1351240.lua
@@ -0,0 +1,12 @@
+--game: Taxi Life: A City Driving Simulator
+
+addappid(1351240)
+addappid(1351241,0,"9e79fc452f59614517425d51bae47b42451e0a8aaba07aa8b48b756424d68539")
+addappid(2633670)
+addappid(2633680)
+addappid(3197460)
+addappid(3260720)
+addappid(3335520)
+addappid(3384710)
+addappid(3576810)
+addappid(3854360)
