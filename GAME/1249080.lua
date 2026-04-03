c27c0923c321dcdff4a4afff2403922e754dd040
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 01:47:17 -0300

    Sync LUA 1249080 a GAME

---
+++ GAME/1249080.lua
@@ -0,0 +1,6 @@
+--game: Lynn , The Girl Drawn On Puzzles
+
+addappid(1249080)
+addappid(1249081,0,"00507ec8d1e33f96b687161a87713065ebe5e6bc0dea4ac9cc492a29ebbc6bdb")
+addappid(1392270)
+addappid(1392271)
