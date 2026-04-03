f0660bd9a79bdbf26e8d80db4f10b6ecbcde2f70
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 18:50:28 -0300

    Sync LUA 1722860 a GAME

---
+++ GAME/1722860.lua
@@ -0,0 +1,7 @@
+--game: Munchkin Digital
+
+addappid(1722860)
+addappid(1722861,0,"764f0b59ddcdcc6aced2c289b2c6e09ac70ed43d573e3a98dd1e25358f50f0e2")
+addappid(2392090)
+addappid(2840360)
+addappid(3008510)
