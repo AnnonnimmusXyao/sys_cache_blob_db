cf3231ce6ef684e3eb018fb93daa1cb49467e0f9
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 01:49:58 -0300

    Sync LUA 1250410 a GAME

---
+++ GAME/1250410.lua
@@ -0,0 +1,6 @@
+--game: Microsoft Flight Simulator (2020) 40th Anniversary Edition
+
+addappid(1250410)
+addappid(1250411,0,"dbf9d5a5cae7eab44d452bfe53856e4b08b448ac7c224124316ca8cf3f91b3a8")
+addappid(1265910)
+addappid(1265911)
