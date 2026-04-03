cc71e9e5471b7648f17f4cfb739bd5fdb1216a55
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 11:33:47 -0300

    Sync LUA 1521160 a GAME

---
+++ GAME/1521160.lua
@@ -0,0 +1,6 @@
+--game: Are You Smarter Than A 5th Grader
+
+addappid(229007)
+addappid(1521160)
+addappid(1521161,0,"7385945ee6b8e74be4b813dd4e2f900b0d7b38986c0abfcd20b9d323d04921d5")
+addappid(1696640)
