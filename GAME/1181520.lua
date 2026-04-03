7dbc2bffab3eb54f594ec0a13f46587eed5080bd
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 22:59:36 -0300

    Sync LUA 1181520 a GAME

---
+++ GAME/1181520.lua
@@ -0,0 +1,6 @@
+--game: Null & Peta -Invasion of the Queen Bug-
+
+addappid(1181520)
+addappid(229020)
+addappid(1181521,0,"4ea1835f607ff8570492ad8a42b9eb0aa339a7a43fc5993d9cf9cad207803927")
+addappid(1181522,0,"8f907e3a86bcfdad2db13b237a303729756331f0bdb6306c91a18deb8719d227")
