5aac20ef3c9a143f9e1a77174592b7a4421032aa
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 07:09:04 -0300

    Sync LUA 1399080 a GAME

---
+++ GAME/1399080.lua
@@ -0,0 +1,7 @@
+--game: The DioField Chronicle
+
+addappid(1399080)
+addappid(1399081,0,"34ffebca3f2cf35760d220c807c1cc9adf69788c4b80413b640df8f08db09aba")
+addappid(1948770)
+addappid(1953450)
+addappid(1953451)
