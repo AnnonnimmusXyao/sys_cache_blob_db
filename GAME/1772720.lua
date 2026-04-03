e974ac086d371ab70486d7bd43db490f0331f3ae
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 20:30:58 -0300

    Sync LUA 1772720 a GAME

---
+++ GAME/1772720.lua
@@ -0,0 +1,7 @@
+--game: Village of Zombies
+
+addappid(1772720)
+addappid(1772721,0,"c10cdf1f35343819299248aa42306aeaf78f8f5b893e8a52e21e21acc03a1334")
+addappid(1868090)
+addappid(1878870)
+addappid(1981240)
