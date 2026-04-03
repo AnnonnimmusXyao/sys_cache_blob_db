a3f2d09a4a9228ecf51f0ca3ca6f7f529e831c69
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 10:15:08 -0300

    Sync LUA 1486520 a GAME

---
+++ GAME/1486520.lua
@@ -0,0 +1,5 @@
+--game: Mermaid Adventures: The Magic Pearl
+
+addappid(1486520)
+addappid(1486521,0,"d57b2646b97ea38dac65c89c00c6200d10ff1763346730ef4f3aa12beeea8952")
+addappid(1486522,0,"bed891097f85b2e51a1de92cea7ffde7c027cc4d55c84c920056f7e5981481a0")
