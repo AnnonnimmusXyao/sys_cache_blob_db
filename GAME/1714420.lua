7d933b5e9f996f94f9af8a19b60e140fae77e490
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 18:32:33 -0300

    Sync LUA 1714420 a GAME

---
+++ GAME/1714420.lua
@@ -0,0 +1,6 @@
+--game: Fursan al-Aqsa: The Knights of the Al-Aqsa Mosque
+
+addappid(228985)
+addappid(229000)
+addappid(1714420)
+addappid(1714421,0,"c3f26046df5d0e37d19336e829d109041ecedcb2f2f613e45cc3ce2ef13668e3")
