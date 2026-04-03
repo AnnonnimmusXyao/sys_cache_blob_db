bf18058abc22e69c17c9c515a11c1304adedc197
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 20:48:51 -0300

    Sync LUA 1781890 a GAME

---
+++ GAME/1781890.lua
@@ -0,0 +1,8 @@
+--game: The Coma 2B: Catacomb
+
+addappid(1781890)
+addappid(1781891,0,"7695a89bfd4e0d3f27fb4028c30100e4cfda9fdd21241199a99b368961309df1")
+addappid(3267780)
+addappid(3267790)
+addappid(3267800)
+addappid(3267810)
