9c39b4a16dcac793ec714c8f114e0ee07c99950b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 01:50:10 -0300

    Sync LUA 1250520 a GAME

---
+++ GAME/1250520.lua
@@ -0,0 +1,4 @@
+--game: Love Spell: Written In The Stars - a magical romantic-comedy otome
+
+addappid(1250520)
+addappid(1250521,0,"45af76a3a893790e6b2024eb9cae58935d8392d560a3e93224e58d4a4637b50b")
