63781f6b1e5b972cfd4a8a116aa6e9b47b0bf213
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 08:52:08 -0300

    Sync LUA 1448000 a GAME

---
+++ GAME/1448000.lua
@@ -0,0 +1,9 @@
+--game: 迷离诡夜 blurred weird night
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(229007)
+addappid(229012)
+addappid(1448000)
+addappid(1448001,0,"0445234211b4ae1169c59a96c9b6663a4f1893494e0dcae848166d852425a8e4")
