92fb2269f71f9a9995202308c66e447f77637538
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 20:54:58 -0300

    Sync LUA 1785000 a GAME

---
+++ GAME/1785000.lua
@@ -0,0 +1,7 @@
+--game: Mokoko X
+
+addappid(1785000)
+addappid(1785001,0,"d5ff4b99b6816c7d1cbfbf6082f9107417aa758246a12889f21c6a98aac30b00")
+addappid(1791240)
+addappid(1791250)
+addappid(2175140)
