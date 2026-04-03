7cf99a7c3c3404e9757715cf3ac8382d96ef06fe
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 11:12:10 -0300

    Sync LUA 1512050 a GAME

---
+++ GAME/1512050.lua
@@ -0,0 +1,5 @@
+--game: World Turtles
+
+addappid(1512050)
+addappid(1512051,0,"0a4301ca7c3cda8ffcbd7c02f7739f54bc30680b0b623c5db7d340abcad82ece")
+addappid(1512053,0,"6eaffbbe0f288a30f998d3df164fae78dd65373ac21f50dc4b6a527fe2de0391")
