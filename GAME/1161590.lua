4cfcae2df9075c09f471db25dd36ef0d97c6be65
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 22:14:07 -0300

    Sync LUA 1161590 a GAME

---
+++ GAME/1161590.lua
@@ -0,0 +1,8 @@
+--game: Punch Club 2: Fast Forward
+
+addappid(1161590)
+addappid(1161591,0,"9873564f2a9ac4b27fdba0569cbe1af44b087cb3098138889d0c08f04765e57a")
+addappid(1161592,0,"31f371fa499049d473c35fdc5af950c4f1661d8bc609097cd18f99a31e3a0311")
+addappid(2456610)
+addappid(2470820)
+addappid(2923090)
