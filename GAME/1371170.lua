3696b399fd065ef92abe44d498f948baa55de380
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 06:04:03 -0300

    Sync LUA 1371170 a GAME

---
+++ GAME/1371170.lua
@@ -0,0 +1,6 @@
+--game: 3 min per day! Improve your aged eyes(Presbyopia)
+
+addappid(229006)
+addappid(229020)
+addappid(1371170)
+addappid(1371171,0,"041b18f5798fcf3d2b5d6af1ef53a3905d8313d02d44947254f0d705ab7b4a88")
