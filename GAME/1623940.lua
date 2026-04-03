c1f49f18c09f6034917e0ece26955b7a48268a81
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 15:22:28 -0300

    Sync LUA 1623940 a GAME

---
+++ GAME/1623940.lua
@@ -0,0 +1,8 @@
+--game: Bramble: The Mountain King
+
+addappid(229007)
+addappid(229020)
+addappid(229030)
+addappid(1623940)
+addappid(1623941,0,"ecbb0a11ee67b804985728a61d9c2271c604efcf6346f846bbc171060a71f0b8")
+addappid(2362270)
