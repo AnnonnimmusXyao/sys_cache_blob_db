b0fc4e25f58ed7f495984c65fc4d6cb2e9bda316
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 05:20:44 -0300

    Sync LUA 1351000 a GAME

---
+++ GAME/1351000.lua
@@ -0,0 +1,8 @@
+--game: Gangs of Sherwood
+
+addappid(1351000)
+addappid(1351001,0,"b55b3a9859675e8cfccbb2c111ca9987ea4becee8654ea81f98cc17fa510172e")
+addappid(2394550)
+addappid(2432270)
+addappid(2432271)
+addappid(2432272)
