50e439ee97d028ef7110e23f0c5ee667884ccc87
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 12:08:21 -0300

    Sync LUA 1539140 a GAME

---
+++ GAME/1539140.lua
@@ -0,0 +1,7 @@
+--game: STONKS-9800: Stock Market Simulator
+
+addappid(1539140)
+addappid(1539141,0,"88024941c78a93e10cc766fd33a92863fcb184a7c95ec37ba2562bcce041b055")
+addappid(1539142)
+addappid(1539144)
+addappid(3554550)
