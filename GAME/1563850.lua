366f408b435cb21f114d6f355060bacacdf12b7a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 13:03:18 -0300

    Sync LUA 1563850 a GAME

---
+++ GAME/1563850.lua
@@ -0,0 +1,10 @@
+--game: DIY MY LADY
+
+addappid(1563850)
+addappid(1563851,0,"54334ae68ae55897974771e22665fd0750f2d8fc7cb6e35b2238ca572c40b3bd")
+addappid(1698790)
+addappid(1716090)
+addappid(1719920)
+addappid(1723640)
+addappid(1733470)
+addappid(1738250)
