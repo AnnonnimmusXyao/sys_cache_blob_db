1d9369f0914b2d758a42761c54b9e2d18ec491bc
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-02 21:22:53 -0300

    Sync LUA 1148530 a GAME

---
+++ GAME/1148530.lua
@@ -0,0 +1,5 @@
+--game: Griddlers Legend Of The Pirates
+
+addappid(1148530)
+addappid(1148531,0,"e8663379e8f44d427febff9932fd1231d05542a9d5d457664b466fcd47355c8f")
+addappid(1148534,0,"ca9d8c1e5354ed0ad60b0589c9a8fe4cbb82f22ff9c59bc420ca0fe74a853b5d")
