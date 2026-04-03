a8a391592cdc6f70e9055c821c11fc881db721ee
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 16:29:31 -0300

    Sync LUA 1658040 a GAME

---
+++ GAME/1658040.lua
@@ -0,0 +1,5 @@
+--game: I Am Future: Cozy Apocalypse Survival
+
+addappid(1658040)
+addappid(1658041,0,"77ecb47b430884d91e502d8757a26fe9752a4334e7ca64047be83f04827aaf5d")
+addappid(2487020,0,"d99612aa965615d6f1ff622a5b0161799911de6acfcf2e5fa26fd79245bf40c6")
