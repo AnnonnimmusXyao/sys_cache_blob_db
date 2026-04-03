977aa3dd9866685b75747357d97d714dbc01caab
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 16:03:01 -0300

    Sync LUA 1643320 a GAME

---
+++ GAME/1643320.lua
@@ -0,0 +1,9 @@
+--game: S.T.A.L.K.E.R. 2: Heart of Chornobyl
+
+addappid(1643320)
+addappid(1643321,0,"71c29e58911c13991edea02ac06db5f5991e43e52cdcd76c19126d657c4a38e7")
+addappid(1661620)
+addappid(1661621)
+addappid(1661622)
+addappid(1661623)
+addappid(3399310)
