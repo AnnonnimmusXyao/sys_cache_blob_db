a378fb0578c3f3081a33d74e24e4799920b87446
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 02:26:04 -0300

    Sync LUA 1266470 a GAME

---
+++ GAME/1266470.lua
@@ -0,0 +1,8 @@
+--game: Real VR Fishing
+
+addappid(1266470)
+addappid(1266471,0,"0cf32a0440490f0a6f9afbaae490c4a43cbd66ab23333cf0835d0ec93e220a66")
+addappid(3158890)
+addappid(3158900)
+addappid(3158910)
+addappid(3158920)
