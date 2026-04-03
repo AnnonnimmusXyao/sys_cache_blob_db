0d6e3e50a2dbd21efbd2d63d7e21438d18009fbe
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 15:45:46 -0300

    Sync LUA 1634680 a SOFTWARE

---
+++ SOFTWARE/1634680.lua
@@ -0,0 +1,8 @@
+--game: Ficket: 追剧引擎
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(1634680)
+addappid(1634681,0,"28c2d6ab2577fc32273cf08dbf89f2804c40e98e7aed1e07cda7d652aca3b083")
