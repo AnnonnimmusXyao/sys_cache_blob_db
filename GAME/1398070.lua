b344891bfc916049376584b5e5b6d4774e8e338c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 07:06:35 -0300

    Sync LUA 1398070 a GAME

---
+++ GAME/1398070.lua
@@ -0,0 +1,7 @@
+--game: The Book of Bondmaids
+
+addappid(1398070)
+addappid(1398071,0,"d107d7b9a11670a339d2600e66607132f67ef8db5a3c09ab2640e5fcd809b4d3")
+addappid(1838410)
+addappid(2212700)
+addappid(3550830)
