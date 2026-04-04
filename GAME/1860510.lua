8cac1a4242fefc32686969bef75856cd574bf5f6
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 23:38:38 -0300

    Sync LUA 1860510 a GAME

---
+++ GAME/1860510.lua
@@ -0,0 +1,7 @@
+--game: Total Conflict: Resistance
+
+addappid(229003)
+addappid(1860510)
+addappid(1860511,0,"6da400d94115d37c6474b287e5f8e41e78ec2083d54ff7dffd6d1b3b2dc8dc0d")
+addappid(2628270)
+addappid(2808880)
