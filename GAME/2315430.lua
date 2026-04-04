996009813a5f3d69b4864c2657c249aaf8fb3581
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 14:37:40 -0300

    Sync LUA 2315430 a GAME

---
+++ GAME/2315430.lua
@@ -0,0 +1,6 @@
+--game: The Executive - Movie Industry Tycoon
+
+addappid(2315430)
+addappid(2315431,0,"3c10b450fd01b2a223c4efb4b565fafb56309518f23a7475025782f7137cee3c")
+addappid(2315432)
+addappid(2315433)
