86c29e1641976c1f08f4b54ff2ae9f01fb9d8490
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 01:59:03 -0300

    Sync LUA 1924480 a GAME

---
+++ GAME/1924480.lua
@@ -0,0 +1,13 @@
+--game: College Kings 2 - Episode 1
+
+addappid(1924480)
+addappid(1924481,0,"ff635112056ca80dc0760a59372d872768946344dc5d10a5e63676f081245cf7")
+addappid(1929620)
+addappid(2100540)
+addappid(2141200)
+addappid(2141201)
+addappid(2164670)
+addappid(2267960)
+addappid(2725540)
+addappid(3126690)
+addappid(3780980)
