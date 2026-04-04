0234b115baa2044f19548d7258a22fed02fa5a62
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 14:28:01 -0300

    Sync LUA 2310160 a GAME

---
+++ GAME/2310160.lua
@@ -0,0 +1,8 @@
+--game: Operation: Polygon Storm
+
+addappid(2310160)
+addappid(2310161,0,"d9e66eb033b40fd60533dc60970313ca9fa354a0c6edcb559cffea6b9094d0ed")
+addappid(3124490)
+addappid(3216920)
+addappid(3280600)
+addappid(3280610)
