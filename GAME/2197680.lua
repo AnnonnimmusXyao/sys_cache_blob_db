f1e8dfcdec2d56d71fddafe404c18553a8964cf6
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 10:36:10 -0300

    Sync LUA 2197680 a GAME

---
+++ GAME/2197680.lua
@@ -0,0 +1,11 @@
+--game: That Time I Got Reincarnated as a Slime ISEKAI Chronicles
+
+addappid(2197680)
+addappid(2197681,0,"753312d661b8597395743cfeeaa70ac6b7bbf87779edbcc6863c0d88dd046749")
+addappid(2788640)
+addappid(2788650)
+addappid(2788670)
+addappid(2788680)
+addappid(2788690)
+addappid(2788700)
+addappid(2788730)
