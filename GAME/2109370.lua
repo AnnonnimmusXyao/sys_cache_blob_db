4443defbe8debca59bcdd9f1d08446b5d2eb8386
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 07:40:02 -0300

    Sync LUA 2109370 a GAME

---
+++ GAME/2109370.lua
@@ -0,0 +1,7 @@
+--game: The Great War: Western Front™
+
+addappid(2109370)
+addappid(2109371,0,"10ef091d30c140edf4aaae9d81771407eb4c6053d7ed10c256f173c2661d5b6b")
+addappid(2285590)
+addappid(2285591)
+addappid(2361670)
