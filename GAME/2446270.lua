40a73ddc8bf30dc152d9ff9e0f60b7e1e905a9fa
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 19:13:41 -0300

    Sync LUA 2446270 a GAME

---
+++ GAME/2446270.lua
@@ -0,0 +1,5 @@
+--game: 花园魔三国2 -TheSacrificialGirloftheFantasy3Kingdoms 2-
+
+addappid(2446270)
+addappid(2446271,0,"1ec7158f1c1dcb6bfba78443cd698604092d2313993f6b964bb33b5470ac7a8c")
+addappid(2622680)
