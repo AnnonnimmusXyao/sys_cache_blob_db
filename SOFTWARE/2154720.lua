59957676a377525629982611b10441e285e56461
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 09:09:40 -0300

    Sync LUA 2154720 a SOFTWARE

---
+++ SOFTWARE/2154720.lua
@@ -0,0 +1,5 @@
+--game: nofio wireless adapter for Valve Index
+
+addappid(2154720)
+addappid(2154721,0,"68e55c1e985612de9cb3a1771ab8ff8b7fdf2ca8d100dc8ef87c812480f7200b")
+addappid(2154723,0,"b709f60c7b772bdbada2ffb4d8593d048576c2031fcd04895a9c65431cb5c504")
