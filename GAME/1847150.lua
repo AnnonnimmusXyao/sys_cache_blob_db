6db247c83a629bf2bbc6ab6decbb5becc37e5628
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 23:11:29 -0300

    Sync LUA 1847150 a GAME

---
+++ GAME/1847150.lua
@@ -0,0 +1,7 @@
+--game: Touhou Genso Wanderer -FORESIGHT-
+
+addappid(1847150)
+addappid(1847151,0,"9f8b15faa6e7d997f2f98facab170b3ea49259938cf880e23dc362f61e309253")
+addappid(2898290)
+addappid(3068120)
+addappid(3296810)
