db3974c126e73478d1ceeee7bdb28f09482be1c7
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 22:10:31 -0300

    Sync LUA 1819020 a GAME

---
+++ GAME/1819020.lua
@@ -0,0 +1,8 @@
+--game: 救公主？救个屁！ Save the princess？Save a fart！
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(1819020)
+addappid(1819021,0,"569193fd34541c820cdc0f7c7b90e27ac9d3395a89575fb48b80763cf9b13e96")
