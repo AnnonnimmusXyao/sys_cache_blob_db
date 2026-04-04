c1ec364b598e92650d5f5a768c0b514caf76672a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 07:57:40 -0300

    Sync LUA 2116980 a GAME

---
+++ GAME/2116980.lua
@@ -0,0 +1,8 @@
+--game: The Hideaway
+
+addappid(229007)
+addappid(229012)
+addappid(229020)
+addappid(229030)
+addappid(2116980)
+addappid(2116981,0,"dfeb03eeaa6fa6c1722ce473b5eaffab86f171284444e70fa217905ef3c77f28")
