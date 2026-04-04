0a5bf30f0046e57e3ccafed456880cec96251d28
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 02:50:00 -0300

    Sync LUA 1948450 a GAME

---
+++ GAME/1948450.lua
@@ -0,0 +1,9 @@
+--game: Sex Chess
+
+addappid(229007)
+addappid(229012)
+addappid(229020)
+addappid(229030)
+addappid(1948450)
+addappid(1948451,0,"cbf3112f8ea4f55627828f036dc614ac52c6f603f6131cb141ad5fd15707316e")
+addappid(2232700)
