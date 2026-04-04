ffd9665341520ecacbc0cc754dfd24c64d38f110
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 17:20:08 -0300

    Sync LUA 2389240 a GAME

---
+++ GAME/2389240.lua
@@ -0,0 +1,8 @@
+--game: G.O.P.O.T.A
+
+addappid(229007)
+addappid(229012)
+addappid(229030)
+addappid(2389240)
+addappid(2389241,0,"9715448e13f9239fe300750f9469f9813576a81a9336d3f139cd515518e1bc3c")
+addappid(2416750)
