b42e59995055e189aef56ae32a70ed3fe62c2a53
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 08:20:18 -0300

    Sync LUA 2129160 a GAME

---
+++ GAME/2129160.lua
@@ -0,0 +1,7 @@
+--game: Blue Wednesday
+
+addappid(229006)
+addappid(229007)
+addappid(2129160)
+addappid(2129161,0,"f1a64d60857c881fab53f59b145ebb961f0283d1decf599bd3efffe5efe8d2f3")
+addappid(2580470)
