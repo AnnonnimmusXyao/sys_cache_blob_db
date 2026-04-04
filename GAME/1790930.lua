74a556046c2162921580ef1ef80c211e8794d96b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 21:08:39 -0300

    Sync LUA 1790930 a GAME

---
+++ GAME/1790930.lua
@@ -0,0 +1,5 @@
+--game: Crisol: Theater of Idols
+
+addappid(1790930)
+addappid(1790930,1,"ed878ef37616a334a9f2769ac5b754ece33e0099b1e11090ac3dcee0d6bed668")
+addappid(1790931,0,"1004ecdf80e18a409de92e25b9422c289ffae11c70dc4480fcb2ac76f4f77f39")
