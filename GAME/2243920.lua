bf55bc186818258b24a97ef80e78f770fdbfd748
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 12:16:14 -0300

    Sync LUA 2243920 a GAME

---
+++ GAME/2243920.lua
@@ -0,0 +1,4 @@
+--game: The 2nd page of the medical examination diary: Another story of exciting days of me and my senpai
+
+addappid(2243920)
+addappid(2243921,0,"07c82b79fdfa024eda4b10cbe7f6963c4fd1a0630617b399987a29709887997b")
