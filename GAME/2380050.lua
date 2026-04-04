a558a3d9509875d9d8038e62d543d4902fcdf35e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 17:01:03 -0300

    Sync LUA 2380050 a GAME

---
+++ GAME/2380050.lua
@@ -0,0 +1,9 @@
+--game: Star Trucker
+
+addappid(2380050)
+addappid(2380051,0,"a692f8ffe9b6df42b1c9344a8d1f0f92109fc67c1d094414b767b5e6a641d27a")
+addappid(2873570)
+addappid(3088950)
+addappid(3218640)
+addappid(3285520)
+addappid(3933200)
