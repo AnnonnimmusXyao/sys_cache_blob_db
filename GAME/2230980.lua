7f6f7f8fb7b28912517a786717cf078a0c22e409
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 11:51:11 -0300

    Sync LUA 2230980 a GAME

---
+++ GAME/2230980.lua
@@ -0,0 +1,11 @@
+--game: Caribbean Legend
+
+addappid(2230980)
+addappid(2230981,0,"e7fe65b71497e767bb8766c4fe56dc415b2d80b04c17e0b513f8f63c913fc4f4")
+addappid(2230982,0,"91bee1c1f2bfb0853316a2d1104ede22aaee2beca78ccecfbbc4c586eee29a8d")
+addappid(2628610)
+addappid(2749310)
+addappid(3162620)
+addappid(3398030)
+addappid(3549890)
+addappid(3549900)
