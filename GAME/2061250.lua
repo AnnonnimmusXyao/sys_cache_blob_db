610a3ad8836f5f409e9b271d0abaa8ebfd8d7597
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 05:56:57 -0300

    Sync LUA 2061250 a GAME

---
+++ GAME/2061250.lua
@@ -0,0 +1,4 @@
+--game: Shin chan: Me and the Professor on Summer Vacation The Endless Seven-Day Journey
+
+addappid(2061250)
+addappid(2061251,0,"9ad04720ada19b496c5036cb51aa4a727363151e3bbc6a088dc225d8543561a0")
