dac91fe891ce8cd4c85a5e02cc2376aa0416c7c8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 17:12:28 -0300

    Sync LUA 2385530 a GAME

---
+++ GAME/2385530.lua
@@ -0,0 +1,14 @@
+--game: PGA TOUR 2K25
+
+addappid(2385530)
+addappid(2385531,0,"891c9c194df2d680a3dbe94e736933f9252184a9680761d1cd6cef9fd178a77a")
+addappid(2385540)
+addappid(2385541)
+addappid(2385570)
+addappid(2385571)
+addappid(2385580)
+addappid(3226840)
+addappid(3226860)
+addappid(3326590)
+addappid(3337880)
+addappid(3337890)
