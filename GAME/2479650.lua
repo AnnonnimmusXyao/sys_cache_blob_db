1814599fed1c8e8104c13a38cad6006c7ad2c561
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 20:19:22 -0300

    Sync LUA 2479650 a GAME

---
+++ GAME/2479650.lua
@@ -0,0 +1,7 @@
+--game: SpongeBob SquarePants: Titans of the Tide
+
+addappid(2479650)
+addappid(2479651,0,"7791fd462f31f7a64bf9748c67ab169f43843783691e9637c12074bdb8981fcc")
+addappid(3840410)
+addappid(3870760)
+addappid(3870880)
