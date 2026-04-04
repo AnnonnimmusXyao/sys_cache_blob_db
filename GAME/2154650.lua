4a9e93a7318832a416d515d324034dfef4569083
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 09:09:28 -0300

    Sync LUA 2154650 a GAME

---
+++ GAME/2154650.lua
@@ -0,0 +1,7 @@
+--game: Pawperty Damage
+
+addappid(2154650)
+addappid(2154651,0,"55009fd965ba13552be86989003aa5537cb28f1dd7a30d76b1a9305b3ed1a562")
+addappid(2498060)
+addappid(3217790)
+addappid(3311750)
