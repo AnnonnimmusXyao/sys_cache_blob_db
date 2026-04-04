b7ba4eee328e60fa39e3dff5acadfd6dfcebf078
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 00:19:40 -0300

    Sync LUA 1875720 a GAME

---
+++ GAME/1875720.lua
@@ -0,0 +1,8 @@
+--game: 动物园怪谈
+
+addappid(229007)
+addappid(229012)
+addappid(229020)
+addappid(229030)
+addappid(1875720)
+addappid(1875722,0,"157dddf42a064e3c12f45b80953785894d902784a6c3bff3d9d9cee304d87ef5")
