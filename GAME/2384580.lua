f39ad14ea0c0f116c6e558aca032a065e7829e7e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 17:10:33 -0300

    Sync LUA 2384580 a GAME

---
+++ GAME/2384580.lua
@@ -0,0 +1,16 @@
+--game: DYNASTY WARRIORS: ORIGINS
+
+addappid(2384580)
+addappid(2384581,0,"c9bfe8a78dc43f75438b3a8e602c7a322b0b53418f80be108058121a71371130")
+addappid(2530290)
+addappid(3319840)
+addappid(3319850)
+addappid(3319860)
+addappid(3319870)
+addappid(3319880)
+addappid(3319890)
+addappid(3319900)
+addappid(3319910)
+addappid(3319920)
+addappid(3319930)
+addappid(3319940)
