7538f7e5e35cf8e46c1264b9a7105d262e10a6a1
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 01:35:17 -0300

    Sync LUA 1913620 a GAME

---
+++ GAME/1913620.lua
@@ -0,0 +1,4 @@
+--game: Puzzle Bobble™2X/BUST-A-MOVE™2 Arcade Edition & Puzzle Bobble™3/BUST-A-MOVE™3 S-Tribute
+
+addappid(1913620)
+addappid(1913621,0,"0cfd8d439500081e3970ec7f4f5b22befc24ebf910cb7b138d7441cf8777a3a2")
