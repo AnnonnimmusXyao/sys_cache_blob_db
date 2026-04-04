c2e2b2847a4a532c6ca3a5936cf33fca015f4fb2
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 00:55:19 -0300

    Sync LUA 1894100 a GAME

---
+++ GAME/1894100.lua
@@ -0,0 +1,9 @@
+--game: Days with Evraco: The Blind witch in Blue Forest
+
+addappid(1894100)
+addappid(1894107,0,"837c377fbb65e1dc775bcec92620f862277617fc72590da31b3104b83b558d73")
+addappid(3014990)
+addappid(3019050)
+addappid(3097020)
+addappid(3153230)
+addappid(3347880)
