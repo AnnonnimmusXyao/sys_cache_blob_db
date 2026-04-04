708b2d98bad4d2e6ba518094eb5395d0216cdfd5
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 23:00:09 -0300

    Sync LUA 1841520 a GAME

---
+++ GAME/1841520.lua
@@ -0,0 +1,8 @@
+--game: 月影待蚀 / The Incomplete Lunar
+
+addappid(229007)
+addappid(229012)
+addappid(229020)
+addappid(229033)
+addappid(1841520)
+addappid(1841521,0,"016982737a4c2d95afab09a6b939342adf93adbf718abe561a6997e3afa46446")
