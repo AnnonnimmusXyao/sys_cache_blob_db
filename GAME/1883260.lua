ae85d9453c9d9f473278f62027f9358f56072db7
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 00:35:05 -0300

    Sync LUA 1883260 a GAME

---
+++ GAME/1883260.lua
@@ -0,0 +1,6 @@
+--game: Little Noah: Scion of Paradise
+
+addappid(1883260)
+addappid(1883261,0,"64e96126cefb28a7ab462c8a1c6c894597f99b34dbaa22ef8cf444f0a2d43c8c")
+addappid(2055110)
+addappid(2058630)
