ab09cf4c72b4b6278ab7fc0321234eae0659e2c1
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 20:48:07 -0300

    Sync LUA 2494450 a GAME

---
+++ GAME/2494450.lua
@@ -0,0 +1,10 @@
+--game: The Last Soldier of the Ming Dynasty
+
+addappid(228983)
+addappid(228985)
+addappid(229007)
+addappid(229033)
+addappid(2494450)
+addappid(2494451,0,"962d06317910f44ab5bd3017cbe4aa004b0a8428cf5ed17160a2d31d216a3446")
+addappid(3090520)
+addappid(3173800)
