e607942d83677c6506d680d87236a78c5e6b245b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 22:08:07 -0300

    Sync LUA 1817800 a GAME

---
+++ GAME/1817800.lua
@@ -0,0 +1,9 @@
+--game: Oddsparks: An Automation Adventure
+
+addappid(1817800)
+addappid(1817801,0,"b59ebe145d953e10adb462fd930cbf0da37483e64c0896babf1861638f87b6a5")
+addappid(3073500)
+addappid(3678620)
+addappid(3678630)
+addappid(3678640)
+addappid(3678650)
