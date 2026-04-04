7872943e96fbc2826f145e1acff3708433ba7762
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 13:24:21 -0300

    Sync LUA 2277560 a GAME

---
+++ GAME/2277560.lua
@@ -0,0 +1,9 @@
+--game: WUCHANG: Fallen Feathers
+
+addappid(2277560)
+addappid(2277561,0,"6db3af8b5be3def24c758a005feb7fa0f65750716392328e2f14321340adb273")
+addappid(3653760)
+addappid(3653770)
+addappid(3653780)
+addappid(3653790)
+addappid(3986030)
