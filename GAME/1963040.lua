586dc1b4fb84cff156e2dc6551ca8b9f381b4113
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 03:17:43 -0300

    Sync LUA 1963040 a GAME

---
+++ GAME/1963040.lua
@@ -0,0 +1,13 @@
+--game: The Lost Village
+
+addappid(229005)
+addappid(229012)
+addappid(229020)
+addappid(229030)
+addappid(1963040)
+addappid(1963041,0,"0960237e0bdfa0375c4556be4c342956e5f6d446657bc4d0a9644f32316b9c76")
+addappid(2850340)
+addappid(2917210)
+addappid(3227480)
+addappid(3348000)
+addappid(3600860)
