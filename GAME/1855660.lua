1fa2e2da04e50d07d80728483ea8ab59287794b4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 23:29:01 -0300

    Sync LUA 1855660 a GAME

---
+++ GAME/1855660.lua
@@ -0,0 +1,6 @@
+--game: 我有一座冒险屋 I have an adventure house
+
+addappid(1855660)
+addappid(228983)
+addappid(228985)
+addappid(1855661,0,"5306e95f34f91c84c446fb6bbf319d799e517e58fa7415dd7d6412c641e5df43")
