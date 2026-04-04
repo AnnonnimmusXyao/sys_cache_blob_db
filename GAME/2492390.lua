2b75c797837629624d57461c823942d082db385f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 20:44:45 -0300

    Sync LUA 2492390 a GAME

---
+++ GAME/2492390.lua
@@ -0,0 +1,9 @@
+--game: Logiart Grimoire
+
+addappid(2492390)
+addappid(2492392,0,"339472ad51cec8d17a8ce990fa1597634a3c33243a6a0ebd323a83f574520cd7")
+addappid(3253250)
+addappid(3253260)
+addappid(3253270)
+addappid(3336380)
+addappid(3336390)
