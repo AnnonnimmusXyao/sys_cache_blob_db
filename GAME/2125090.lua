2150702fc90ad0f79e6184ba7c8aa2f4d086c442
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 08:12:13 -0300

    Sync LUA 2125090 a GAME

---
+++ GAME/2125090.lua
@@ -0,0 +1,6 @@
+--game: Tintin Reporter - Cigars of the Pharaoh
+
+addappid(228983)
+addappid(228985)
+addappid(2125090)
+addappid(2125091,0,"979ee4fcab4ba27bf6ae66d7f30fd6fd4803bad91600833952ce39568bd11e96")
