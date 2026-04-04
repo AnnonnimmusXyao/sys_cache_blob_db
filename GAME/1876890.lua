91426e820430642053e07bca951daf6f813207a7
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 00:22:00 -0300

    Sync LUA 1876890 a GAME

---
+++ GAME/1876890.lua
@@ -0,0 +1,7 @@
+--game: Wandering Sword
+
+addappid(1876890)
+addappid(1876891,0,"40985dfc0fbcd23c2b692a036c22fad2ad18d03794b58ea33fd079acbfb53c5d")
+addappid(3027840)
+addappid(3600640)
+addappid(3600650)
