e5e6ed9c631af1eacec29a66cf511a583b6252d5
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 21:40:31 -0300

    Sync LUA 1805260 a GAME

---
+++ GAME/1805260.lua
@@ -0,0 +1,4 @@
+--game: Mommy-Goddess of Unconditional Love ~Wow, You Sure Gave It Your All Out There!~
+
+addappid(1805260)
+addappid(1805261,0,"e25c543945b29b90c452daf42e07082c7062e1b291f79c39d09c08d81ab07087")
