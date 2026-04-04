7dbee74f7d1718dbed541f3b2d647ce65e6d8d24
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 08:47:43 -0300

    Sync LUA 2143680 a GAME

---
+++ GAME/2143680.lua
@@ -0,0 +1,6 @@
+--game: 梦灯花 Noctuary
+
+addappid(2143680)
+addappid(3879310)
+addappid(2875660,0,"953808f75dcbd30bfbf952489377636f15eda238c4cbebf059d223f9c0afab09")
+addappid(2143681,0,"f5d8ce3a9f4f95d8e0a4367c3add3fc756cc8e301455dac56682f2ebe15bd74f")
