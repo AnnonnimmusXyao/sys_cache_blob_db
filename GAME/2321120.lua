df7e262b8a488719b92d2b06755bb24d58997c4b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 14:48:13 -0300

    Sync LUA 2321120 a GAME

---
+++ GAME/2321120.lua
@@ -0,0 +1,7 @@
+--game: Voice Love on Air
+
+addappid(2321120)
+addappid(2321121,0,"1d7072e2c9d2388fa7e05e5bab9ca43ec79cdc2d3270c3de29886d6fff26fc70")
+addappid(2875420)
+addappid(3052430)
+addappid(3058340)
