996a7898bd8b6e552158840814e9df89598b2bb0
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 09:10:38 -0300

    Sync LUA 2155060 a GAME

---
+++ GAME/2155060.lua
@@ -0,0 +1,10 @@
+--game: Heartbreak Hotel
+
+addappid(228983)
+addappid(228985)
+addappid(229004)
+addappid(229005)
+addappid(229006)
+addappid(229007)
+addappid(2155060)
+addappid(2155061,0,"3d7ac3b4d86b2d4c98abb60237ce65b01940cf47881e32f05cf8bce4f93a592a")
