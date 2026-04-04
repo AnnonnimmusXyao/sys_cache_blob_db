76cd6a61505d42725f453482c81270c7547544fe
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 18:15:46 -0300

    Sync LUA 2417610 a GAME

---
+++ GAME/2417610.lua
@@ -0,0 +1,6 @@
+--game: METAL GEAR SOLID Δ: SNAKE EATER
+
+addappid(2417610)
+addappid(2417611,0,"9a9b7b9956ecfecde761c3ec639b3c9efcc0e38d3b11a153d66014a015a8004d")
+addappid(2881290)
+addappid(3364840)
