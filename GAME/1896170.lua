10891be130c72156e14c55b1ee8170ffc6a554f4
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 00:59:17 -0300

    Sync LUA 1896170 a GAME

---
+++ GAME/1896170.lua
@@ -0,0 +1,6 @@
+--game: Prince of Suburbia - Part 1
+
+addappid(1896170)
+addappid(1896171,0,"b210131310ba6e0161da389a200c7ca8915bdf84aaa88eda3d362f8ed2aa0d82")
+addappid(2149280)
+addappid(3593230)
