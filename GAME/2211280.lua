af4e1c26e9c4bd0f3b2a5dd04bf302565c920b5a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 11:03:34 -0300

    Sync LUA 2211280 a GAME

---
+++ GAME/2211280.lua
@@ -0,0 +1,13 @@
+--game: Roller Champions™
+
+addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+addappid(2211280)
+addappid(2212900)
+addappid(2212901)
+addappid(2212902)
+addappid(2212903)
+addappid(2236160)
+addappid(2369340)
+addappid(2369341)
+addappid(2369342)
+addappid(2369343)
