51015c7c8a3dec10915f548199d4baeb2779096f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 04:54:46 -0300

    Sync LUA 2010900 a GAME

---
+++ GAME/2010900.lua
@@ -0,0 +1,6 @@
+--game: 后汉稽异录 - Obscure Chronicle of Dynasty
+
+addappid(2010900)
+addappid(2010901,0,"c3f53ddfca46d00c2c3f9d50d36167d936c18e55cab77e90133bfb10aaf9ee2f")
+addappid(2674440)
+addappid(2694320)
