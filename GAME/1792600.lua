e5a2a76d6ff599af6e66f45aa2d4e5d0916abc53
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-03 21:11:42 -0300

    Sync LUA 1792600 a GAME

---
+++ GAME/1792600.lua
@@ -0,0 +1,7 @@
+--game: Dusk Diver 2
+
+addappid(1792600)
+addappid(1792601,0,"8434d696dd21d12dca0589e96bffb42bee74bb26838a8dd9d7860c641ed76794")
+addappid(1919820)
+addappid(1930530)
+addappid(1930660)
