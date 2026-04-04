35302b32fc509b141b5a839c8a93815137b32a6d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 14:49:01 -0300

    Sync LUA 2321470 a GAME

---
+++ GAME/2321470.lua
@@ -0,0 +1,6 @@
+--game: Deep Rock Galactic: Survivor
+
+addappid(2321470)
+addappid(2321471,0,"18154d412c1c3c3719d8f05c119354536a5228cdd169ca75488708a215c1cddf")
+addappid(2782850)
+addappid(3818560)
