71e629c8b2b380a0613bf0387b798b7a9a4e96f9
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 04:29:38 -0400

    Sync LUA 2773230 a GAME

---
+++ GAME/2773230.lua
@@ -0,0 +1,7 @@
+--game: 18层:忘忧 18Hell:Lost
+
+addappid(229000)
+addappid(229006)
+addappid(229030)
+addappid(2773230)
+addappid(2773232,0,"5a63ea2ee809ae9135b018a223200ad4fec73ed4ad49702fedebd2665ea1d400")
