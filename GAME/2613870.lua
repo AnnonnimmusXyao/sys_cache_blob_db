0964ae03e15f06a1f3654f1f219e6f7e85205272
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 23:24:34 -0400

    Sync LUA 2613870 a GAME

---
+++ GAME/2613870.lua
@@ -0,0 +1,11 @@
+--game: BattleCore Arena
+
+addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+addappid(2613870)
+addappid(2781410)
+addappid(2826340)
+addappid(2826350)
+addappid(2826360)
+addappid(2826370)
+addappid(2842220)
+addappid(2842230)
