56c9ddb6947465031b7c6a27a0e19c8b8f4121ee
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 11:34:36 -0400

    Sync LUA 2986260 a GAME

---
+++ GAME/2986260.lua
@@ -0,0 +1,5 @@
+--game: Dreamlike Love with Seira
+
+addappid(2986260)
+addappid(2986261,0,"446ed15ab2cdaa4d73ba54453a09f6b9aef798107b33e5ba94d483cf208c797e")
+addappid(2986263,0,"d23bcf75695d713847e34ed80fffc7edeb2430369f8ca4a7e22887003d29d64b")
