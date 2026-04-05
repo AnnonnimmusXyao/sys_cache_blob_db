739fb3ed277b4a287b5e071919abe00a486d05bc
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 23:43:58 -0300

    Sync LUA 2591070 a GAME

---
+++ GAME/2591070.lua
@@ -0,0 +1,8 @@
+--game: Piczle Cross: Story of Seasons
+
+addappid(229004)
+addappid(229005)
+addappid(229006)
+addappid(229007)
+addappid(2591070)
+addappid(2591071,0,"dac632edd14b74c50d53c006a61c69ffe719d1365db2cfc835337b9f5a29f1f4")
