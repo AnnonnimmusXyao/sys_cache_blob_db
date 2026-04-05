b82e9f04d8544c4204612e0487493c1fe58f17fb
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 03:51:01 -0400

    Sync LUA 2751320 a GAME

---
+++ GAME/2751320.lua
@@ -0,0 +1,11 @@
+--game: Johnny Trigger
+
+addappid(229006)
+addappid(2751320)
+addappid(2751321,0,"3e7cd8ad11ea3904f9fb7a09c53c2fd3210f9be3f33c45146ce3c4dadc4fec41")
+addappid(2821540)
+addappid(2821550)
+addappid(2821560)
+addappid(2821570)
+addappid(2821580)
+addappid(2821590)
