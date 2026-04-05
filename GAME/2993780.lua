7a6a684d742945c4d44c4ace9b57e6b82b1588af
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 11:57:22 -0400

    Sync LUA 2993780 a GAME

---
+++ GAME/2993780.lua
@@ -0,0 +1,8 @@
+--game: FANTASY LIFE i: The Girl Who Steals Time
+
+addappid(228989)
+addappid(228990)
+addappid(229007)
+addappid(2993780)
+addappid(2993781,0,"ad98d2baa7c277a9a3d59599e17cb30eb75781386d8c475317d4570b8dc72aaf")
+addappid(3464380)
