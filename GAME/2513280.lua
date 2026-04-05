9b759b93dda0d946f67e5f22c90267d98ecf7faa
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 21:22:03 -0300

    Sync LUA 2513280 a GAME

---
+++ GAME/2513280.lua
@@ -0,0 +1,9 @@
+--game: SONIC X SHADOW GENERATIONS
+
+addappid(2513280)
+addappid(2513281,0,"e5b9cddfe91ed04af8c6d17537301797ab16a5041a7edca1d45ae281208a740f")
+addappid(2909770)
+addappid(2909780)
+addappid(2909790)
+addappid(2983600)
+addappid(2999160)
