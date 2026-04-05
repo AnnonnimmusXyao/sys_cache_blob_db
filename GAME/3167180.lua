ec65d8f69291f3de71f3a2138d2daff4d7bf899b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 17:14:18 -0400

    Sync LUA 3167180 a GAME

---
+++ GAME/3167180.lua
@@ -0,0 +1,9 @@
+--game: 你好！我们还有场恋爱没谈
+
+addappid(3167180)
+addappid(3167181,0,"f61ad6c1640cf36271016dc035341874c16ab54aabaca29279cfc49f05b68741")
+addappid(3167182)
+addappid(3167183)
+addappid(3167184)
+addappid(3167185)
+addappid(3167186)
