284a50217896fef1f6a9a73887f3946ff991911f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 05:33:32 -0400

    Sync LUA 2808570 a GAME

---
+++ GAME/2808570.lua
@@ -0,0 +1,6 @@
+--game: KunKun dream Startles the Soul
+
+addappid(2808570)
+addappid(2808572,0,"9cb2adadc8637c2632dce8be4859287fe6e868a11f81f1ade65d699cbc724e14")
+addappid(3033330)
+addappid(3033720)
