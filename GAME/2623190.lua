b97daa50ae6b4ad5b0e9c9fd04aafa4c2082613a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 23:40:44 -0400

    Sync LUA 2623190 a GAME

---
+++ GAME/2623190.lua
@@ -0,0 +1,6 @@
+--game: The Elder Scrolls IV: Oblivion Remastered
+
+addappid(2623190)
+addappid(2623191,0,"35dc19b14e7481262176a54f0a9de8f9e76c3dcf493a26fa085ed999e684b5f7")
+addappid(2988900)
+addappid(3533970)
