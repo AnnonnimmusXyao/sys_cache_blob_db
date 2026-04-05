dd04c2134d122dde5f397b3dd87f06b0ce872cc3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 18:14:28 -0400

    Sync LUA 3201910 a GAME

---
+++ GAME/3201910.lua
@@ -0,0 +1,5 @@
+--game: RailGods of Hysterra
+
+addappid(3201910)
+addappid(3201911,0,"46a048eabbe26d9436a14036da2caa89bb0aedc6120abe014d8a647ca5abb411")
+addappid(3601390,0,"64ba32802db186b2061a124acd28e1c105394eaee551f5249aa0bdba82704e39")
