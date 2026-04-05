900464cd991281e74fcbed7e8308dbf261776d2c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 01:43:53 -0400

    Sync LUA 2688950 a GAME

---
+++ GAME/2688950.lua
@@ -0,0 +1,8 @@
+--game: Planet Coaster 2
+
+addappid(2688950)
+addappid(2688951,0,"d1fc4044f8dbec56ccd7ccac0a38810470c5fecf00dedc36a294c39453f9adb0")
+addappid(3091180)
+addappid(3091190)
+addappid(3337040)
+addappid(3902620)
