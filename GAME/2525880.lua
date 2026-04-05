a59012adc37c730f649114bed9deb089a9febea1
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 21:45:01 -0300

    Sync LUA 2525880 a GAME

---
+++ GAME/2525880.lua
@@ -0,0 +1,8 @@
+--game: Sanguo's Ambition 4 :Three Kingdoms
+
+addappid(228981)
+addappid(228982)
+addappid(228983)
+addappid(228985)
+addappid(2525880)
+addappid(2525881,0,"7b0b08d2cf9f33ec0aa12def14e974f1557a111ac700228f0ba38aa57b3ade6b")
