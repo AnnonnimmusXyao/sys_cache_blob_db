a4347e7448f72411e307706f9ce310632bfba52f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 05:01:15 -0400

    Sync LUA 2790000 a GAME

---
+++ GAME/2790000.lua
@@ -0,0 +1,6 @@
+--game: Sin Slayers: Reign of The 8th
+
+addappid(229000)
+addappid(2790000)
+addappid(3313980)
+addappid(3313990,0,"31dfa1391278b513b1682e7fd9202ed850d7c5ce31e07610d4f4541202c4c037")
