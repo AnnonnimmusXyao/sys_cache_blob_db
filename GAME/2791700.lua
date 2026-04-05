955eaaae2b68d74795b3b6e2cf527a86f9dc8a9a
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 05:04:17 -0400

    Sync LUA 2791700 a GAME

---
+++ GAME/2791700.lua
@@ -0,0 +1,7 @@
+--game: Infantry Attack
+
+addappid(2791700)
+addappid(2791701,0,"12642b33843bc523f6a5daf518c326c5ac04348b5a10db889642e67e9e16c0b8")
+addappid(2792200)
+addappid(2792210)
+addappid(2792220)
