16dbef3a2a9335870adfa597d766529175840f39
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 19:33:31 -0400

    Sync LUA 3245380 a GAME

---
+++ GAME/3245380.lua
@@ -0,0 +1,4 @@
+--game: They Do Not Die: Call of Crimson Tgirl Apex Land Zombie Shooter Duty 2D Keywords
+
+addappid(3245380)
+addappid(3245381,0,"9817d6be6da4067bc2fc7c381300c218d2a27cd83cf70e2736eee9f55c26ce32")
