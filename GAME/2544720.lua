6aa5a3d8879b2cf777087e7af8629150ad94c90f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 22:21:13 -0300

    Sync LUA 2544720 a GAME

---
+++ GAME/2544720.lua
@@ -0,0 +1,8 @@
+--game: Workplace Fantasy
+
+addappid(2544720)
+addappid(2544721,0,"8efd5e37dbb85b2b655f5332cb3c5060ffa02e17f594658dfb45a8fad670b98b")
+addappid(2626390)
+addappid(2626400)
+addappid(3040550)
+addappid(3606670)
