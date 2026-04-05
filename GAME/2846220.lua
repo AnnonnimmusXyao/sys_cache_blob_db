ccbfd2b32ab251d5f7f5cc1f027b48ace8e514bd
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 06:58:16 -0400

    Sync LUA 2846220 a GAME

---
+++ GAME/2846220.lua
@@ -0,0 +1,5 @@
+--game: Together with Oneesan～Yuina's Sweet Encouragement～
+
+addappid(2846220)
+addappid(2846221,0,"44ef95668a222003eeda4bdf982dad64225e46476fe3bfe7381f66459a7e6c2b")
+addappid(3600610)
