adc3c579a917a6881cdcb675d55f562e9b506819
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 06:26:13 -0400

    Sync LUA 2829790 a GAME

---
+++ GAME/2829790.lua
@@ -0,0 +1,5 @@
+--game: Windstorm: Start of a Great Friendship - Remastered
+
+addappid(2829790)
+addappid(2829791,0,"f205e2369971a3648f4ecfa09318f96511a707f005a067f90384a8dedc8c1d34")
+addappid(3145870)
