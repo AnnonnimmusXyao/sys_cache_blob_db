d8ca5a818d464da5ef466a8cefca0fe94353de64
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-04 23:01:44 -0400

    Sync LUA 2600670 a GAME

---
+++ GAME/2600670.lua
@@ -0,0 +1,9 @@
+--game: Vanity
+
+addappid(2600670)
+addappid(2600671,0,"30574fcac232fa312e1a55508a7a9f9d9628d8fadd37a077ca246cadd981a392")
+addappid(2659130)
+addappid(2663900)
+addappid(2718630)
+addappid(2718640)
+addappid(2999140)
