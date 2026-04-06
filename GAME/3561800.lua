31fc5a782930da525f885cb0fdaac3837f7a0157
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-06 04:50:30 -0400

    Sync LUA 3561800 a GAME

---
+++ GAME/3561800.lua
@@ -0,0 +1,5 @@
+--game: Hero in an All-Forgiving Fantasy World RPG
+
+addappid(3561800)
+addappid(3561802,0,"e174cca35c4fec9973346dc5bacecfb603fb5d82f44177a5deb006d5ee738d1e")
+addappid(3561803,0,"2a7fc44432356622eaf954204a49a1361e1fa673fca2d0f81215306c9a9c9659")
