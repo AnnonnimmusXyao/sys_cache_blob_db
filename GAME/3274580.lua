f3026fb93f5bfbf86aabdb7e810a4713a55522e9
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 20:21:15 -0400

    Sync LUA 3274580 a GAME

---
+++ GAME/3274580.lua
@@ -0,0 +1,12 @@
+--game: Anno 117: Pax Romana
+
+addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+addappid(3274580)
+addappid(3274581,0,"29f1b2f2b84b5981fe5e3ebadc14b925e747eea87b6556a95001d576acb9beb9")
+addappid(3749050)
+addappid(3749060)
+addappid(3749080)
+addappid(3749090)
+addappid(3875910)
+addappid(4168120)
+addappid(4168130)
