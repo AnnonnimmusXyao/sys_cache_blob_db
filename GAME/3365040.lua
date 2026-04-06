4d4035eb55056fa0e8aa634a33d3e388343797ee
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-05 23:21:30 -0400

    Sync LUA 3365040 a GAME

---
+++ GAME/3365040.lua
@@ -0,0 +1,5 @@
+--game: Atelier Ryza 2: Lost Legends & the Secret Fairy DX
+
+addappid(3365040)
+addappid(3365041,0,"d598945b8eadbb0ec9bad54928aeaa1549a79faa8df45f1d1d6d83c0d5127cbd")
+addappid(3782460)
