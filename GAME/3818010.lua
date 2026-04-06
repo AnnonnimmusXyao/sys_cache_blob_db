60a53670f32e488ba0d9f4beb062db0a8946da26
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-06 12:19:38 -0400

    Sync LUA 3818010 a GAME

---
+++ GAME/3818010.lua
@@ -0,0 +1,11 @@
+--game: BEYBLADE X EVOBATTLE
+
+addappid(3818010)
+addappid(3818011,0,"9e190331a1336edfff4571412d26622426d9895a4bf952d4c2d58470fd4c0780")
+addappid(4128510)
+addappid(4128520)
+addappid(4128530)
+addappid(4128540)
+addappid(4128550)
+addappid(4128560)
+addappid(4139560)
