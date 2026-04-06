9fb18bf1d09443a3a133e45f98ad2c2e62a2f0bb
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-06 06:05:47 -0400

    Sync LUA 3606890 a GAME

---
+++ GAME/3606890.lua
@@ -0,0 +1,7 @@
+--game: Upload Labs
+
+addappid(3606890)
+addappid(3606891,0,"37cce33183ad0a0855c91edbd4e8920e5537e2b66c1ec405cac79bc6e9407e46")
+addappid(3606990)
+addappid(3958950)
+addappid(3958960)
