9c4397c9ae1b70eae78fcd64ca45596aa4ba45e7
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-06 12:35:49 -0400

    Sync LUA 3828900 a GAME

---
+++ GAME/3828900.lua
@@ -0,0 +1,7 @@
+--game: Elven Holes Must Fall
+
+addappid(3828900)
+addappid(3828901,0,"c018f2b320fb54c0efe25dbe0e760280f70a66fd1dd19077110ab093a0b73fe9")
+addappid(3970540)
+addappid(3970550)
+addappid(3973830)
