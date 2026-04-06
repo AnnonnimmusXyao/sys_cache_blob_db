d93fb0647ec57518cce4011d3720c1edae3d378d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-04-06 04:51:42 -0400

    Sync LUA 3562310 a GAME

---
+++ GAME/3562310.lua
@@ -0,0 +1,5 @@
+--game: Trials of interspecies Sisters~Erotic exchange with Humans~
+
+addappid(3562310)
+addappid(3562312,0,"e118c7e559cd4092d8c0e7da7ff01e0950df15e4f9c2ab5fcde4780bdfe74ace")
+addappid(3562314,0,"eb4998add99536e229fb1c26ba90e0a92e0d26a4c2b85fc0a73f667cef68c5d4")
