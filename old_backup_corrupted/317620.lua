bfe9aa9cd3a00fc058326c32f67bb8572909171d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-05-17 16:46:43 -0400

    Auto-Reparación: Sincronizando ID 317620 - By Xyao.mkz

---
+++ old_backup_corrupted/317620.lua
@@ -0,0 +1,8 @@
+--game: Space Hulk: Ascension (Classic)
+
+addappid(317620)
+addappid(317621,0,"9e8f85b5198c26753388f8d7143d064a474b1dd5819cf321e058909fed8b8184")
+addappid(335260)
+addappid(347980)
+addappid(347981)
+addappid(355900)
