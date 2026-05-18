d4b5ddd749519a615c574d848fd374c8f1146390
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-05-18 11:14:16 -0400

    Sync LUA 219830 - By Xyao.mkz

---
+++ old_backup_corrupted/219830.lua
@@ -0,0 +1,8 @@
+--game: King Arthur's Gold
+
+addappid(219830)
+addappid(219831,0,"9d780caa23fc0766cb83471cb789817fc380ea3585e7789667537071ff2b7647")
+addappid(219833,0,"94ea56defd8bfe40ec5f1626f63ff477f4d9f1a1fe60166c317a068712a2bc7f")
+addappid(640900)
+addappid(894670)
+addappid(1140690)
