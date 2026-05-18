ec02c123acefd037866f82c906c6de4109d92eb8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-05-18 11:16:22 -0400

    Sync LUA 257730 - By Xyao.mkz

---
+++ old_backup_corrupted/257730.lua
@@ -0,0 +1,13 @@
+--game: Infinity Wars: Animated Trading Card Game
+
+addappid(257730)
+addappid(257731,0,"d87fa0b01e7a3904c829208f9e32529a7ddee5948df274680a96cb548edc59c5")
+addappid(257732,0,"afbe015157eb81424585d53a3d4908adcc88df7103a94d682462518af566f63d")
+addappid(470610)
+addappid(473870)
+addappid(473871)
+addappid(502310)
+addappid(544560)
+addappid(544561)
+addappid(547240)
+addappid(4695450)
