4a519d4a3b45d191a7686eab235ec1a0f66610da
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 13:45:05 -0400

    Sync LUA 259410 - By Xyao.mkz

--- old_backup_corrupted/259410.lua
+++ old_backup_corrupted/259410.lua
@@ -1,5 +1,6 @@
 --game: Sneaky Sneaky
 
 addappid(259410)
-addappid(228983)
 addappid(259411,0,"5ad81cc406e28fbc5d018f9088e599bec7ba63d2746f6d8fdfab7b319ab91062")
+addappid(259412,0,"e24c31065a4cf0f241378e4af776268c9bad0d9b7ea8d9dd539abe3f66893b03")
+addappid(333320,0,"6c1fc080406e20127ccfa4f9439f5a35d238d4e6f03cb3c26fb69ea30e5c4bd4")
