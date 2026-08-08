f15b02218e914d581c46354e5bbdbcac2e212f9e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 17:36:17 -0400

    Sync LUA 3273420 - By Xyao.mkz

--- old_backup_corrupted/3273420.lua
+++ old_backup_corrupted/3273420.lua
@@ -1,4 +1,4 @@
---game: FPV Battleground: Combat Drone Simulator 
+--game: FPV Battleground
 
 addappid(3273420)
 addappid(3273421,0,"4e4efe7954c8629f3b6c6932d5caf2befadccfcf820cb9c4871867095e7818ab")
