caa570afb4abf05e9a5761645a2744bcb3f3a749
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 17:01:48 -0400

    Sync LUA 2300160 - By Xyao.mkz

--- old_backup_corrupted/2300160.lua
+++ old_backup_corrupted/2300160.lua
@@ -1,4 +1,4 @@
---game: Death From Above: A Ukrainian Drone Warstory
+--game: Death From Above
 
 addappid(2300160)
 addappid(2300161,0,"c6682388389db35a2d939cdfafe685e4e36692732af45490f61c11d9c4ef7309")
