6e89d57ba1f34a66c352fba87341c38217374563
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 15:14:06 -0400

    Sync LUA 726840 - By Xyao.mkz

--- old_backup_corrupted/726840.lua
+++ old_backup_corrupted/726840.lua
@@ -1,4 +1,4 @@
---game: City Game Studio: Your Game Dev Adventure Begins
+--game: City Game Studio
 
 addappid(726840)
 addappid(726844,0,"cbe477aab7ce09bfebf6c5c4096023b76e4bb51a8599feea893c80eaaa51de71")
