1e86415c949dadfebbbb02a281f1755913a6d677
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 13:36:01 -0400

    Sync LUA 220420 - By Xyao.mkz

--- old_backup_corrupted/220420.lua
+++ old_backup_corrupted/220420.lua
@@ -1,4 +1,4 @@
---game: Nancy Drew®: The Deadly Device
+--game: Nancy Drew: The Deadly Device
 
 addappid(220420)
 addappid(220421,0,"f1eff53ba7b22b2228f37e74c78457a9d29e92a5a46b20b6d8c83897b603ebcc")
