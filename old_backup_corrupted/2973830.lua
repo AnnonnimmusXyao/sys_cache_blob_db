5d14d4e76767f1cf32dce6d2458648b3456c3eee
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 17:26:17 -0400

    Sync LUA 2973830 - By Xyao.mkz

--- old_backup_corrupted/2973830.lua
+++ old_backup_corrupted/2973830.lua
@@ -1,4 +1,4 @@
---game: Prison Boss Prohibition
+--game: Crime Shop Simulator: A Prison Boss Game
 
 addappid(2973830)
 addappid(2973831,0,"121a06c3d642b69345f6880ee9ee860dabbbf2783bd9fef529f10b9b62bfca9a")
