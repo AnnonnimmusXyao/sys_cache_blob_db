2190d1cdb515d571625ef8006f7e095f0fee8d81
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 16:03:20 -0400

    Sync LUA 1303670 - By Xyao.mkz

--- old_backup_corrupted/1303670.lua
+++ old_backup_corrupted/1303670.lua
@@ -1,4 +1,4 @@
---game: WORLD OF ART JIGSAW PUZZLES 
+--game: World of Art - learn with Jigsaw Puzzles
 
 addappid(1303670)
 addappid(1303671,0,"45714352c2e9f81eaa578b84056a3c1cd50418730254b654bd9ba26722aa7b0c")
