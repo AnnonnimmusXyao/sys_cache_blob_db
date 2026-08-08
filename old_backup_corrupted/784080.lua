809202de30279fd489df31956db110828fa3e16d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 15:19:45 -0400

    Sync LUA 784080 - By Xyao.mkz

--- old_backup_corrupted/784080.lua
+++ old_backup_corrupted/784080.lua
@@ -1,7 +1,5 @@
 --game: MechWarrior 5: Mercenaries
 
-addappid(784080)
-addappid(784081,0,"b8b97d071286439862b9f60a8898a85f6e6c648afd1e3064b37f125523f14a46")
 addappid(1456180)
 addappid(1456210)
 addappid(1705820)
@@ -10,3 +8,5 @@ addappid(2179800)
 addappid(2489020)
 addappid(2782050)
 addappid(3799610)
+addappid(784080)
+addappid(784081,0,"b8b97d071286439862b9f60a8898a85f6e6c648afd1e3064b37f125523f14a46")
