d6b1b856ecf35d93b2733c04b86e9a8c1a208aa0
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-08-08 17:31:00 -0400

    Sync LUA 3112260 - By Xyao.mkz

--- old_backup_corrupted/3112260.lua
+++ old_backup_corrupted/3112260.lua
@@ -1,4 +1,4 @@
---game:  Virtua Fighter 5 R.E.V.O. World Stage
+--game: Virtua Fighter 5 R.E.V.O. World Stage
 
 addappid(3112260)
 addappid(3112261,0,"e6f1715a5b59614a89b1dd93f5b2f9c4078e5365b885651d49acfbfc4df8ad89")
