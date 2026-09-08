14dc39b3b6e341b7bf48a5e4c1f5a7acf8b70d75
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 13:58:40 -0300

    Sync LUA 222480 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/222480.lua
+++ old_backup_corrupted/222480.lua
@@ -1,11 +1,12 @@
 --game: Resident Evil Revelations
 
 addappid(222480)
-addappid(222481,0,"65e75d906613ce658af73b8db0772cf9f6fa3905427161dd1c0482572da72c0d")
-addappid(229621,0,"cd908e40d47ae228e942070e0e6e9dd87d874f16434488b0b6d70091915ec999")
-addappid(229622,0,"ae8ea828b9a2c97510d5968ce55a9dae3a04346865801ac4757d7cd7ff106d9d")
-addappid(229626,0,"1badd29cc032ee6227834ed10f78d0f4e785be725ae708391b1d6083e9d0fd67")
-addappid(229627,0,"05fe47e8229993a4957c546b6b11f34f46e75c4344ec653a896e52fcfb8d4343")
-addappid(229628,0,"deda58ec7e09d693118e91f904d7e25e08198a746dfe30f5a988ade2caf96dbb")
-addappid(229629,0,"c9a80ed80166abc5061fe93f2ca57b2a013075300639e2a7963909215b3cdc57")
-addappid(229630,0,"c338077d7ee7ef5a03646b62ae08290d05705e25da98b945e75cc86b5c3da7a3")
+addappid(222481, 1, "65e75d906613ce658af73b8db0772cf9f6fa3905427161dd1c0482572da72c0d") -- Resident Evil: Revelations Content
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(229621, 1, "cd908e40d47ae228e942070e0e6e9dd87d874f16434488b0b6d70091915ec999") -- RE Rev.  BH REV. UE Upgrade Parts Resistance Set - DLC_MER_WPSET_01
+addappid(229622, 1, "ae8ea828b9a2c97510d5968ce55a9dae3a04346865801ac4757d7cd7ff106d9d") -- RE Rev.  BH REV. UE Upgrade Parts Enhancement Set - DLC_MER_WPSET_02
+addappid(229626, 1, "1badd29cc032ee6227834ed10f78d0f4e785be725ae708391b1d6083e9d0fd67") -- RE Rev.  BH REV. UE RAID Outfit LADY HUNK - DLC_ULK_PL_RH
+addappid(229627, 1, "05fe47e8229993a4957c546b6b11f34f46e75c4344ec653a896e52fcfb8d4343") -- RE Rev.  BH REV. UE RAID Outfit Rachael Ooze - DLC_ULK_PL_RO
+addappid(229628, 1, "deda58ec7e09d693118e91f904d7e25e08198a746dfe30f5a988ade2caf96dbb") -- RE Rev.  BH REV. UE Weapon Parkers Government  Custom Part FBC - DLC_ULK_WP_GBM
+addappid(229629, 1, "c9a80ed80166abc5061fe93f2ca57b2a013075300639e2a7963909215b3cdc57") -- RE Rev.  BH REV. UE Weapon Jessicas G18  Custom Part BSAA - DLC_ULK_WP_PC356
+addappid(229630, 1, "c338077d7ee7ef5a03646b62ae08290d05705e25da98b945e75cc86b5c3da7a3") -- RE Rev.  BH REV. UE Weapon Jills Samurai Edge  Custom Part S.T.A.R.S. - DLC_ULK_WP_SMR
