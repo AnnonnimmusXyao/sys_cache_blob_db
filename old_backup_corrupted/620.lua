77b93dda5cd7c98db6317ccb8ee4cf5bba83a446
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 03:42:25 -0300

    Sync LUA 620 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/620.lua
+++ old_backup_corrupted/620.lua
@@ -1,9 +1,6 @@
 --game: Portal 2
 
-addappid(323180)
-addappid(323184, 1, "e20344b3d3b8542dd688df9c0ea6c68482dabe0d49774a297fc06782acd1d952") -- Portal 2 Soundtrack - Main Content
-addappid(323185, 1, "33658cb0a5a5c9d74b8bb94b2307030da8b8925b41edc06674458a1d094cabba") -- Portal 2 Soundtrack - Main Content
-addappid(620) -- Portal 2
+addappid(620)
 addappid(621, 1, "d2718f2424c94832a7112661774d98ffb3d52f454312f543a71418d253100d1d") -- portal 2 common
 addappid(622, 1, "0be76bef0b464b2d58b6ced70c86e608708c18c1204836cb1bb5d209776f8a0e") -- portal 2 win content
 addappid(623, 1, "6a9fe60f83046355da81ea12f8fe895e3f59f1223d1689cc7e1ba327358e99fb") -- portal 2 mac content
@@ -19,3 +16,6 @@ addappid(653) -- Portal 2 - Bot Antenna Topper DLC
 addappid(654) -- Portal 2 - Summer 2011 DLC
 addappid(661, 1, "5e338dd8d07e4cda5dfbcc7eb03418d9b1d93192bc20d5aac2174b851b11c1a6") -- Portal 2 linux content
 addappid(669) -- Portal 2 - Sixense Motion Preorder
+addappid(323180)
+addappid(323184, 1, "e20344b3d3b8542dd688df9c0ea6c68482dabe0d49774a297fc06782acd1d952") -- Portal 2 Soundtrack - Main Content
+addappid(323185, 1, "33658cb0a5a5c9d74b8bb94b2307030da8b8925b41edc06674458a1d094cabba") -- Portal 2 Soundtrack - Main Content
