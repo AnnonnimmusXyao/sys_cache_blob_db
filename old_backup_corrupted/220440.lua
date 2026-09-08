ad41bfc263c2b0ed4a4b31bb7d1653c93b8f38cf
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 13:58:17 -0300

    Sync LUA 220440 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/220440.lua
+++ old_backup_corrupted/220440.lua
@@ -1,8 +1,9 @@
 --game: DmC Devil May Cry
 
 addappid(220440)
-addappid(220441,0,"aed2b190f9b8ea15e6f1e22aed2d339f162f17b8673f06124dd11e1ea45b11be")
-addappid(223790,0,"84ed6b57c32b4629f5cc2274674cb95f00d4b65d067170bb9a2f0ba4ffbd76a6")
-addappid(223792,0,"8ca028cec8d61d9c12f8ea45f546d2c50f66aa8a2ddbda601a042ab68e909b0a")
-addappid(223793,0,"e839ea49e739fb933e04b272dedc90509e470ad48ad698e48433772c64ef86fd")
-addappid(229080,0,"2d5f510f44b55ffd17e3fe6d09bba6d2751544fd151293553fa4466dea0660ef")
+addappid(220441, 1, "aed2b190f9b8ea15e6f1e22aed2d339f162f17b8673f06124dd11e1ea45b11be") -- DmC Devil May Cry Content
+addappid(220442, 1, "390e4902a68f3b732b8e42fe6fe3ccea44c54c19421e21eaa9860842c26fa973") -- DmC Devil May Cry Content LV
+addappid(223790, 1, "84ed6b57c32b4629f5cc2274674cb95f00d4b65d067170bb9a2f0ba4ffbd76a6") -- DmC Devil May Cry Costume Pack - DmC Devil May Cry DanteOryginalDLC
+addappid(223792, 1, "8ca028cec8d61d9c12f8ea45f546d2c50f66aa8a2ddbda601a042ab68e909b0a") -- DmC Devil May Cry Weapon Bundle - DmC Devil May Cry WeaponsBundleDLC
+addappid(223793, 1, "e839ea49e739fb933e04b272dedc90509e470ad48ad698e48433772c64ef86fd") -- DmC Devil May Cry Vergils Downfall - DmC Devil May Cry VergilsDownfallDLC WW
+addappid(229080, 1, "2d5f510f44b55ffd17e3fe6d09bba6d2751544fd151293553fa4466dea0660ef") -- DmC Devil May Cry Bloody Palace Mode - Dmc Devil May Cry BloodyPalaceDLC WW
