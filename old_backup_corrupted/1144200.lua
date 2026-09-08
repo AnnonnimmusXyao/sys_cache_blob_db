4fa4ba5fa342093e54738119ffc908e3717be581
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:15:53 -0300

    Sync LUA 1144200 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1144200.lua
+++ old_backup_corrupted/1144200.lua
@@ -1,7 +1,9 @@
 --game: Ready or Not
 
 addappid(1144200)
-addappid(1144201,0,"b9236894ba071d12dd117ce16220fefeb20574d7c6f7d2170e3113346867f3d6")
-addappid(1844910)
-addappid(3015760)
-addappid(3174120)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(1144201, 1, "b9236894ba071d12dd117ce16220fefeb20574d7c6f7d2170e3113346867f3d6") -- Ready Or Not - Alpha Content
+addappid(1844910) -- Ready or Not Supporter Edition
+addappid(3015760) -- Ready or Not Home Invasion
+addappid(3174120) -- Ready or Not Dark Waters
