f6683104bfe4295310d31e022c831f6b8797a6e7
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:05:46 -0300

    Sync LUA 292180 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/292180.lua
+++ old_backup_corrupted/292180.lua
@@ -1,6 +1,8 @@
 --game: X-Plane 10 Global - 64 Bit
 
 addappid(292180)
-addappid(292181,0,"4c4b3a7275650cbc464389bd6639dad14dc1067cae61e2c73d2ae2e517ff19f2")
-addappid(292182,0,"06fed149c0005ab0f2e521263b1abc4e80085d5618883a86f2bd03c55937670c")
-addappid(310430,0,"45db10c59b9ec4f706bad96ea65ab9499be8d98caae3cde10e0241d2f7c7033f")
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+addappid(292182, 1, "06fed149c0005ab0f2e521263b1abc4e80085d5618883a86f2bd03c55937670c") -- Windows
+addappid(310430, 1, "45db10c59b9ec4f706bad96ea65ab9499be8d98caae3cde10e0241d2f7c7033f") -- X-Plane 10 Global - 64 Bit - North America Scenery - X-Plane 10 Global - 64 Bit - North America Scenery (310430) Depot
+addappid(499530) -- X-Plane 10 AddOn - JustFlight - Air Hauler
