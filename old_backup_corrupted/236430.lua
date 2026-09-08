4c2f709b448c317398b7d7e6a38b0a78a609a9cc
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 13:59:58 -0300

    Sync LUA 236430 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/236430.lua
+++ old_backup_corrupted/236430.lua
@@ -1,11 +1,14 @@
 --game: DARK SOULS™ II
 
-addappid(228982)
-addappid(228983)
 addappid(236430)
-addappid(236432,0,"eda018a11af0a6a37b59a60dadcbfca12bd721227868fcff4053b8b85e67bd05")
-addappid(271940,0,"591c3310fabae8ea378bcd7de0d2d6e51021ff65de92140c0869f2a90df0e3ce")
-addappid(271941,0,"e6c617c30eb9d95791592e9fb5200627d32cb74264e74e58d55ffd04d01b0ff6")
-addappid(271942,0,"a1965b62e56b19e4ff236ec31cef3b501ea253fd4d0f958b9f3db502e7e92e86")
-addappid(271943,0,"03066e42170eb2798cc95b53972b74fc6ad4e994b1f0ad1c62720136abbd954a")
-addappid(271944,0,"cce41eba79f1afc8fe13f0c97724e9bf9e1fbe22782a2eb17582fdc3a8b5780e")
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (Shared from App 228980)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(236432, 1, "eda018a11af0a6a37b59a60dadcbfca12bd721227868fcff4053b8b85e67bd05") -- DARK SOULS™ II TechDev Depot
+addappid(271940, 1, "591c3310fabae8ea378bcd7de0d2d6e51021ff65de92140c0869f2a90df0e3ce") -- Dark Souls II - Pre-Order DLC ROW - Dark Souls II - Pre-Order DLC (271940) Depot
+addappid(271941, 1, "e6c617c30eb9d95791592e9fb5200627d32cb74264e74e58d55ffd04d01b0ff6") -- Dark Souls II - Digital Extras - Dark Souls II - Digital Extras (271941) Depot
+addappid(271942, 1, "a1965b62e56b19e4ff236ec31cef3b501ea253fd4d0f958b9f3db502e7e92e86") -- Dark Souls II Crown of the Sunken King - Dark Souls II - DLC 1 (271942) Depot
+addappid(271943, 1, "03066e42170eb2798cc95b53972b74fc6ad4e994b1f0ad1c62720136abbd954a") -- DARK SOULS II Crown of the Old Iron King - Dark Souls II - DLC 2 (271943) Depot
+addappid(271944, 1, "cce41eba79f1afc8fe13f0c97724e9bf9e1fbe22782a2eb17582fdc3a8b5780e") --  DARK SOULS II Crown of the Ivory King  - Dark Souls II - DLC 3 (271944) Depot
+addappid(284450) -- DARK SOULS II - Season Pass
+addappid(355700) -- Dark Souls II Upgrade to DX11 (no content)
