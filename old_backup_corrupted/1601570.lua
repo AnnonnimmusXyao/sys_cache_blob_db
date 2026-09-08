c789c60a90b3a2dc27d307f5de08ea1b107648ae
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:35:13 -0300

    Sync LUA 1601570 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1601570.lua
+++ old_backup_corrupted/1601570.lua
@@ -1,6 +1,8 @@
 --game: The Alters
 
 addappid(1601570)
-addappid(1601571,0,"0439da537fb9ca417bd1efa7f41890efff942c7cae69a68d72ff61d8b3e2ca8b")
-addappid(3237510)
-addappid(3773900)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(1601571, 1, "0439da537fb9ca417bd1efa7f41890efff942c7cae69a68d72ff61d8b3e2ca8b") -- Main Game Content (Windows Content)
+addappid(3773900) -- The Alters Deluxe Edition Upgrade
