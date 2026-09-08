35618e5c0c1c7a893aa4a8bc27e7a1d0c5922349
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:29:39 -0300

    Sync LUA 1449110 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1449110.lua
+++ old_backup_corrupted/1449110.lua
@@ -1,7 +1,10 @@
 --game: The Outer Worlds 2
 
 addappid(1449110)
-addappid(1449111,0,"1691760a5a08bde773f0880b6741283070c2c3931c41a09cab6682e29171c563")
-addappid(3729910)
-addappid(3938110)
-addappid(4055350)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(1449111, 1, "1691760a5a08bde773f0880b6741283070c2c3931c41a09cab6682e29171c563") -- Depot 1449111
+addappid(3729850, 1, "5de7b19426b9195dd06c2774cb6432657fb1a818723432602636beb88dce3dd4") -- Depot 3729850
+addappid(3729910) -- The Outer Worlds 2 - Commander Zanes Battle Pack
+addappid(3938110) -- The Outer Worlds 2 Pet Tie-in
+addappid(4055350) -- The Outer Worlds 2 Pet Tie-in 2
