3d87b7763d2a75ee1cbd859957826d1c22dee544
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:16:47 -0300

    Sync LUA 362680 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/362680.lua
+++ old_backup_corrupted/362680.lua
@@ -1,6 +1,8 @@
 --game: Fran Bow
 
 addappid(362680)
-addappid(362681,0,"44d14e5570baff72ae61a2c1d9b484c8e0ada528e532b141ec1234afaa5c49e8")
-addappid(362682,0,"b0c58d31e871b42fc70b331ec6b01ea3d6513e0316f8a2fef08ac01f2aa51014")
-addappid(362683,0,"a44a84f94135c910aba50badf09799ce3ebd35ff5160292fd5b402a75523459d")
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(362681, 1, "44d14e5570baff72ae61a2c1d9b484c8e0ada528e532b141ec1234afaa5c49e8") -- Fran Bow Depot Win
+addappid(362682, 1, "b0c58d31e871b42fc70b331ec6b01ea3d6513e0316f8a2fef08ac01f2aa51014") -- Fran Bow Depot Linux
+addappid(362683, 1, "a44a84f94135c910aba50badf09799ce3ebd35ff5160292fd5b402a75523459d") -- Fran Bow Depot OSX
+addappid(404840, 1, "6954fc834142b8700efb11fa6b92c17db6f3ee81b4b12aa515b6a323ba4a3b33") -- Fran Bow - Soundtrack - Fran Bow - Soundtrack (404840) Depot
