0f76a809c479469fe32c3b39cc283a7af2b627dd
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:18:44 -0300

    Sync LUA 1190000 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1190000.lua
+++ old_backup_corrupted/1190000.lua
@@ -1,7 +1,7 @@
 --game: Car Mechanic Simulator 2021
 
 addappid(1190000)
-addappid(1190001,0,"b4f04348a15ca7c50e5275db9f0569947a2e3926ad0f05db23b4032050ee2f77")
+addappid(1190001,1,"b4f04348a15ca7c50e5275db9f0569947a2e3926ad0f05db23b4032050ee2f77")
 addappid(1685720)
 addappid(1685721)
 addappid(1748990)
@@ -19,6 +19,3 @@ addappid(2464230)
 addappid(2546830)
 addappid(2601200)
 addappid(2721070)
-addappid(2889700)
-addappid(3335470)
-addappid(3660210)
