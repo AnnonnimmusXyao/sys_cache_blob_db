c88e268006071a3e032a83aa09f1715f269947ea
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:01:56 -0300

    Sync LUA 859280 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/859280.lua
+++ old_backup_corrupted/859280.lua
@@ -1,4 +1,4 @@
 --game: 拯救大魔王:重生 Falsemen: Rebirth
 
 addappid(859280)
-addappid(859281,0,"d1b152d1a2f8eb983097ff7c656bd70d310423cfa9c59e75a5d63922dae54bbb")
+addappid(859281,1,"d1b152d1a2f8eb983097ff7c656bd70d310423cfa9c59e75a5d63922dae54bbb")
