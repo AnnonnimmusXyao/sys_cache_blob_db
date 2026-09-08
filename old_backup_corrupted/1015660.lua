d5c3709be9ef50e72f90e91a07414484171a8be8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:09:12 -0300

    Sync LUA 1015660 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1015660.lua
+++ old_backup_corrupted/1015660.lua
@@ -1,4 +1,4 @@
 --game: Hentai Shooter 2: World Tour
 
 addappid(1015660)
-addappid(1015661,0,"f68d06d75654966d0ac244faff57afcb2bcca82a07160ca05267e5399dc2db60")
+addappid(1015661, 1, "f68d06d75654966d0ac244faff57afcb2bcca82a07160ca05267e5399dc2db60")
