10330e73f91dc7c7d83c29eb3208c74f310d0d52
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:01:29 -0300

    Sync LUA 251570 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/251570.lua
+++ old_backup_corrupted/251570.lua
@@ -1,10 +1,13 @@
 --game: 7 Days to Die
 
 addappid(251570)
-addappid(251571,0,"2787b64cd24c9e42a114df4d2559813dc0f63405faa2f7e41cc77e6c3cc67bd0")
-addappid(251576,0,"525d3fda8d2195cf5a8bd10c0c158513d9f870f99a036830287c1465c73c6979")
-addappid(251577,0,"53a3478a67db1f8ed8030e845ec1be174a60cea2448401ffd4dda0c4838cbb84")
-addappid(251578,0,"4550672ef4a498246530208707b36b490dd3bb73c6b54ba6b832dd48db88f71a")
-addappid(3314750)
-addappid(3486400)
-addappid(3635260)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(251571, 1, "2787b64cd24c9e42a114df4d2559813dc0f63405faa2f7e41cc77e6c3cc67bd0") -- Windows 32 Bit
+addappid(251576, 1, "525d3fda8d2195cf5a8bd10c0c158513d9f870f99a036830287c1465c73c6979") -- Windows 64 Bit
+addappid(251577, 1, "53a3478a67db1f8ed8030e845ec1be174a60cea2448401ffd4dda0c4838cbb84") -- Mac
+addappid(251578, 1, "4550672ef4a498246530208707b36b490dd3bb73c6b54ba6b832dd48db88f71a") -- Linux Universal
+addappid(3314750) -- The Hoarder Armor Set
+addappid(3486400) -- The Marauder Armor Set
+addappid(3635260) -- The Desert Armor Set
