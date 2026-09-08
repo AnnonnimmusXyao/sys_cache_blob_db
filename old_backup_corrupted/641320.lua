4722c1c6a21566cee27f69b2a2580f8301a36ae8
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:48:19 -0300

    Sync LUA 641320 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/641320.lua
+++ old_backup_corrupted/641320.lua
@@ -1,11 +1,14 @@
 --game: Cooking Simulator
 
-addappid(1168680,0,"797b64d673b14e960e7038223232c095037fffe4c87d0a5df408038992d80277")
-addappid(1227350,0,"95e78a282673605fd207d57e859d2ebfad79b7e109bf46948174367912417ea4")
-addappid(1357180)
-addappid(1400460,0,"0b0a23c7ebe1db10757fd3fe167ad02d5a24d30b85ea6204aa955224a82deb4f")
-addappid(1575660,0,"3da4fdcde0a30525db779f45c19281032e80bdf2174c17b75edae1ff9892c4ac")
-addappid(2655000,0,"2f18b51f4778cff77849ba20d5641847ce0ee6d1695ca634a6ba22af6482b01d")
 addappid(641320)
-addappid(641321,0,"693af0f9d3395af0e1b880807b3b8b17e6391a03fc860c96b19ed41405f60b47")
-addappid(641322,0,"db47cd4de11025ce2760303af3fd623baa4f940a1d4abade766929e6fdea5773")
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- .NET 3.5 Redist (Shared from App 228980)
+addappid(641321, 1, "693af0f9d3395af0e1b880807b3b8b17e6391a03fc860c96b19ed41405f60b47") -- Cooking Simulator Windows
+addappid(641322, 1, "db47cd4de11025ce2760303af3fd623baa4f940a1d4abade766929e6fdea5773") -- Depot 641322
+addappid(1168680, 1, "797b64d673b14e960e7038223232c095037fffe4c87d0a5df408038992d80277") -- Cooking Simulator - Cooking with Food Network - Cooking Simulator - Cooking with Food Network (1168680) Depot
+addappid(1227350, 1, "95e78a282673605fd207d57e859d2ebfad79b7e109bf46948174367912417ea4") -- Cooking Simulator - Cakes and Cookies - Cooking Simulator - Cakes and Cookies (1227350) Depot
+addappid(1357180) -- Cooking Simulator - Chaos Tool
+addappid(1400460, 1, "0b0a23c7ebe1db10757fd3fe167ad02d5a24d30b85ea6204aa955224a82deb4f") -- Cooking Simulator - Pizza - Cooking Simulator - Pizza (1400460) Depot
+addappid(1575660, 1, "3da4fdcde0a30525db779f45c19281032e80bdf2174c17b75edae1ff9892c4ac") -- Cooking Simulator - Shelter - Cooking Simulator - Shelter (1575660) Depot
+addappid(2655000, 1, "2f18b51f4778cff77849ba20d5641847ce0ee6d1695ca634a6ba22af6482b01d") -- Cooking Simulator - Sushi - Depot 2655000
