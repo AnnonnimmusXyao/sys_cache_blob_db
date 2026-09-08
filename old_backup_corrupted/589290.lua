e3a4658d18b50e8700e21432a47b1599e4fa0e6f
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:44:01 -0300

    Sync LUA 589290 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/589290.lua
+++ old_backup_corrupted/589290.lua
@@ -1,15 +1,16 @@
 --game: Holdfast: Nations At War
 
-addappid(1243770)
-addappid(1324000)
-addappid(1415510)
-addappid(1489800)
-addappid(1906770)
-addappid(1906771)
-addappid(1960210)
-addappid(1960220)
-addappid(2107250)
-addappid(2692480)
-addappid(3320400)
 addappid(589290)
-addappid(589291,0,"a7c1417721600265bba5c83ede099119af0a65f1e2f3b341cc26c5cc7987b7b3")
+addappid(589291, 1, "a7c1417721600265bba5c83ede099119af0a65f1e2f3b341cc26c5cc7987b7b3") -- Holdfast: Nations At War Content
+addappid(1243770)
+addappid(1243771, 1, "889f1fb0c89f83e6b5940bd922810c574affb5f838d6cd81d85a1f3fcf6b25cb") -- Holdfast Nations At War - Loyalist Edition Upgrade - Holdfast: Nations At War - Loyalist Edition Upgrade Depot
+addappid(1324000) -- Holdfast Nations At War - Regiments of the Line
+addappid(1415510) -- Holdfast Nations At War - High Command
+addappid(1489800) -- Holdfast Nations At War - Regiments of the Guard
+addappid(1906770) -- Holdfast Frontlines WW1
+addappid(1906771) -- Holdfast Frontlines WW1 - American Forces
+addappid(1960210) -- Holdfast Frontlines WW1 - Ottoman Forces
+addappid(1960220) -- Holdfast Frontlines WW1 - Australian Forces
+addappid(2107250) -- Holdfast Nations At War - Grenadier Regiments
+addappid(2692480) -- Holdfast Nations At War - Napoleons Rise
+addappid(3320400) -- Holdfast Nations At War - Blackpowder Firearms
