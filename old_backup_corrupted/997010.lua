dd38bea81aa5f00bc0ef880b128f9fa21ee1b3cf
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:08:18 -0300

    Sync LUA 997010 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/997010.lua
+++ old_backup_corrupted/997010.lua
@@ -1,22 +1,20 @@
 --game: Police Simulator: Patrol Officers
 
-addappid(2009550)
-addappid(2199030)
-addappid(2210960)
-addappid(2350410)
-addappid(2513250)
-addappid(2527820)
-addappid(2527830)
-addappid(2893870)
-addappid(2893880)
-addappid(3048950)
-addappid(3167630)
-addappid(3597210)
-addappid(3597220)
-addappid(3768290)
-addappid(3768300)
-addappid(3924710)
-addappid(3924720)
-addappid(3924730)
 addappid(997010)
-addappid(997011,0,"788e33ebc2e38174d9b613b57ed47947aacaf7399a6bbd32762161e56ce34d62")
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(997011, 1, "788e33ebc2e38174d9b613b57ed47947aacaf7399a6bbd32762161e56ce34d62") -- Project Bavaria Content
+addappid(2009550) -- Police Simulator Patrol Officers Urban Terrain Vehicle DLC
+addappid(2199030) -- Police Simulator Patrol Officers Compact Police Vehicle DLC
+addappid(2210960) -- Police Simulator Patrol Officers Guardian Police Vehicle DLC
+addappid(2350410) -- Police Simulator Patrol Officers Highway Patrol Expansion
+addappid(2513250) -- Police Simulator Patrol Officers Multipurpose Police Vehicle DLC
+addappid(2527820) -- Police Simulator Patrol Officers Surveillance Police Vehicle DLC
+addappid(2527830) -- Police Simulator Patrol Officers Warden Police Vehicle DLC
+addappid(2893870) -- Police Simulator Patrol Officers Unmarked Police Vehicle Pack
+addappid(2893880) -- Police Simulator Patrol Officers Special Police Vehicle Pack
+addappid(3048950) -- Police Simulator Patrol Officers Tropical Taskforce Pack
+addappid(3167630) -- Police Simulator Patrol Officers Fast Pursuit Police Vehicle DLC
+addappid(3597210) -- Police Simulator Patrol Officers - Season Pass
+addappid(3597220) -- Police Simulator Patrol Officers - Vehicle Customization Pack
+addappid(3768290) -- Police Simulator Patrol Officers Adventurer Police Vehicle DLC
