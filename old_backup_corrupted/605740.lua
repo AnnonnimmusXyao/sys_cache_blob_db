ecffe95525ed9a7c8b2d669aebea55ae365a9889
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:45:45 -0300

    Sync LUA 605740 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/605740.lua
+++ old_backup_corrupted/605740.lua
@@ -1,13 +1,12 @@
 --game: Flashing Lights
 
-addappid(2449260)
-addappid(2649400)
-addappid(2649410)
-addappid(2649420)
-addappid(2649430)
-addappid(3051230)
-addappid(3640340)
-addappid(3640350)
 addappid(605740)
-addappid(605741,0,"95d3d0ace153500ff96a04353c87950fc4420523d33bd980470ba1bcc5513ec2")
-addappid(605742,0,"a8211cb137d2735d7ff465f3f245198f857855a1e31a0ed5d1cbd54f4e795989")
+addappid(605741, 1, "95d3d0ace153500ff96a04353c87950fc4420523d33bd980470ba1bcc5513ec2") -- Flashing Lights Content
+addappid(605742, 1, "a8211cb137d2735d7ff465f3f245198f857855a1e31a0ed5d1cbd54f4e795989") -- Flashing Lights Depot MAC
+addappid(2449260) -- Flashing Lights Pickup Truck Triple Pack (Police, Fire, EMS)
+addappid(2649400) -- Flashing Lights - Visor Lights
+addappid(2649410) -- Flashing Lights Beast Swat Truck DLC
+addappid(2649420) -- Flashing Lights Emergency Response Lightbar Collection
+addappid(2649430) -- Flashing Lights Thunder Sport Sedan Pack (Police, Fire, EMS)
+addappid(3051230) -- Flashing Lights Interceptor SUV Pack (Police, Fire, EMS)
+addappid(3640340) -- Flashing Lights BUCK - Police, Fire, Rescue Pack
