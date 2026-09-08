f5592303470d26bb0bd86b371a84f414e844e7d7
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:37:08 -0300

    Sync LUA 1643320 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1643320.lua
+++ old_backup_corrupted/1643320.lua
@@ -1,9 +1,12 @@
 --game: S.T.A.L.K.E.R. 2: Heart of Chornobyl
 
 addappid(1643320)
-addappid(1643321,0,"71c29e58911c13991edea02ac06db5f5991e43e52cdcd76c19126d657c4a38e7")
-addappid(1661620)
-addappid(1661621)
-addappid(1661622)
-addappid(1661623)
-addappid(3399310)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+addappid(1643321, 1, "71c29e58911c13991edea02ac06db5f5991e43e52cdcd76c19126d657c4a38e7") -- Depot 1643321
+addappid(1661620) -- S.T.A.L.K.E.R. 2 Heart of Chornobyl - Deluxe Content
+addappid(1661621) -- S.T.A.L.K.E.R. 2 Heart of Chornobyl - Ultimate Content
+addappid(1661622) -- S.T.A.L.K.E.R. 2 Heart of Chornobyl - Season Pass
+addappid(1661623) -- S.T.A.L.K.E.R. 2 Heart of Chornobyl - Pre-order Bonus
+addappid(3399310) -- S.T.A.L.K.E.R. 2 Heart of Chornobyl - Community Items
