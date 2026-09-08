a69295c5e9a09a643daca8101e8c47523295a0bc
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:00:43 -0300

    Sync LUA 243800 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/243800.lua
+++ old_backup_corrupted/243800.lua
@@ -1,5 +1,4 @@
 --game: Gas Guzzlers Extreme
 
-addappid(228985)
 addappid(243800)
-addappid(243801,0,"cae65bfd3eb6c2c8cdaeb9746efedf2f915b951ac62d2e1f17053126225e6362")
+addappid(243801, 1, "cae65bfd3eb6c2c8cdaeb9746efedf2f915b951ac62d2e1f17053126225e6362")
