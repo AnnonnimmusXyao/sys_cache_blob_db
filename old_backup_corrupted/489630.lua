5a0d0795529b9cc06c92d583acad37825f59b19c
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:34:44 -0300

    Sync LUA 489630 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/489630.lua
+++ old_backup_corrupted/489630.lua
@@ -1,25 +1,26 @@
 --game: Warhammer 40,000: Gladius - Relics of War
 
-addappid(1084820,0,"ee313c687835e5166562e269b36572e89de3a45cafbeaf1879db587d5681bd49")
-addappid(1170010,0,"78a8474c3c6a3d4d0437cd808aeaa18fc4e4e923f0f58988c2887c12b6e1e949")
-addappid(1239450,0,"06a825d11874fb9ccec98d4720b93362b1401cd05e30f240b6c64d1cdcd302dc")
-addappid(1330480,0,"dc33d113a2f943b9be884c2817828603859f93972619902dab7c39d0ecb8b649")
-addappid(1425820,0,"be8694de13023b51c4cad8d672e68571f20dd0072714424f5fbf97dc9e0ac4a7")
-addappid(1640850,0,"d69f24faedae1efb5695169b70140cc649be555e18e2be643ee324a617fe7294")
-addappid(1788210,0,"85a2c84a029166be6e595e081b6bf9f39a8f98652b0c8c01d0bc35e7d91f9a60")
-addappid(2006870,0,"541a5c3ccb66295018e90753817508bd11691d17aa4d894379b0dfdbf433f2ab")
-addappid(2130960,0,"2e41d3eb51b475a717f4526395d8cf7bbbbc09ee871cd5ecc6efdfafdf2481b6")
-addappid(229006)
-addappid(2399890,0,"2d8288c197415cced529152df9e87700f628d56e35db913c1549eaecca674086")
-addappid(2668040,0,"d994cddf0661503e891556f8a8dc28f74586be3c16c9520fa5ba392dd2dbcea2")
-addappid(2932380,0,"cbe017581e8b542a1262cbd153e731d294da59db188801a36647944a080b9d52")
-addappid(3326080,0,"5a032138ec68be28197d6e03a29eb1c99e8c7f392838b8ea170ac329111040cc")
 addappid(489630)
-addappid(489631,0,"98cad733831ba87cf9c07a92272afbcfcf98b22daea917ab778b0f46ac6473b8")
-addappid(489632,0,"2181cc760c11eec2b2088e83099b893bbef7db3b725ab0f437942b2f6a6042b6")
-addappid(489633,0,"6bfeeddc0c7c9ab4c84e53df522ca4caf4b1613a43e564d36b986ec55f5463f8")
-addappid(870530,0,"4e12760f62e2227d48295b0f3b0422870e19f58cb4fb97f5a845b2b36f4aa30a")
-addappid(870540,0,"6e0cd7069ff1bb92e841c6045af49df24f0012760cbfa2b36a6baea8e8648e33")
-addappid(870550,0,"8f35ba8a63d84bb32165ffd1c934a166e581becffc9e3908a561c927f605caac")
-addappid(961840,0,"e04d4e06fffc382f6c245d339f0c0b761d3813b94f38e9a69135b828ecd1ed6f")
-addappid(995860,0,"2c8c2a78b1d4c6d478c7de8f44bd9fae8f9199cc5fd003a203005136d53ce006")
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+addappid(489631, 1, "98cad733831ba87cf9c07a92272afbcfcf98b22daea917ab778b0f46ac6473b8") -- Warhammer 40,000: Gladius - Relics of War - Windows
+addappid(489632, 1, "2181cc760c11eec2b2088e83099b893bbef7db3b725ab0f437942b2f6a6042b6") -- Warhammer 40,000: Gladius - Relics of War - Linux
+addappid(489633, 1, "6bfeeddc0c7c9ab4c84e53df522ca4caf4b1613a43e564d36b986ec55f5463f8") -- Warhammer 40,000: Gladius - Relics of War - Shared
+addappid(870530, 1, "4e12760f62e2227d48295b0f3b0422870e19f58cb4fb97f5a845b2b36f4aa30a") -- Warhammer 40,000: Gladius - Relics of War - Soundtrack (870530) Depot
+addappid(870540, 1, "6e0cd7069ff1bb92e841c6045af49df24f0012760cbfa2b36a6baea8e8648e33") -- Warhammer 40,000 Gladius - Lord of Skulls - Warhammer 40,000: Gladius - Relics of War - Lord of Skulls exclusive unit (870540) Depot
+addappid(870550, 1, "8f35ba8a63d84bb32165ffd1c934a166e581becffc9e3908a561c927f605caac") -- Warhammer 40,000 Gladius - Relics of War - Wallpapers - Warhammer 40,000: Gladius - Relics of War - Wallpapers (870550) Depot
+addappid(961840, 1, "e04d4e06fffc382f6c245d339f0c0b761d3813b94f38e9a69135b828ecd1ed6f") -- Warhammer 40,000 Gladius - Reinforcement Pack - Warhammer 40,000: Gladius - Reinforcement Pack (961840) Depot
+addappid(995860, 1, "2c8c2a78b1d4c6d478c7de8f44bd9fae8f9199cc5fd003a203005136d53ce006") -- Warhammer 40,000 Gladius - Tyranids - Warhammer 40,000: Gladius - Tyranids (995860) Depot
+addappid(1084820, 1, "ee313c687835e5166562e269b36572e89de3a45cafbeaf1879db587d5681bd49") -- Warhammer 40,000 Gladius - Chaos Space Marines - Warhammer 40,000: Gladius - Chaos Space Marines (1084820) Depot
+addappid(1170010, 1, "78a8474c3c6a3d4d0437cd808aeaa18fc4e4e923f0f58988c2887c12b6e1e949") -- Warhammer 40,000 Gladius - Fortification Pack - Warhammer 40,000: Gladius - Fortification Pack (1170010) Depot
+addappid(1239450, 1, "06a825d11874fb9ccec98d4720b93362b1401cd05e30f240b6c64d1cdcd302dc") -- Warhammer 40,000 Gladius - Tau - Warhammer 40,000: Gladius - Tau (1239450) Depot
+addappid(1330480, 1, "dc33d113a2f943b9be884c2817828603859f93972619902dab7c39d0ecb8b649") -- Warhammer 40,000 Gladius - Assault Pack - Warhammer 40,000: Gladius - Assault Pack (1330480) Depot
+addappid(1425820, 1, "be8694de13023b51c4cad8d672e68571f20dd0072714424f5fbf97dc9e0ac4a7") -- Warhammer 40,000 Gladius - Craftworld Aeldari - Warhammer 40,000: Gladius - Craftworld Aeldari (1425820) Depot
+addappid(1640850, 1, "d69f24faedae1efb5695169b70140cc649be555e18e2be643ee324a617fe7294") -- Warhammer 40,000 Gladius - Specialist Pack - Warhammer 40,000: Gladius - Specialist Pack (1640850) Depot
+addappid(1788210, 1, "85a2c84a029166be6e595e081b6bf9f39a8f98652b0c8c01d0bc35e7d91f9a60") -- Warhammer 40,000 Gladius - Adeptus Mechanicus - Warhammer 40,000: Gladius - Adeptus Mechanicus (1788210) Depot
+addappid(2006870, 1, "541a5c3ccb66295018e90753817508bd11691d17aa4d894379b0dfdbf433f2ab") -- Warhammer 40,000 Gladius - Escalation Pack - Depot 2006870
+addappid(2130960, 1, "2e41d3eb51b475a717f4526395d8cf7bbbbc09ee871cd5ecc6efdfafdf2481b6") -- Warhammer 40,000 Gladius - Adepta Sororitas - Depot 2130960
+addappid(2399890, 1, "2d8288c197415cced529152df9e87700f628d56e35db913c1549eaecca674086") -- Warhammer 40,000 Gladius - Firepower Pack - Depot 2399890
+addappid(2668040, 1, "d994cddf0661503e891556f8a8dc28f74586be3c16c9520fa5ba392dd2dbcea2") -- Warhammer 40,000 Gladius - Drukhari - Depot 2668040
+addappid(2932380, 1, "cbe017581e8b542a1262cbd153e731d294da59db188801a36647944a080b9d52") -- Warhammer 40,000 Gladius - Demolition Pack - Depot 2932380
+addappid(3326080, 1, "5a032138ec68be28197d6e03a29eb1c99e8c7f392838b8ea170ac329111040cc") -- Warhammer 40,000 Gladius - Ultima Founding - Depot 3326080
+addappid(3743110, 1, "126dd0c29b9af92ee931d376f40a6b77e15602a53c6c4ff3e4963c427853d1f5") -- Warhammer 40,000 Gladius - Onslaught Pack - Depot 3743110
