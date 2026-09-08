e9bcc9b2637b3d3b4862eac03b886d9eded75e8d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 14:39:07 -0300

    Sync LUA 526790 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/526790.lua
+++ old_backup_corrupted/526790.lua
@@ -1,5 +1,4 @@
 --game: Monsti
 
-addappid(229020)
 addappid(526790)
-addappid(526791,0,"9786debb1322800a410a36ae6c7cd2c1040a33c5496aaad1eb4ce5da174ef67f")
+addappid(526791,1,"9786debb1322800a410a36ae6c7cd2c1040a33c5496aaad1eb4ce5da174ef67f")
