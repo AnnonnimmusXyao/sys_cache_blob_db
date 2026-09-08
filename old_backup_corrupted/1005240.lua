1737583f61ca32d5190a683fb8722dca206431cf
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-08 15:08:32 -0300

    Sync LUA 1005240 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1005240.lua
+++ old_backup_corrupted/1005240.lua
@@ -1,4 +1,4 @@
 --game: Pixel Puzzles Illustrations & Anime Jigsaw Puzzles
 
 addappid(1005240)
-addappid(1005241,0,"ca76c73881bb2164be62cd5962f965896b9d6643c56ada9f01a8f8ffc1d6325d")
+addappid(1005241, 1, "CA76C73881BB2164BE62CD5962F965896B9D6643C56ADA9F01A8F8FFC1D6325D")
