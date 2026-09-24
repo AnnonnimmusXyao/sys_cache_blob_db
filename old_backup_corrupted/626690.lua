1db947945596c7eceeb93252bdf4c769bba02ce3
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 20:26:19 -0300

    Sync LUA 626690 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/626690.lua
+++ old_backup_corrupted/626690.lua
@@ -1,7 +1,7 @@
 --game: Sword Art Online: Fatal Bullet
 
 addappid(626690)
-addappid(626691,1,"d13786cc9fbb38ba37c0da1e5a7148f908520d248bcb1b44cd3d368b4c569984")
+addappid(626691,0,"d13786cc9fbb38ba37c0da1e5a7148f908520d248bcb1b44cd3d368b4c569984")
 addappid(774821)
 addappid(774822)
 addappid(774823)
