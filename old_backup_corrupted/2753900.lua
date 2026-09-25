451c3328509ec7eb467813a17dd56deefa2b9378
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:51:34 -0300

    Sync LUA 2753900 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/2753900.lua
+++ old_backup_corrupted/2753900.lua
@@ -1,4 +1,6 @@
 --game: The King is Watching
 
 addappid(2753900)
-addappid(2753901,0,"416d49c23e9d45fbd519129ae945ff52e8e57b2e8a0170688a8623c6ca0d3941")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(2753901, 1, "416d49c23e9d45fbd519129ae945ff52e8e57b2e8a0170688a8623c6ca0d3941") -- Depot 2753901
