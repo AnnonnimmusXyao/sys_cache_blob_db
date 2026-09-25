ae0c355b4befb5c0d91093bc7070611a35809c8b
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:25:42 -0300

    Sync LUA 1853200 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1853200.lua
+++ old_backup_corrupted/1853200.lua
@@ -1,4 +1,5 @@
 --game: TYRONE vs COPS
 
 addappid(1853200)
-addappid(1853201,0,"39a775fb16aa1add61ddb97c4fa9c07398c229b8758ddb8bc1dddef5940157ba")
+addappid(1853201, 1, "39a775fb16aa1add61ddb97c4fa9c07398c229b8758ddb8bc1dddef5940157ba") -- Main Game Content (Windows Content)
+addappid(1853202, 1, "2ac62fcace20bceeaac76b117518f206f57d91160812e48443ca48aaba20df75") -- Game Content (Linux Binaries)
