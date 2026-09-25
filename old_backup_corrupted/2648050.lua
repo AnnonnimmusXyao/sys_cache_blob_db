7394f2a18614adec81a579e2127d9456c1bbb42e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:48:24 -0300

    Sync LUA 2648050 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/2648050.lua
+++ old_backup_corrupted/2648050.lua
@@ -1,4 +1,6 @@
 --game: Sam & Max: The Devil's Playhouse
 
 addappid(2648050)
-addappid(2648052,0,"19d40578faab545e248a07945688c4b8652911fa745ced31364303a471c93ff7")
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(2648051, 1, "8003b2e8ee42c8673c714d4a74712e8566f9f78df60fcfc3b08faddda0d1804c") -- Main Game Content (Windows Content)
+addappid(2648052, 1, "19d40578faab545e248a07945688c4b8652911fa745ced31364303a471c93ff7") -- Game Content (Linux Binaries)
