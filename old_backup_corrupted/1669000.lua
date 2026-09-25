2ea2b079baea8a3dad3f3212d83ab02eea9833ba
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:18:25 -0300

    Sync LUA 1669000 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1669000.lua
+++ old_backup_corrupted/1669000.lua
@@ -1,18 +1,21 @@
 --game: Age of Wonders 4
 
 addappid(1669000)
-addappid(1669001,0,"c9f8c2fa2410e5cdca74811acc196b199d9831a62e6f4365e521fcadca9d12a5")
-addappid(1669002,0,"c4f706f7261dee685e2d74998d61a7b4fb175bb47659cce89b560f89fe30ff61")
-addappid(2257970,0,"2725cc874450911b4c09f09607e3a3125e1daa5574ee01d74fe2b27e7d8400f0")
-addappid(2257990,0,"7e40f1272e9d72fa90cb32e8faf281b53d56bd56d220983df52143d7590517e8")
-addappid(2260160)
-addappid(2330150,0,"279b4878ac89aace60e568e256cfa63ceff174760e2873b46fece3f4435209cb")
-addappid(2367860,0,"0deaaf18077207ee03464d1de42b12c00ac5a7d0030f00d7f27ddf8f1c93d0f4")
-addappid(2401830,0,"8c4094bd7b64677817f0211860cbf741b9157cafe4a11f2421288c69ae5107e8")
-addappid(2401850,0,"c29a39c1b2c6028d82b7dd85839375cca70dda4dfdee57687761ae35e4c082e5")
-addappid(2401860,0,"46a1086fb27c2492df2940ac528d7793ed980733ce1c2ea0ab216202a1244dd7")
-addappid(2914280,0,"efa7dbf9c27c1c8fe6553dab29b0e257ecb8c3a3235765ae79671ff744affd13")
-addappid(3119880,0,"bc0d77b48758274a4e61539d17c04865ad8c53089786afd2de063fe74551ba6e")
-addappid(3181480)
-addappid(3181500,0,"f597777e5312438bf357be4399b5be50ebe4b75a469dd5afcb5093d146926979")
-addappid(3181540,0,"f86f726aa931e7892ad89db18d3c78c584e28d33b8fe9a5a61fc719f5ee781df")
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- .NET 4.5.2 Redist (Shared from App 228980)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+addappid(1669001, 1, "c9f8c2fa2410e5cdca74811acc196b199d9831a62e6f4365e521fcadca9d12a5") -- Depot 1669001
+addappid(1669002, 1, "c4f706f7261dee685e2d74998d61a7b4fb175bb47659cce89b560f89fe30ff61") -- Depot 1669002
+addappid(2257970, 1, "2725cc874450911b4c09f09607e3a3125e1daa5574ee01d74fe2b27e7d8400f0") -- Age of Wonders 4 Pre-Order Content Pack - Depot 2257970
+addappid(2257990, 1, "7e40f1272e9d72fa90cb32e8faf281b53d56bd56d220983df52143d7590517e8") -- Age of Wonders 4 Sign-Up Content - Depot 2257990
+addappid(2260160) -- Age of Wonders 4 Expansion Pass 1
+addappid(2330150, 1, "279b4878ac89aace60e568e256cfa63ceff174760e2873b46fece3f4435209cb") -- Age of Wonders 4 Archmage Attire - Depot 2330150
+addappid(2367860, 1, "0deaaf18077207ee03464d1de42b12c00ac5a7d0030f00d7f27ddf8f1c93d0f4") -- Age of Wonders 4 Dragon Dawn - Depot 2367860
+addappid(2401830, 1, "8c4094bd7b64677817f0211860cbf741b9157cafe4a11f2421288c69ae5107e8") -- Age of Wonders 4 Empires  Ashes - Depot 2401830
+addappid(2401850, 1, "c29a39c1b2c6028d82b7dd85839375cca70dda4dfdee57687761ae35e4c082e5") -- Age of Wonders 4 Primal Fury - Depot 2401850
+addappid(2401860, 1, "46a1086fb27c2492df2940ac528d7793ed980733ce1c2ea0ab216202a1244dd7") -- Age of Wonders 4 Eldritch Realms - Depot 2401860
+addappid(2914280, 1, "efa7dbf9c27c1c8fe6553dab29b0e257ecb8c3a3235765ae79671ff744affd13") -- Age of Wonders 4 Ways of War - Depot 2914280
+addappid(3119880, 1, "bc0d77b48758274a4e61539d17c04865ad8c53089786afd2de063fe74551ba6e") -- Age of Wonders 4 Herald of Glory - Depot 3119880
+addappid(3181480) -- Age of Wonders 4 Expansion Pass 2
+addappid(3181500, 1, "f597777e5312438bf357be4399b5be50ebe4b75a469dd5afcb5093d146926979") -- Age of Wonders 4 Giant Kings - Depot 3181500
+addappid(3181540, 1, "f86f726aa931e7892ad89db18d3c78c584e28d33b8fe9a5a61fc719f5ee781df") -- Age of Wonders 4 Archon Prophecy - Depot 3181540
