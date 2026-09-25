015c3c580c8dca3b9697a060d6efc1fd15d0cf46
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:31:03 -0300

    Sync LUA 2019760 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/2019760.lua
+++ old_backup_corrupted/2019760.lua
@@ -1,4 +1,6 @@
 --game: Necrophosis
 
 addappid(2019760)
-addappid(2019761,0,"8f8acee5e26a3229f755f338c1a3268e923929e686b8c7b4f7d243d76f99b19a")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(2019761, 1, "8f8acee5e26a3229f755f338c1a3268e923929e686b8c7b4f7d243d76f99b19a") -- Depot 2019761
