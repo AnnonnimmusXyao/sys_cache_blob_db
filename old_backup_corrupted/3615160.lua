db92e475dd242b9f305e968c55c3190978c144bc
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 22:09:02 -0300

    Sync LUA 3615160 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/3615160.lua
+++ old_backup_corrupted/3615160.lua
@@ -1,4 +1,6 @@
 --game: Teddy's Haven
 
 addappid(3615160)
-addappid(3615161,0,"f41e04a71bb0bfcaecd7722b2d1a181e80bb1b08012861f85a3f74b88dc48da2")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(3615161, 1, "f41e04a71bb0bfcaecd7722b2d1a181e80bb1b08012861f85a3f74b88dc48da2") -- Depot 3615161
