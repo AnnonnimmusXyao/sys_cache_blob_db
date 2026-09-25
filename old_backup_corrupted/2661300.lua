3a8f7c3ff69fb126b2ee5972210039709f309c71
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:48:51 -0300

    Sync LUA 2661300 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/2661300.lua
+++ old_backup_corrupted/2661300.lua
@@ -1,5 +1,9 @@
 --game: Grounded 2
 
 addappid(2661300)
-addappid(2661301,0,"cac026f4758d6992ff85de08375749a662b82dc6517be7dc72368060eca7427d")
-addappid(3788090)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(2661301, 1, "cac026f4758d6992ff85de08375749a662b82dc6517be7dc72368060eca7427d") -- Depot 2661301
+addappid(3788090) -- Grounded 2 Founders Pack
