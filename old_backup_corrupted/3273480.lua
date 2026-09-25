5b862060a68d76a9b017243ddefa8cb66e19ff99
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 22:03:00 -0300

    Sync LUA 3273480 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/3273480.lua
+++ old_backup_corrupted/3273480.lua
@@ -1,6 +1,10 @@
 --game: Reach
 
 addappid(3273480)
-addappid(3273481,0,"475572ec87b0b91df731aabda5a7a2da66b591f5e021dec801ead09088507404")
-addappid(3565160)
-addappid(3919650)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(3273481, 1, "475572ec87b0b91df731aabda5a7a2da66b591f5e021dec801ead09088507404") -- Depot 3273481
+addappid(3565160) -- Reach - Agent Cosmetic Set
+addappid(3919650) -- Reach - Ferran Cosmetic Set (Black)
+addappid(3919660)
