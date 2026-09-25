45512f099f49dcf9ff0e95d3997bc4e6c9f20e0e
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:37:09 -0300

    Sync LUA 2235200 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/2235200.lua
+++ old_backup_corrupted/2235200.lua
@@ -1,5 +1,5 @@
 --game: Neon Abyss 2
 
 addappid(2235200)
-addappid(2235201,0,"64b6a7741dff52341709bb548d844f5cbfb4d551a165eba90db90d49ca88586c")
-addappid(3821980)
+addappid(2235201, 1, "64b6a7741dff52341709bb548d844f5cbfb4d551a165eba90db90d49ca88586c") -- Depot 2235201
+addappid(3821980) -- Neon Abyss 2 - Supporter Pack
