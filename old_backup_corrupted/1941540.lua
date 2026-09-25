31b6e8a5a08199c8fd5dddf0dd27ff9cef30a228
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:28:23 -0300

    Sync LUA 1941540 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1941540.lua
+++ old_backup_corrupted/1941540.lua
@@ -1,8 +1,11 @@
 --game: Mafia: The Old Country
 
 addappid(1941540)
-addappid(1941541,0,"47c1ea957b9ecc8662a117fa6b19eea01ed30ef06478a364110fe40afa4e9f0e")
-addappid(3262000)
-addappid(3269260)
-addappid(3269270)
-addappid(3269280)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") -- .NET 4.0 Redist (Shared from App 228980)
+addappid(1941541, 1, "47c1ea957b9ecc8662a117fa6b19eea01ed30ef06478a364110fe40afa4e9f0e") -- Depot 1941541
+addappid(3262000) -- Mafia The Old Country - Soldato Pack
+addappid(3269260) -- Mafia The Old Country - Padrino Pack
+addappid(3269270) -- Mafia The Old Country - Gatto Nero Pack
+addappid(3269280) -- Mafia The Old Country - Soundtrack and Digital Artbook
