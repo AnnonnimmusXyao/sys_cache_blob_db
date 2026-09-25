b1a1faa8c84e1d681e1443fe927e242a8df12b14
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:48:13 -0300

    Sync LUA 2646460 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/2646460.lua
+++ old_backup_corrupted/2646460.lua
@@ -1,6 +1,4 @@
 --game: Soulmask
 
 addappid(2646460)
-addappid(2646461,0,"b888b7fdf975bfc80d87df6beb2c5d56ada854d2adb6537fce281905a5d74d2a")
-addappid(3336600)
-addappid(3731990)
+addappid(2646461,1,"b888b7fdf975bfc80d87df6beb2c5d56ada854d2adb6537fce281905a5d74d2a")
