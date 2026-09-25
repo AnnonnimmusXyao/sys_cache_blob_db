24aa297c9fc65c3647c17dff802e98722b8c77ff
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 22:08:19 -0300

    Sync LUA 3576880 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/3576880.lua
+++ old_backup_corrupted/3576880.lua
@@ -1,4 +1,5 @@
 --game: Dreadline: Net Quota
 
 addappid(3576880)
-addappid(3576881,0,"305a917fb951148e78dda70afd6cc512e4515b8cc9702304f2eeef6e3f44a2b5")
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(3576881, 1, "305a917fb951148e78dda70afd6cc512e4515b8cc9702304f2eeef6e3f44a2b5") -- Depot 3576881
