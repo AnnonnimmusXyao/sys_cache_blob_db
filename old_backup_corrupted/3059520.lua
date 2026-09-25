7eb53e5f236315182ac8b82ef595a378f8ddea0d
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:58:15 -0300

    Sync LUA 3059520 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/3059520.lua
+++ old_backup_corrupted/3059520.lua
@@ -1,17 +1,19 @@
 --game: F1® 25
 
 addappid(3059520)
-addappid(3059521,0,"69b468d1904c13ad60ff37077c6c6350439f3e70a1df3afcfc075f41325f6b8b")
-addappid(3059522,0,"136b91fa8f220638f0a95077f481dbcb9c6f8d5a936401ec0222ddc2cc73de68")
-addappid(3059523,0,"0fefd33650cc16731c9eaa7582b567f5ab2e1dfe22480ba457f704740cb59275")
-addappid(3059524,0,"0f267a629c6d00a85bbea9fcadd7a8031d68da2504ea8e24821967b15c695e01")
-addappid(3335200)
-addappid(3335210)
-addappid(3335220)
-addappid(3335250)
-addappid(3335260)
-addappid(3493790)
-addappid(3493820)
-addappid(3493830)
-addappid(3546340)
-addappid(3601380)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+addappid(3059521, 1, "69b468d1904c13ad60ff37077c6c6350439f3e70a1df3afcfc075f41325f6b8b")
+addappid(3059522, 1, "136b91fa8f220638f0a95077f481dbcb9c6f8d5a936401ec0222ddc2cc73de68")
+addappid(3059523, 1, "0fefd33650cc16731c9eaa7582b567f5ab2e1dfe22480ba457f704740cb59275")
+addappid(3059524, 1, "0f267a629c6d00a85bbea9fcadd7a8031d68da2504ea8e24821967b15c695e01")
+addappid(3335200) -- F1 25 Iconic Bumper Pack
+addappid(3335210) -- F1 25 Iconic Content Pack
+addappid(3335220) -- F1 25 F1 The Movie - Chapter Scenarios
+addappid(3335250) -- F1 25 Standard Starter Pack
+addappid(3335260) -- F1 25 F1 75 Celebration Pack
+addappid(3493790) -- F1 TV Pro 1-Month Subscription
+addappid(3493820) -- F1 25 Iconic Edition Upgrade
+addappid(3493830) -- F1 25 Iconic Edition Upgrade
+addappid(3546340) -- F1 25 APXGP Team Pack
+addappid(3601380) -- F1 25 - EA Play Trial Key
