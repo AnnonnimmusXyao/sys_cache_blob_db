ad7d8a982abf09c3b588c21d6f97a7859b3b8665
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:24:20 -0300

    Sync LUA 1817800 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/1817800.lua
+++ old_backup_corrupted/1817800.lua
@@ -1,9 +1,10 @@
 --game: Oddsparks: An Automation Adventure
 
 addappid(1817800)
-addappid(1817801,0,"b59ebe145d953e10adb462fd930cbf0da37483e64c0896babf1861638f87b6a5")
-addappid(3073500)
-addappid(3678620)
-addappid(3678630)
-addappid(3678640)
-addappid(3678650)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+addappid(1817801, 1, "b59ebe145d953e10adb462fd930cbf0da37483e64c0896babf1861638f87b6a5") -- Depot 1817801
+addappid(3073500) -- Oddsparks An Automation Adventure - Deluxe Upgrade
+addappid(3678620) -- Oddsparks - Tailors Animal Costumes Pack
+addappid(3678640) -- Oddsparks An Automation Adventure - Making Music
