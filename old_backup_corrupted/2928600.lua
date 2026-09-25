bd8543eb9fc0baa2afa2579039c9951d265180f9
AnnonnimmusXyao <xxasesino2364xx@gmail.com>
2026-09-24 21:55:14 -0300

    Sync LUA 2928600 - Botón 5 (Falta_DLC)

--- old_backup_corrupted/2928600.lua
+++ old_backup_corrupted/2928600.lua
@@ -1,10 +1,12 @@
 --game: Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2
 
 addappid(2928600)
-addappid(2928601,0,"486448df6cd95d0d663ae6dbc0dc5cb7ed590875a2a7481fee25ffe7572b60fc")
-addappid(3371870)
-addappid(3371880)
-addappid(3371890)
-addappid(3371900)
-addappid(3371910)
-addappid(3900330)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(2928601, 1, "486448df6cd95d0d663ae6dbc0dc5cb7ed590875a2a7481fee25ffe7572b60fc") -- Depot 2928601
+addappid(3371870) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Battle Attire Tanjiros Kimono (Entertainment District), Inosukes Kimono (Entertainment District), and Uzuis Shinobi Attire
+addappid(3371880) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Character Unlock Keys Sanemi Shinazugawa, Obanai Iguro, and Gyomei Himejima
+addappid(3371890) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 VS Mode System Voice Upper Rank Demons Set (Akaza, Daki, Gyutaro, Gyokko, Zohakuten)
+addappid(3371900) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Character Unlock Keys Mitsuri Kanroji and Muichiro Tokito
+addappid(3371910) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Digital Version Bonus - Character Unlock Keys Academy Rengoku and Academy Uzui
+addappid(3900330) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Core Add-on Bundle
